55e718d56fd754ef0bb64b6ddfc707c2627c04e4
Johan Berggren <jberggren@gmail.com>
2015-01-27 20:47:53 -0800

    New database model for user

---
+++ timesketch/models/user.py
@@ -0,0 +1,70 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module implements the user model."""
+
+from flask_bcrypt import generate_password_hash
+from flask_bcrypt import check_password_hash
+from flask_login import UserMixin
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+from timesketch.models import BaseModel
+
+
+class User(UserMixin, BaseModel):
+    """Implements the User model."""
+
+    username = Column(String(255), unique=True)
+    password = Column(String(128))
+    name = Column(String(255))
+    email = Column(String(255))
+    sketches = relationship('Sketch', backref='user', lazy='dynamic')
+    searchindices = relationship('SearchIndex', backref='user', lazy='dynamic')
+    timelines = relationship('Timeline', backref='user', lazy='dynamic')
+    views = relationship('View', backref='user', lazy='dynamic')
+
+    def __init__(self, username, name=None):
+        """Initialize the User object.
+
+        Args:
+            username: Username for the user
+            name: Name of the user
+        """
+        self.username = username
+        self.name = name
+        if not name:
+            self.name = username
+
+    def set_password(self, plaintext, rounds=12):
+        """Sets the password for the user. The password hash is created with the
+        Bcrypt python library (http://www.mindrot.org/projects/py-bcrypt/).
+
+        Args:
+            plaintext: The plaintext password to hash
+            rounds: Number of rounds to use for the bcrypt hashing
+        """
+        self.password = generate_password_hash(plaintext, rounds)
+
+    def check_password(self, plaintext):
+        """Check a plaintext password against a stored password hash.
+
+        Args:
+            plaintext: A plaintext password
+
+        Returns:
+            A boolean value indicating if the plaintext password matches the
+            stored password hash.
+        """
+        return check_password_hash(self.password, plaintext)
---
+++ timesketch/models/user_test.py
@@ -0,0 +1,44 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for the user model."""
+
+from timesketch.lib.testlib import ModelBaseTest
+from timesketch.models.user import User
+
+
+class UserModelTest(ModelBaseTest):
+    """Tests the user model."""
+    def test_user_model(self):
+        """
+        Test that the test user has the expected data stored in the
+        database.
+        """
+        expected_result = frozenset([
+            (u'name', 'test1'),
+            (u'username', 'test1')
+        ])
+        self._test_db_object(
+            expected_result=expected_result, model_cls=User)
+
+    def test_set_password(self):
+        """Test setting a password for the user."""
+        self.assertIsNone(self.user1.set_password('test', rounds=1))
+
+    def test_valid_password(self):
+        """Test checking a valid password."""
+        self.assertTrue(self.user1.check_password('test'))
+
+    def test_invalid_password(self):
+        """Test checking a invalid password."""
+        self.assertFalse(self.user1.check_password('invalid password'))
