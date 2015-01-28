e8561c31223f0e5196092767400c9a7c742087c4
Johan Berggren <jberggren@gmail.com>
2015-01-27 20:48:30 -0800

    Database models for the annotation system

---
+++ timesketch/models/annotations.py
@@ -0,0 +1,201 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module implements annotations that can be use on other database models.
+"""
+
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Text
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import relationship
+
+from timesketch.models import BaseModel
+from timesketch.models import db_session
+
+
+class BaseAnnotation(object):
+    """Base class with common attributes."""
+    @declared_attr
+    def user_id(self):
+        """Foreign key to a user model.
+
+        Returns:
+            A column (instance of sqlalchemy.Column)
+        """
+        return Column(Integer, ForeignKey('user.id'))
+
+    @declared_attr
+    def user(self):
+        """A relationship to a user object.
+
+        Returns:
+            A relationship (instance of sqlalchemy.orm.relationship)
+        """
+        return relationship('User')
+
+
+class Label(BaseAnnotation):
+    """A label annotation."""
+    label = Column(String(255))
+
+    def __init__(self, user, label):
+        """Initialize the model.
+
+        Args:
+            user = A user (instance of timesketch.models.user.User)
+            name = Name of the label
+        """
+        self.user = user
+        self.label = label
+
+
+class Comment(BaseAnnotation):
+    """A comment annotation."""
+    comment = Column(Text())
+
+    def __init__(self, user, comment):
+        """Initialize the model.
+
+        Args:
+            user = A user (instance of timesketch.models.user.User)
+            body = The body if the comment
+        """
+        self.user = user
+        self.comment = comment
+
+
+class Status(BaseAnnotation):
+    """A status annotation."""
+    status = Column(String(255))
+
+    def __init__(self, user, status):
+        """Initialize the model.
+
+        Args:
+            user = A user (instance of timesketch.models.user.User)
+            status = The type of status (string, e.g. open)
+        """
+        self.user = user
+        self.status = status
+
+
+class LabelMixin(object):
+    """
+    A MixIn for generating the necessary tables in the database and to make
+    it accessible from the parent model object (the model object that uses this
+    MixIn, i.e. the object that the label is added to).
+    """
+    @declared_attr
+    def labels(self):
+        """
+        Generates the label tables and adds the attribute to the parent model
+        object.
+
+        Returns:
+            A relationship to an label (timesketch.models.annotation.Label)
+        """
+        self.Label = type(
+            '%sLabel' % self.__name__,
+            (Label, BaseModel,),
+            dict(
+                __tablename__='%s_label' % self.__tablename__,
+                parent_id=Column(
+                    Integer, ForeignKey('%s.id' % self.__tablename__)),
+                parent=relationship(self),
+            )
+        )
+        return relationship(self.Label)
+
+
+class CommentMixin(object):
+    """
+    A MixIn for generating the necessary tables in the database and to make
+    it accessible from the parent model object (the model object that uses this
+    MixIn, i.e. the object that the comment is added to).
+    """
+    @declared_attr
+    def comments(self):
+        """
+        Generates the comment tables and adds the attribute to the parent model
+        object.
+
+        Returns:
+            A relationship to a comment (timesketch.models.annotation.Comment)
+        """
+        self.Comment = type(
+            '%sComment' % self.__name__,
+            (Comment, BaseModel,),
+            dict(
+                __tablename__='%s_comment' % self.__tablename__,
+                parent_id=Column(
+                    Integer, ForeignKey('%s.id' % self.__tablename__)),
+                parent=relationship(self),
+            )
+        )
+        return relationship(self.Comment)
+
+
+class StatusMixin(object):
+    """
+    A MixIn for generating the necessary tables in the database and to make
+    it accessible from the parent model object (the model object that uses this
+    MixIn, i.e. the object that the status is added to).
+    """
+    @declared_attr
+    def status(self):
+        """
+        Generates the status tables and adds the attribute to the parent model
+        object.
+
+        Returns:
+            A relationship to a status (timesketch.models.annotation.Status)
+        """
+        self.Status = type(
+            '%sStatus' % self.__name__,
+            (Status, BaseModel,),
+            dict(
+                __tablename__='%s_status' % self.__tablename__,
+                parent_id=Column(
+                    Integer, ForeignKey('%s.id' % self.__tablename__)),
+                parent=relationship(self),
+            )
+        )
+        return relationship(self.Status)
+
+    def set_status(self, status):
+        """
+        Set status on object. Although this is a many-to-many relationship
+        this makes sure that the parent object only has one status set.
+
+        Args:
+            status: Name of the status
+        """
+        for _status in self.status:
+            self.status.remove(_status)
+        self.status.append(self.Status(user=None, status=status))
+        db_session.commit()
+
+    @property
+    def get_status(self):
+        """Get the current status.
+
+        Returns:
+            The status as a string
+        """
+        if not self.status:
+            self.status.append(self.Status(user=None, status='new'))
+        return self.status[0]
---
+++ timesketch/models/annotations_test.py
@@ -0,0 +1,48 @@
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for the annotation models."""
+
+from timesketch.lib.testlib import BaseTest
+
+
+class AnnotationBaseTest(BaseTest):
+    """Base class for common tests."""
+    def _test_annotation(self, parent):
+        """Test that the annotations has our test user as owner."""
+        self.assertEquals(parent[0].user, self.user1)
+        self.assertEquals(len(parent), 1)
+
+
+class LabelModelTest(AnnotationBaseTest):
+    """Test the label annotation."""
+    def test_label_annotation(self):
+        """Test that the label is associated with our test sketch."""
+        self._test_annotation(self.sketch1.labels)
+        self.assertEquals(self.sketch1.labels[0].label, 'Test label')
+
+
+class StatusModelTest(AnnotationBaseTest):
+    """Test the status annotation."""
+    def test_status_annotation(self):
+        """Test that the status is associated with our test sketch."""
+        self._test_annotation(self.sketch1.status)
+        self.assertEquals(self.sketch1.status[0].status, 'Test status')
+
+
+class CommentModelTest(AnnotationBaseTest):
+    """Test the comment annotation."""
+    def test_comment_annotation(self):
+        """Test that the comment is associated with our test event."""
+        self._test_annotation(self.event.comments)
+        self.assertEquals(self.event.comments[0].comment, 'test')
