5ca5e515214765089313632dbc4b037dac9179b4
Fredrik Pettai <pettai@nordu.net>
2015-11-29 17:14:28 +0100

    Generate SECREY_KEY on startup
    Generate SECRET_KEY on startup instead of having it statically
    (un)configured.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+set -x
 set -e
 
 if [ "$1" = 'timesketch' ]; then
-  #celery should be running in a separate container
-  #su -c 'celery -A timesketch.lib.tasks worker -D' timesketch &
-  #sleep 1 # avoid race condition in-between celery <-> tsctl on timesketch.db
+  # check if SECRET_KEY is configured
+  if [[ `grep -c "SECRET_KEY = u''" /etc/timesketch.conf` -eq 1 ]]; then
+    NEWKEY=`openssl rand -base64 32`
+    #sed -i -e '/^SECRET_KEY = u''/s/^/#/' /etc/timesketch.conf
+    echo "SECRET_KEY = u'$NEWKEY'" >> /etc/timesketch.conf
+  fi
+  su -c 'celery -A timesketch.lib.tasks worker -D --workdir=/usr/local/share/timesketch' timesketch &
   if [ $USER_NAME ] && [ $USER_PASSWORD ]; then
     su -c 'tsctl add_user -u "$USER_NAME" -p "$USER_PASSWORD"' timesketch
   else
