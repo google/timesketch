ac24593f80700b279af5268ece13f793bea673ca
Jesse Millar <hellojessemillar@gmail.com>
2016-10-31 15:03:25 -0600

    Finally found the issue with Flask

--- Dockerfile
+++ Dockerfile
@@ -10,20 +10,22 @@ RUN apt-get -y dist-upgrade
 RUN apt-get -y install python-pip python-dev libffi-dev
 
 # Use pip to install Timesketch
-RUN pip install timesketch
+# RUN pip install timesketch
+# Install from source (master branch of repository) because flask_login is currently broken (https://github.com/google/timesketch/pull/244)
+RUN pip install https://github.com/google/timesketch/archive/master.zip
 
 # Copy the Timesketch configuration file into /etc
 RUN cp /usr/local/share/timesketch/timesketch.conf /etc
 RUN chmod 600 /etc/timesketch.conf
 
 # Copy the entrypoint script into the container
-COPY docker-entrypoint.sh /usr/local/bin/
-
-# Run the entrypoint script on container start
-ENTRYPOINT ["docker-entrypoint.sh"]
+COPY docker-entrypoint.sh / 
 
 # Expose the port used by Timesketch
 EXPOSE 5000
 
-# Run Timesketch on container start
+# Load the entrypoint script to be run later
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+# Invoke the entrypoint script
 CMD ["timesketch"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# Run the container in default way
 if [ "$1" = 'timesketch' ]; then
 	# Set SECRET_KEY in /etc/timesketch.conf if it isn't already set
 	if grep -q "SECRET_KEY = u''" /etc/timesketch.conf; then
 		OPENSSL_RAND=$( openssl rand -base64 32 )
-		sed -i 's/SECRET_KEY = u\x27\x27/SECRET_KEY = u\x27'$OPENSSL_RAND'\x27/' /etc/timesketch.conf
+		# Using the pound sign as a delimiter to avoid problems with / being output from openssl
+		sed -i 's#SECRET_KEY = u\x27\x27#SECRET_KEY = u\x27'$OPENSSL_RAND'\x27#' /etc/timesketch.conf 
 	fi
 
 	if [ $USER_NAME ] && [ $USER_PASSWORD ]; then
-		tsctl add_user -u "$USER_NAME" -p "$USER_PASSWORD"
+		sed -i 's/postgresql:\/\/<USERNAME>:<PASSWORD>/postgresql:\/\/'$USER_NAME':'$USER_PASSWORD'/' /etc/timesketch.conf
+		tsctl add_user -u "$USER_NAME" 
 	else
-		tsctl add_user -u demo -p demo
+		sed -i 's/postgresql:\/\/<USERNAME>:<PASSWORD>/postgresql:\/\/demo:demo/' /etc/timesketch.conf
+		tsctl add_user -u demo
 	fi
 
 	# Run the Timesketch server (without SSL)
 	exec `tsctl runserver -h 0.0.0.0 -p 5000`
 fi
 
-# Run normally with all passed parameters
+# Run a custom command on container start
 exec "$@"
