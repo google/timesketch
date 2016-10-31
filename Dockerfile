3c2315b4dfdb2c9f1ce5c776b3080c475973d5be
Jesse Millar <hellojessemillar@gmail.com>
2016-10-31 09:39:54 -0600

    Trying to fix an import error

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ RUN apt-get -y install python-pip python-dev libffi-dev
 RUN pip install timesketch
 
 # Copy the Timesketch configuration file into /etc
-RUN cp /usr/local/share/timesketch/timesketch.conf /etc/
+RUN cp /usr/local/share/timesketch/timesketch.conf /etc
 RUN chmod 600 /etc/timesketch.conf
 
 # Copy the entrypoint script into the container
@@ -21,9 +21,6 @@ COPY docker-entrypoint.sh /usr/local/bin/
 
 # Run the entrypoint script on container start
 ENTRYPOINT ["docker-entrypoint.sh"]
-
-# Use a Docker volume to make data persistent across container restarts
-VOLUME ["/usr/local/share/timesketch"]
 
 # Expose the port used by Timesketch
 EXPOSE 5000
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,18 +3,18 @@
 if [ "$1" = 'timesketch' ]; then
 	# Set SECRET_KEY in /etc/timesketch.conf if it isn't already set
 	if grep -q "SECRET_KEY = u''" /etc/timesketch.conf; then
-		OPENSSL_RAND=${openssl rand -base64 32}
-		sed -i 's/SECRET_KEY = u''/SECRET_KEY = u\x27$OPENSSL_RAND\x27/' /etc/timesketch.conf
-		cat /etc/timesketch.conf
+		OPENSSL_RAND=$( openssl rand -base64 32 )
+		sed -i 's/SECRET_KEY = u\x27\x27/SECRET_KEY = u\x27'$OPENSSL_RAND'\x27/' /etc/timesketch.conf
 	fi
 
 	if [ $USER_NAME ] && [ $USER_PASSWORD ]; then
-		su -c 'tsctl add_user -u "$USER_NAME" -p "$USER_PASSWORD"' timesketch
+		tsctl add_user -u "$USER_NAME" -p "$USER_PASSWORD"
 	else
-		su -c 'tsctl add_user -u demo -p demo' timesketch
+		tsctl add_user -u demo -p demo
 	fi
 
-	exec `su -c 'tsctl runserver -h 0.0.0.0' timesketch`
+	# Run the Timesketch server (without SSL)
+	exec `tsctl runserver -h 0.0.0.0 -p 5000`
 fi
 
 # Run normally with all passed parameters
