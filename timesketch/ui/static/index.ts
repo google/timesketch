4ef25bf9d1ac05bd8ddc0ef552ec14cead88f942
Franciszek Piszcz <franek@google.com>
2017-08-23 16:30:05 +0200

    Add css pipeline to Webpack.

--- timesketch/ui/static/index.ts
+++ timesketch/ui/static/index.ts
@@ -1,6 +1,12 @@
+import 'font-awesome/css/font-awesome.css'
 import * as $ from 'jquery'
 (window as any).jQuery = $
-import 'twitter-bootstrap-3.0.0/dist/js/bootstrap.min.js'
+import 'twitter-bootstrap-3.0.0/dist/css/bootstrap.css'
+import 'twitter-bootstrap-3.0.0/dist/js/bootstrap.js'
+import 'medium-editor/dist/css/medium-editor.css'
+import 'medium-editor/dist/css/themes/default.css'
+
+import 'css/ts.css'
 import 'app'
 import 'components/api/api'
 import 'components/api/api-service'
