50e6664982eca55cdf593edaa7b3e51a13394ef3
Franciszek Piszcz <franekp@users.noreply.github.com>
2017-09-12 18:24:01 +0200

    Add frontend unit tests with Karma and PhantomJS. (#423)
    * More Angular 2 style guide.
    
    * Rename navigation.ng.html -> navigation.component.html
    * Rename ts-navigation -> ts-sketch-navigation
    
    * Add javascript test runner.
    
    * Add coverage reporting to js tests.

---
+++ timesketch/ui/main.spec.ts
@@ -0,0 +1,15 @@
+// needed for angular async magic in unit tests
+import 'zone.js/dist/zone'
+import 'zone.js/dist/long-stack-trace-zone'
+import 'zone.js/dist/async-test'
+import 'zone.js/dist/fake-async-test'
+import 'zone.js/dist/sync-test'
+import 'zone.js/dist/proxy'
+import 'zone.js/dist/jasmine-patch'
+
+import 'app.module'
+
+// require all modules ending in ".spec.ts" from the current directory and
+// all subdirectories
+const tests_context = (require as any).context('.', true, /\.spec\.ts$/)
+tests_context.keys().forEach(tests_context)
--- timesketch/ui/main.ts
+++ timesketch/ui/main.ts
@@ -1,17 +1,6 @@
-import 'font-awesome/css/font-awesome.css'
-import * as $ from 'jquery'
-(window as any).jQuery = $
-import 'twitter-bootstrap-3.0.0/dist/css/bootstrap.css'
-import 'twitter-bootstrap-3.0.0/dist/js/bootstrap.js'
-import 'medium-editor/dist/css/medium-editor.css'
-import 'medium-editor/dist/css/themes/default.css'
-
-import 'zone.js'
-
 import angular from 'angularjs-for-webpack'
 import {downgradeModule} from '@angular/upgrade/static'
 
-import 'css/ts.css'
 import {tsAppModule, AppModule} from 'app.module'
 
 angular.element(function () {
