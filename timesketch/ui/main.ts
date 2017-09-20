google/timesketch

commit 50e6664982eca55cdf593edaa7b3e51a13394ef3

Add frontend unit tests with Karma and PhantomJS. (#423)
* More Angular 2 style guide.

* Rename navigation.ng.html -> navigation.component.html
* Rename ts-navigation -> ts-sketch-navigation

* Add javascript test runner.

* Add coverage reporting to js tests.

timesketch/ui/main.ts
@@ -1,17 +1,6 @@
-import 'font-awesome/css/font-awesome.css'
-import * as $ from 'jquery'
-(window as any).jQuery = $
-import 'twitter-bootstrap-3.0.0/dist/css/bootstrap.css'
-import 'twitter-bootstrap-3.0.0/dist/js/bootstrap.js'
-import 'medium-editor/dist/css/medium-editor.css'
-import 'medium-editor/dist/css/themes/default.css'
-
-import 'zone.js'
-
 import angular from 'angularjs-for-webpack'
 import {downgradeModule} from '@angular/upgrade/static'
 
-import 'css/ts.css'
 import {tsAppModule, AppModule} from 'app.module'
 
 angular.element(function () {

karma-coverage.conf.js
@@ -1,0 +1,67 @@
+const path = require('path')
+const webpack_config = require('./webpack.config.js')
+
+const webpack_test_config = {
+  entry: './timesketch/ui/main.spec.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'istanbul-instrumenter-loader',
+          'ts-loader',
+          'angular2-template-loader',
+        ],
+        exclude: /node_modules|\.spec\.ts/,
+      },
+      {
+        test: /\.spec\.ts/,
+        use: [
+          'ts-loader',
+          'angular2-template-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: 'null-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: [path.resolve(__dirname, 'timesketch/ui/'), 'node_modules'],
+  },
+}
+
+module.exports = function(config) {
+  config.set({
+    files: [
+      './node_modules/babel-polyfill/dist/polyfill.js',
+      'timesketch/ui/main.spec.ts',
+    ],
+    preprocessors: {'timesketch/ui/main.spec.ts': ['webpack']},
+    webpack: webpack_test_config,
+    webpackMiddleware: {
+      noInfo: true,
+      stats: {chunks: false},
+    },
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-webpack'),
+      require('karma-jasmine'),
+      require('karma-phantomjs-launcher'),
+      require('karma-coverage-istanbul-reporter'),
+    ],
+    browsers: ['PhantomJS'],
+    reporters: ['progress', 'coverage-istanbul'],
+    coverageIstanbulReporter: {
+      reports: ['text-summary'],
+      fixWebpackSourcePaths: false,
+    }
+  })
+}

karma.conf.js
@@ -1,0 +1,49 @@
+const path = require('path')
+
+const webpack_test_config = {
+  entry: './timesketch/ui/main.spec.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader', 'angular2-template-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: 'null-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: [path.resolve(__dirname, 'timesketch/ui/'), 'node_modules'],
+  },
+}
+
+module.exports = function(config) {
+  config.set({
+
+    files: [
+      './node_modules/babel-polyfill/dist/polyfill.js',
+      'timesketch/ui/main.spec.ts',
+    ],
+    preprocessors: {'timesketch/ui/main.spec.ts': ['webpack']},
+    webpack: webpack_test_config,
+    webpackMiddleware: {
+      noInfo: true,
+      stats: {chunks: false},
+    },
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-webpack'),
+      require('karma-jasmine'),
+      require('karma-phantomjs-launcher'),
+    ],
+    browsers: ['PhantomJS'],
+  })
+}

run_tests.py
@@ -37,8 +37,10 @@ def run_python(args):
         run_python_linter()
 
 def run_javascript_tests(coverage=False):
-    # pylint: disable=unused-argument
-    return NotImplemented
+    if coverage:
+        subprocess.check_call(['yarn', 'run', 'test:coverage'])
+    else:
+        subprocess.check_call(['yarn', 'run', 'test'])
 
 def run_javascript_linter():
     subprocess.check_call(['yarn', 'run', 'lint'])

timesketch/ui/app.module.ts
@@ -13,9 +13,21 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import 'font-awesome/css/font-awesome.css'
+import * as $ from 'jquery'
+(window as any).jQuery = $
+import 'twitter-bootstrap-3.0.0/dist/css/bootstrap.css'
+import 'twitter-bootstrap-3.0.0/dist/js/bootstrap.js'
+import 'medium-editor/dist/css/medium-editor.css'
+import 'medium-editor/dist/css/themes/default.css'
+import 'zone.js'
+
 import angular from 'angularjs-for-webpack'
 import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
+
+import 'css/ts.css'
+
 import {tsApiModule} from './api/api.module'
 import {tsCoreModule} from './core/core.module'
 import {tsExploreModule} from './explore/explore.module'

timesketch/ui/main.spec.ts
@@ -1,0 +1,15 @@
+// needed for angular async magic in unit tests
+import 'zone.js/dist/zone'
+import 'zone.js/dist/long-stack-trace-zone'
+import 'zone.js/dist/async-test'
+import 'zone.js/dist/fake-async-test'
+import 'zone.js/dist/sync-test'
+import 'zone.js/dist/proxy'
+import 'zone.js/dist/jasmine-patch'
+
+import 'app.module'
+
+// require all modules ending in ".spec.ts" from the current directory and
+// all subdirectories
+const tests_context = (require as any).context('.', true, /\.spec\.ts$/)
+tests_context.keys().forEach(tests_context)

timesketch/ui/sketch/navigation.component.spec.ts
@@ -1,0 +1,39 @@
+// App tests
+import {async, inject, TestBed, ComponentFixture} from '@angular/core/testing'
+import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing'
+import {BrowserModule} from '@angular/platform-browser'
+import {NavigationComponent} from './navigation.component'
+
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting())
+
+describe('NavigationComponent', () => {
+
+  let fixture: ComponentFixture<NavigationComponent>
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [BrowserModule],
+      declarations: [NavigationComponent],
+    })
+    fixture = TestBed.createComponent(NavigationComponent)
+  })
+
+  const tabs = ['overview', 'explore', 'stories', 'views', 'timelines']
+
+  for (const [tabIndex, tab] of tabs.entries()) {
+    it(`should render navigation for the "${tab}" tab correctly`, async(() => {
+      fixture.componentInstance.active = tab
+      fixture.detectChanges()
+      const navEntries = fixture.nativeElement.querySelectorAll('li')
+      expect(navEntries.length).toEqual(tabs.length)
+      for (const [navEntryIndex, navEntry] of navEntries.entries()) {
+        if (navEntryIndex === tabIndex) {
+          expect(navEntry.classList).toContain('active')
+        } else {
+          expect(navEntry.classList).not.toContain('active')
+        }
+      }
+    }))
+  }
+
+})

commit 26926a2d15e43874800ce3c90182896807ae70b9

Fix Angular bootstrap broken in #423

timesketch/ui/main.ts
@@ -1,9 +1,10 @@
+import * as $ from 'jquery'
 import angular from 'angularjs-for-webpack'
 import {downgradeModule} from '@angular/upgrade/static'
 
 import {tsAppModule, AppModule} from 'app.module'
 
-angular.element(function () {
+$(function () {
 
   // @ngtools/webpack should automatically convert AppModule to
   // AppModuleNgFactory. We are casting it through any to make TypeScript happy