a801ea9cd039e27ec20a628235b10a99d67a467b
Johan Berggren <jberggren@gmail.com>
2017-10-23 15:21:32 +0200

    Predefined queries (#479)
    * Small UI changes
    
    * Add predfined queries
    
    * Change order of dropdowns
    
    * Stay consitent in CSS

--- timesketch/ui/graphs/cypher-query.component.ts
+++ timesketch/ui/graphs/cypher-query.component.ts
@@ -6,7 +6,23 @@ import {Component, EventEmitter, Output} from '@angular/core'
 })
 export class CypherQueryComponent {
   @Output() cypherSearch = new EventEmitter<string>()
-  query = 'MATCH (a)-[e]->(b) RETURN *'
+  query = ''
+
+  predefinedQueries = [
+    {
+      name: 'Interactive logins',
+      query: 'MATCH (user:WindowsADUser)-[r1:ACCESS]->(m1:WindowsMachine) WHERE r1.method = "Interactive" RETURN *',
+    },
+    {
+      name: 'Entire graph',
+      query: 'MATCH (a)-[e]->(b) RETURN *',
+    },
+  ]
+
+  onQuerySelect(query) {
+    this.query = query
+    this.onSubmit()
+  }
 
   onSubmit() {
     this.cypherSearch.emit(this.query)
