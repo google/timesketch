43fa2011811220f77b1673980b75c9ec606547bb
Franciszek Piszcz <franekp@users.noreply.github.com>
2017-10-23 14:17:07 +0200

    Fix memory leak - destroy Cytoscape instances when not used. (#477)

--- timesketch/ui/graphs/graph-view.component.ts
+++ timesketch/ui/graphs/graph-view.component.ts
@@ -12,6 +12,10 @@ export type Ready = {
 
 export type GraphViewState = Empty | Loading | Ready
 
+export type Layout = Cy.LayoutOptions & {animationThreshold?: number}
+
+export type Style = Array<Cy.Stylesheet & {padding?: number}>
+
 function format(formatString: string, params: {[k: string]: string}): string {
   let result = formatString
   for (const [k, v] of Object.entries(params)) {
@@ -54,7 +58,7 @@ export class GraphViewComponent {
     wheelSensitivity: 1,
     pixelRatio: ('auto' as any),
   }
-  layout: Cy.LayoutOptions & {animationThreshold?: number} = {
+  private _layout: Layout = {
     name: 'cose',
     animate: true,
     animationThreshold: 250,
@@ -78,74 +82,88 @@ export class GraphViewComponent {
     weaver: false,
     nodeDimensionsIncludeLabels: false,
   }
+  private _null_layout: Layout = {name: 'null'}
+  get layout(): Layout {
+    if (this.state.type === 'ready') {
+      return this._layout
+    } else {
+      return this._null_layout
+    }
+  }
   nodeLabel(node_data) {
-    const {label_template} = (this.state as Ready).graph.schema.nodes[node_data.type]
-    return format(label_template, node_data)
+    if (this.state.type === 'ready') {
+      const {label_template} = this.state.graph.schema.nodes[node_data.type]
+      return format(label_template, node_data)
+    } else {
+      return ''
+    }
   }
   edgeLabel(edge_data) {
-    const {label_template} = (this.state as Ready).graph.schema.edges[edge_data.type]
-    return format(label_template, edge_data)
+    if (this.state.type === 'ready') {
+      const {label_template} = this.state.graph.schema.edges[edge_data.type]
+      return format(label_template, edge_data)
+    } else {
+      return ''
+    }
   }
-  get style(): Array<Cy.Stylesheet & {padding?: number}> {
-    return [
-      {
-        selector: 'node',
-        style: {
-          'shape': 'roundrectangle',
-          'width': 'label',
-          'height': 'label',
-          'padding': 10,
-          'label': (node) => this.nodeLabel(node.data()),
-          'text-halign': 'center',
-          'text-valign': 'center',
-          'color': '#FFFFFF',
-          'font-size': '10',
-          'background-color': '#68BDF6',
-          'border-color': '#5CA8DB',
-          'border-width': '1',
-          'text-wrap': 'wrap',
-          'text-max-width': '20',
-        },
+  style: Style = [
+    {
+      selector: 'node',
+      style: {
+        'shape': 'roundrectangle',
+        'width': 'label',
+        'height': 'label',
+        'padding': 10,
+        'label': (node) => this.nodeLabel(node.data()),
+        'text-halign': 'center',
+        'text-valign': 'center',
+        'color': '#FFFFFF',
+        'font-size': '10',
+        'background-color': '#68BDF6',
+        'border-color': '#5CA8DB',
+        'border-width': '1',
+        'text-wrap': 'wrap',
+        'text-max-width': '20',
       },
-      {
-        selector: "node[type = 'WindowsADUser']",
-        style: {
-          'background-color': '#FF756E',
-          'border-color': '#E06760',
-        },
+    },
+    {
+      selector: "node[type = 'WindowsADUser']",
+      style: {
+        'background-color': '#FF756E',
+        'border-color': '#E06760',
       },
-      {
-        selector: "node[type = 'WindowsMachine']",
-        style: {
-          'background-color': '#68BDF6',
-          'border-color': '#5CA8DB',
-        },
+    },
+    {
+      selector: "node[type = 'WindowsMachine']",
+      style: {
+        'background-color': '#68BDF6',
+        'border-color': '#5CA8DB',
       },
-      {
-        selector: "node[type = 'WindowsService']",
-        style: {
-          'background-color': '#6DCE9E',
-          'border-color': '#60B58B',
-        },
+    },
+    {
+      selector: "node[type = 'WindowsService']",
+      style: {
+        'background-color': '#6DCE9E',
+        'border-color': '#60B58B',
       },
-      {
-        selector: "node[type = 'WindowsServiceImagePath']",
-        style: {
-          'background-color': '#DE9BF9',
-          'border-color': '#BF85D6',
-        },
+    },
+    {
+      selector: "node[type = 'WindowsServiceImagePath']",
+      style: {
+        'background-color': '#DE9BF9',
+        'border-color': '#BF85D6',
       },
-      {
-        selector: 'edge',
-        style: {
-          'width': 1,
-          'curve-style': 'bezier',
-          'target-arrow-shape': 'triangle',
-          'label': (edge) => this.edgeLabel(edge.data()),
-          'font-size': 10,
-          'text-rotation': 'autorotate',
-        },
+    },
+    {
+      selector: 'edge',
+      style: {
+        'width': 1,
+        'curve-style': 'bezier',
+        'target-arrow-shape': 'triangle',
+        'label': (edge) => this.edgeLabel(edge.data()),
+        'font-size': 10,
+        'text-rotation': 'autorotate',
       },
-    ]
-  }
+    },
+  ]
 }
