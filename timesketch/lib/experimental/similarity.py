334ef598299b7e6f811a4d80d9191208e239d22f
Johan Berggren <jberggren@gmail.com>
2017-12-06 17:32:05 +0100

    Move client to tsctl

--- timesketch/lib/experimental/similarity.py
+++ timesketch/lib/experimental/similarity.py
@@ -11,106 +11,122 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from flask import current_app
-
-from datetime import datetime
 import re
-
+from flask import current_app
 from datasketch.minhash import MinHash
 from datasketch.lsh import MinHashLSH
-
 from timesketch.lib.datastores.elastic import ElasticsearchDataStore
 
 
-data_type_registry = {
-    'windows:evtx:record': {
-        'query': 'data_type:"windows:evtx:record"',
-        'field': 'message',
-        'delimiters': [' ', '-', '/']
-    },
-    'chrome:history:page_visited': {
-        'query': 'data_type:"chrome:history:page_visited"',
-        'field': 'message',
-        'delimiters': [' ', '-', '/']
+class DataTypeConfig(object):
+
+    REGISTRY = {
+        'windows:evtx:record': {
+            'query': 'data_type:"windows:evtx:record"',
+            'field': 'message',
+            'delimiters': [' ', '-', '/']
+        },
+        'chrome:history:page_visited': {
+            'query': 'data_type:"chrome:history:page_visited"',
+            'field': 'message',
+            'delimiters': [' ', '-', '/']
+        }
     }
-}
+
+    def __init__(self, data_type):
+        self._data_type = self._get_config(data_type)
+
+    def _get_config(self, data_type):
+        default_config = {
+            'query': 'data_type:"{0}"'.format(data_type),
+            'field': 'message',
+            'delimiters': [' ', '-', '/']
+        }
+        return self.REGISTRY.get(data_type, default_config)
+
+    @property
+    def query(self):
+        return self._data_type.get('query')
+
+    @property
+    def field(self):
+        return self._data_type.get('field')
+
+    @property
+    def delimiters(self):
+        return self._data_type.get('delimiters')
 
 
-class TimesketchSimilarityScorer(object):
+class SimilarityScorer(object):
 
-    def __init__(self, index, data_type):
-        self._index = index
-        self._data_type = data_type_registry.get(data_type)
-        self.es = ElasticsearchDataStore(
+    def __init__(self, index, data_type, threshold=0.5, num_perm=128):
+        self._datastore = ElasticsearchDataStore(
             host=current_app.config[u'ELASTIC_HOST'],
             port=current_app.config[u'ELASTIC_PORT'])
+        self._threshold = threshold
+        self._num_perm = num_perm
+        self._LSH = MinHashLSH(threshold, num_perm)
+        self._index = index
+        self._data_type_name = data_type
+        self._config = DataTypeConfig(data_type)
 
-    def _generate_shingles(self, text):
-        delimiters = self._data_type.get('delimiters')
+    def _shingles_from_text(self, text):
+        delimiters = self._config.delimiters
         return re.split('|'.join(delimiters), text)
 
-    def _generate_minhash(self, text):
-        minhash = MinHash(num_perm=128)
-        for word in self._generate_shingles(text):
+    def _minhash_from_text(self, text):
+        minhash = MinHash(self._num_perm)
+        for word in self._shingles_from_text(text):
             minhash.update(word.encode('utf8'))
         return minhash
 
-    def _generate_lsh(self):
-        lsh = MinHashLSH(threshold=0.5, num_perm=128)
-        query = self._data_type.get('query')
-        field = self._data_type.get('field')
-        docs = {}
+    def _new_lsh_index(self, event_generator):
+        minhashes = {}
 
-        t1 = datetime.now()
-        for event in self.es.search_stream(
-                query_string=query, query_filter={}, indices=[self._index],
-                return_fields=[field]):
-            doc_id = event['_id']
-            doc_type = event['_type']
-            index_id = event['_index']
-            text = event['_source'][field]
-            doc_key = doc_id + '+' + index_id + '+' + doc_type
+        with self._LSH.insertion_session() as lsh_session:
+            for event in event_generator:
+                event_id = event['_id']
+                index_name = event['_index']
+                event_type = event['_type']
+                event_text = event['_source'][self._config.field]
 
-            # Create LSH index
-            minhash = self._generate_minhash(text)
-            docs[doc_key] = minhash
-            lsh.insert(doc_key, minhash)
+                # Insert minhash in LSH index
+                key = (event_id, event_type, index_name)
+                minhash = self._minhash_from_text(event_text)
+                minhashes[key] = minhash
+                lsh_session.insert(key, minhash)
 
-        t2 = datetime.now()
-        delta = t2 - t1
+        return minhashes
 
-        return lsh, docs, delta.seconds
+    def _score(self, minhash, total_num_events):
+        neighbours = self._LSH.query(minhash)
+        return float(len(neighbours)) / float(total_num_events)
 
-    @staticmethod
-    def _score(lsh, docs):
-        total_num_docs = len(docs)
-        for doc_key, minhash in docs.items():
-            neighbours = lsh.query(minhash)
-            score = float(len(neighbours)) / float(total_num_docs)
-            doc = doc_key.split('+')
-            doc_id = doc[0]
-            doc_index = doc[1]
-            doc_type = doc[2]
-            yield (doc_id, doc_index, doc_type, score)
+    def _event_generator(self):
+        query = self._config.query
+        field = self._config.field
 
-    def run(self):
-        lsh, docs, lsh_seconds = self._generate_lsh()
-        t1 = datetime.now()
-        for event in self._score(lsh, docs):
-            doc_id, doc_index, doc_type, score = event
-            new_doc = {u'similarity_score': score}
-            self.es.import_event(
-                flush_interval=1000, index_name=doc_index, event_type=doc_type,
-                event=new_doc, event_id=doc_id)
-        t2 = datetime.now()
-        delta = t2 - t1
+        return self._datastore.search_stream(
+            query_string=query, query_filter={}, indices=[self._index],
+            return_fields=[field])
+
+    def _update_event(self, event_id, event_type, index_name, score):
+        update_doc = {u'similarity_score': score}
+        self._datastore.import_event(
+            flush_interval=1000, index_name=index_name, event_type=event_type,
+            event_id=event_id, event=update_doc)
+
+    def process(self):
+        event_generator = self._event_generator()
+        minhashes = self._new_lsh_index(event_generator)
+        total_num_events = len(minhashes)
+        for key, minhash in minhashes.items():
+            event_id, event_type, index_name = key
+            score = self._score(minhash, total_num_events)
+            self._update_event(event_id, event_type, index_name, score)
 
         return dict(
-            num_docs=len(docs),
             index=self._index,
-            query=self._data_type.get('query'),
-            hash_seconds=lsh_seconds,
-            score_seconds=delta.seconds
+            data_type=self._data_type_name,
+            num_events_processed=total_num_events
         )
-
-
