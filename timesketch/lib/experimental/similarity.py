2e1c05623eb865617d21ef23c9899596dcd19314
Johan Berggren <jberggren@gmail.com>
2017-12-12 12:20:20 +0100

    Docstrings

--- timesketch/lib/experimental/similarity.py
+++ timesketch/lib/experimental/similarity.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Calculate similarity scores based on the Jaccard distance between events."""
 from __future__ import unicode_literals
 
 import re
@@ -21,6 +22,7 @@ from timesketch.lib.datastores.elastic import ElasticsearchDataStore
 
 
 class SimilarityScorerConfig(object):
+    """Configuration for a similarity scorer."""
 
     # Parameters for Jaccard and Minhash calculations.
     DEFAULT_THRESHOLD = 0.5
@@ -34,6 +36,9 @@ class SimilarityScorerConfig(object):
     }
 
     # For any data_type that need custom config parameters.
+    # TODO: Move this to its own file.
+    # TODO: Add stopwords boolean config parameter.
+    # TODO: Add remove_words boolean config parameter.
     CONFIG_REGISTRY = {
         'windows:evtx:record': {
             'query': 'data_type:"windows:evtx:record"',
@@ -45,53 +50,103 @@ class SimilarityScorerConfig(object):
     }
 
     def __init__(self, data_type, index):
-        config = self._get_config(data_type, index)
-        for key in config:
-            setattr(self, key, config[key])
+        """Initializes a similarity scorer config.
 
-    def _get_config(self, data_type, index):
-        config = self.CONFIG_REGISTRY.get(data_type)
+        Args:
+            index: Elasticsearch index name.
+            data_type: Name of the data_type.
+        """
+        self._data_type = data_type
+        self._index = index
+        for k, v in self._get_config().items():
+            setattr(self, k, v)
+
+    def _get_config(self):
+        """Get config for supplied data_type.
+
+        Returns:
+            Dictionary with configuration parameters.
+        """
+        config_dict = self.CONFIG_REGISTRY.get(self._data_type)
 
         # If there are no config for this data_type, use generic config and set
         # the query based on the data_type.
-        if not config:
-            config = self.DEFAULT_CONFIG
-            config['query'] = 'data_type:"{0}"'.format(data_type)
+        if not config_dict:
+            config_dict = self.DEFAULT_CONFIG
+            config_dict['query'] = 'data_type:"{0}"'.format(self._data_type)
 
-        config['index'] = index
-        config['data_type'] = data_type
-
-        return config
+        config_dict['index'] = self._index
+        config_dict['data_type'] = self._data_type
+        return config_dict
 
 
 class SimilarityScorer(object):
+    """Score events based on Jaccard distance."""
 
     def __init__(self, index, data_type):
+        """Initializes a similarity scorer.
+
+        Args:
+            index: Elasticsearch index name.
+            data_type: Name of the data_type.
+        """
         self._datastore = ElasticsearchDataStore(
             host=current_app.config['ELASTIC_HOST'],
             port=current_app.config['ELASTIC_PORT'])
-        self.config = SimilarityScorerConfig(data_type, index)
+        self._config = SimilarityScorerConfig(index, data_type)
 
     def _shingles_from_text(self, text):
-        delimiters = self.config.delimiters
+        """Splits string into words.
+
+        Args:
+            text: String to extract words from.
+
+        Returns:
+            List of words.
+        """
+        # TODO: Remove stopwords using the NLTK python package.
+        # TODO: Remove configured patterns from string.
+        delimiters = self._config.delimiters
         return re.split('|'.join(delimiters), text)
 
     def _minhash_from_text(self, text):
-        minhash = MinHash(self.config.num_perm)
+        """Calculate minhash of text.
+
+        Args:
+            text: String to calculate minhash of.
+
+        Returns:
+            A minhash (instance of datasketch.minhash.MinHash)
+        """
+        minhash = MinHash(self._config.num_perm)
         for word in self._shingles_from_text(text):
             minhash.update(word.encode('utf8'))
         return minhash
 
-    def _new_lsh_index(self, event_generator):
+    def _new_lsh_index(self):
+        """Create a new LSH from a set of Timesketch events.
+
+        Returns:
+            A tuple with an LSH (instance of datasketch.lsh.LSH) and a
+            dictionary with event ID as key and minhash as value.
+        """
         minhashes = {}
-        lsh = MinHashLSH(self.config.threshold, self.config.num_perm)
+        lsh = MinHashLSH(self._config.threshold, self._config.num_perm)
+
+        # Event generator for streaming Elasticsearch results.
+        events = self._datastore.search_stream(
+            query_string=self._config.query,
+            query_filter={},
+            indices=[self._config.index],
+            return_fields=[self._config.field]
+        )
 
         with lsh.insertion_session() as lsh_session:
-            for event in event_generator:
+            for event in events:
                 event_id = event['_id']
                 index_name = event['_index']
                 event_type = event['_type']
-                event_text = event['_source'][self.config.field]
+                event_text = event['_source'][self._config.field]
 
                 # Insert minhash in LSH index
                 key = (event_id, event_type, index_name)
@@ -102,36 +157,48 @@ class SimilarityScorer(object):
         return lsh, minhashes
 
     @staticmethod
-    def _score(lsh, minhash, total_num_events):
+    def _calculate_score(lsh, minhash, total_num_events):
+        """Calculate a score based on Jaccard distance.
+
+        The score is calculated based on how many similar events that there are
+        for the event being scored. This is called neighbours and we simply
+        calculate how many neighbours the event has devided by the total events
+        in the LSH.
+
+        Returns: A float between 0 and 1.
+
+        """
         neighbours = lsh.query(minhash)
         return float(len(neighbours)) / float(total_num_events)
 
-    def _event_generator(self):
-        return self._datastore.search_stream(
-            query_string=self.config.query,
-            query_filter={},
-            indices=[self.config.index],
-            return_fields=[self.config.field]
-        )
-
     def _update_event(self, event_id, event_type, index_name, score):
+        """Add a similarity_score attribute to the event in Elasticsearch.
+
+        Args:
+            event_id: ID of the Elasticsearch document.
+            event_type: The Elasticsearch type of the event.
+            index_name: The name of the index in Elasticsearch.
+            score: A numerical similarity score with value between 0 and 1.
+        """
         update_doc = {'similarity_score': score}
-        flush_interval = 1000  # Number of events that will be sent in batch
         self._datastore.import_event(
-            flush_interval=flush_interval, index_name=index_name,
-            event_type=event_type, event_id=event_id, event=update_doc)
+            index_name, event_type, event_id=event_id, event=update_doc)
 
     def run(self):
-        event_generator = self._event_generator()
-        lsh, minhashes = self._new_lsh_index(event_generator)
+        """Entry point for a SimilarityScorer.
+
+        Returns:
+            A dict with metadata about the processed data set.
+        """
+        lsh, minhashes = self._new_lsh_index()
         total_num_events = len(minhashes)
         for key, minhash in minhashes.items():
             event_id, event_type, index_name = key
-            score = self._score(lsh, minhash, total_num_events)
+            score = self._calculate_score(lsh, minhash, total_num_events)
             self._update_event(event_id, event_type, index_name, score)
 
         return dict(
-            index=self.config.index,
-            data_type=self.config.data_type,
+            index=self._config.index,
+            data_type=self._config.data_type,
             num_events_processed=total_num_events
         )
