182a5eafabd16cbca777fa52e6a92a175c543b2b
Johan Berggren <jberggren@gmail.com>
2018-01-22 13:36:08 +0100

    Refactor celery tasks

--- timesketch/lib/tasks.py
+++ timesketch/lib/tasks.py
@@ -28,6 +28,40 @@ from timesketch.models.sketch import SearchIndex
 from timesketch.models.sketch import Timeline
 
 celery = create_celery_app()
+flask_app = create_app()
+es = ElasticsearchDataStore(
+    host=current_app.config[u'ELASTIC_HOST'],
+    port=current_app.config[u'ELASTIC_PORT'])
+
+
+def _set_timeline_status(index_name, status, error_msg=None):
+    """Helper function to set searchindex and all related timelines status.
+
+    Args:
+        index_name: Name of the datastore index.
+        status: Status to set.
+        error_msg: Error message.
+    """
+    # Run within Flask context so we can make database changes
+    with flask_app.app_context():
+        searchindex = SearchIndex.query.filter_by(index_name=index_name).first()
+        timelines = Timeline.query.filter_by(searchindex=searchindex).all()
+
+        # Set status
+        searchindex.set_status(status)
+        for timeline in timelines:
+            timeline.set_status(status)
+            db_session.add(timeline)
+
+        # Update description if there was a failure in ingestion
+        if error_msg and status == u'fail':
+            # TODO: Don't overload the description field.
+            searchindex.description = error_msg
+            es.delete_index(index_name)
+
+        # Commit changes to database
+        db_session.add(searchindex)
+        db_session.commit()
 
 
 @celery.task(track_started=True)
@@ -43,8 +77,6 @@ def run_plaso(source_file_path, timeline_name, index_name, username=None):
     Returns:
         String with summary of processed events.
     """
-    app = create_app()
-    fail = False
     cmd = [
         u'psort.py', u'-o', u'timesketch', source_file_path, u'--name',
         timeline_name, u'--status_view', u'none', u'--index', index_name
@@ -53,38 +85,17 @@ def run_plaso(source_file_path, timeline_name, index_name, username=None):
         cmd.append(u'--username')
         cmd.append(username)
 
-    es = ElasticsearchDataStore(
-        host=current_app.config[u'ELASTIC_HOST'],
-        port=current_app.config[u'ELASTIC_PORT'])
-
     # Run psort.py
     try:
         cmd_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
-        fail = True
-        cmd_output = e.output
+        # Mark the searchindex and timelines as failed and exit the task
+        _set_timeline_status(index_name, status=u'fail', error_msg=e.output)
+        logging.error(e.output)
+        return
 
-    # Run within Flask context so we can make database changes.
-    with app.app_context():
-        searchindex = SearchIndex.query.filter_by(index_name=index_name).first()
-        timelines = Timeline.query.filter_by(searchindex=searchindex).all()
-
-        if fail:
-            # Cleanup if psort failed.
-            searchindex.set_status(u'fail')
-            searchindex.description = cmd_output
-            for timeline in timelines:
-                timeline.set_status(u'fail')
-                timeline.description = cmd_output
-                db_session.add(timeline)
-            # Delete index in Elasticsearch.
-            es.delete_index(index_name)
-        else:
-            searchindex.set_status(u'ready')
-
-        # Commit changes to database.
-        db_session.add(searchindex)
-        db_session.commit()
+    # Mark the searchindex and timelines as ready
+    _set_timeline_status(index_name, status=u'ready')
 
     return cmd_output
 
@@ -103,17 +114,12 @@ def run_csv(source_file_path, timeline_name, index_name, username=None):
         Dictionary with count of processed events.
     """
     event_type = u'generic_event'  # Document type for Elasticsearch
-    app = create_app()
 
     # Log information to Celery
     logging.info(u'Index name: %s', index_name)
     logging.info(u'Timeline name: %s', timeline_name)
     logging.info(u'Document type: %s', event_type)
     logging.info(u'Owner: %s', username)
-
-    es = ElasticsearchDataStore(
-        host=current_app.config[u'ELASTIC_HOST'],
-        port=current_app.config[u'ELASTIC_PORT'])
 
     es.create_index(index_name=index_name, doc_type=event_type)
     for event in read_and_validate_csv(source_file_path):
@@ -122,10 +128,8 @@ def run_csv(source_file_path, timeline_name, index_name, username=None):
     # Import the remaining events
     total_events = es.import_event(index_name, event_type)
 
-    # Set status to ready when done.
-    with app.app_context():
-        searchindex = SearchIndex.query.filter_by(index_name=index_name).first()
-        searchindex.set_status(u'ready')
+    # Set status to ready when done
+    _set_timeline_status(index_name, status=u'ready')
 
     return {u'Events processed': total_events}
 
@@ -144,17 +148,12 @@ def run_jsonl(source_file_path, timeline_name, index_name, username=None):
         Dictionary with count of processed events.
     """
     event_type = u'generic_event'  # Document type for Elasticsearch
-    app = create_app()
 
     # Log information to Celery
     logging.info(u'Index name: %s', index_name)
     logging.info(u'Timeline name: %s', timeline_name)
     logging.info(u'Document type: %s', event_type)
     logging.info(u'Owner: %s', username)
-
-    es = ElasticsearchDataStore(
-        host=current_app.config[u'ELASTIC_HOST'],
-        port=current_app.config[u'ELASTIC_PORT'])
 
     es.create_index(index_name=index_name, doc_type=event_type)
     for event in read_and_validate_jsonl(source_file_path):
@@ -163,9 +162,7 @@ def run_jsonl(source_file_path, timeline_name, index_name, username=None):
     # Import the remaining events
     total_events = es.import_event(index_name, event_type)
 
-    # Set status to ready when done.
-    with app.app_context():
-        searchindex = SearchIndex.query.filter_by(index_name=index_name).first()
-        searchindex.set_status(u'ready')
+    # Set status to ready when done
+    _set_timeline_status(index_name, status=u'ready')
 
     return {u'Events processed': total_events}
