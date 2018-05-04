aafd8468970abd2779576c5a20a7a3ff48866619
Johan Berggren <jberggren@gmail.com>
2018-05-04 15:17:27 +0200

    Enable upload by default when using Vagrant (#593)

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -93,16 +93,16 @@ chown "${RUN_AS_USER}" /var/lib/timesketch
 cp "${TIMESKETCH_PATH}"/timesketch.conf /etc/
 
 # Set session key for Timesketch
-sed s/"SECRET_KEY = u'<KEY_GOES_HERE>'"/"SECRET_KEY = u'${SECRET_KEY}'"/ /etc/timesketch.conf > /etc/timesketch.conf.new
-mv /etc/timesketch.conf.new /etc/timesketch.conf
+sed -i s/"SECRET_KEY = u'<KEY_GOES_HERE>'"/"SECRET_KEY = u'${SECRET_KEY}'"/ /etc/timesketch.conf
 
 # Configure the DB password
-sed s/"<USERNAME>:<PASSWORD>@localhost"/"timesketch:${PSQL_PW}@localhost"/ /etc/timesketch.conf > /etc/timesketch.conf.new
-mv /etc/timesketch.conf.new /etc/timesketch.conf
+sed -i s/"<USERNAME>:<PASSWORD>@localhost"/"timesketch:${PSQL_PW}@localhost"/ /etc/timesketch.conf
 
 # Configure the Neo4j password
-sed s/"<N4J_PASSWORD>"/"neo4j"/ /etc/timesketch.conf > /etc/timesketch.conf.new
-mv /etc/timesketch.conf.new /etc/timesketch.conf
+sed -i s/"<N4J_PASSWORD>"/"neo4j"/ /etc/timesketch.conf
+
+# Enable upload
+sed -i s/"UPLOAD_ENABLED = False"/"UPLOAD_ENABLED = True"/ /etc/timesketch.conf
 
 # Copy groovy scripts
 cp "${TIMESKETCH_PATH}"/contrib/*.groovy /etc/elasticsearch/scripts/
