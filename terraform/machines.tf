334013eba160ccefe90d95ccfe1ba9e50ecc092a
Johan Berggren <jberggren@gmail.com>
2018-06-18 08:55:56 +0200

    terraform for elasticsearch

--- terraform/machines.tf
+++ terraform/machines.tf
@@ -2,6 +2,7 @@ resource "google_compute_instance" "timesketch" {
   name         = "timesketch"
   machine_type = "g1-small"
   zone         = "${var.zone}"
+  depends_on = ["google_compute_instance.elasticsearch"]
 
   # Allow to stop/start the machine to enable change machine type.
   allow_stopping_for_update = true
@@ -24,5 +25,40 @@ resource "google_compute_instance" "timesketch" {
 
   # Provision the machine with a script.
   metadata_startup_script = "${data.template_file.timesketch.rendered}"
+}
+
+resource "google_compute_instance" "elasticsearch" {
+  count        = 1
+  name         = "timesketch-esnode-${count.index}"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+
+  # Allow to stop/start the machine to enable change machine type.
+  allow_stopping_for_update = true
+
+  # Use default Ubuntu image as operating system.
+  boot_disk {
+    initialize_params {
+      image = "${var.ubuntu_1604_image}"
+    }
+  }
+
+  # Assign a generated public IP address. Needed for SSH access.
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  # Tag for service enumeration.
+  tags = ["elasticsearch"]
+
+
+  # Enable the GCE discovery module to call required APIs.
+  service_account {
+    scopes = ["compute-ro"]
+  }
+
+  # Provision the machine with a script.
+  metadata_startup_script = "${data.template_file.elasticsearch.rendered}"
 }
 
