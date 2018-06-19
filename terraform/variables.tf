ae241ac14b0b92c73d964c5aec9f43246d2b4cec
Johan Berggren <jberggren@gmail.com>
2018-06-19 10:40:56 +0200

    Refactor

--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "project"  {}
-variable "region"   {}
-variable "zone"     {}
-variable "ubuntu_1604_image" { default = "ubuntu-os-cloud/ubuntu-1604-lts" }
-variable "es_cluster_name" { default = "timesketch" }
