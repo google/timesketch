273a450a6b4c8c45062a3acf144a2d3a50c56b6a
jaegeral <alex@fuemmbf.de>
2018-06-24 10:19:33 +0200

    delimiter

--- timesketch/lib/utils.py
+++ timesketch/lib/utils.py
@@ -69,7 +69,7 @@ def read_and_validate_csv(path, delimiter):
 
             yield row
 
-def read_and_validate_redline(path):
+def read_and_validate_redline(path,delimiter):
     """Generator for reading a Redline CSV file.
     Args:
         path: Path to the file
