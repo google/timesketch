cdbe43b27c4d47d874e882cdfd078a49b24f202d
Kristinn Gudjonsson <kristinn@log2timeline.net>
2018-12-18 16:08:32 +0000

    adding a similarity scorer library.

---
+++ timesketch/lib/similar.py
@@ -0,0 +1,95 @@
+# Copyright 2018 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Similarity scorer."""
+
+from __future__ import unicode_literals
+
+from datasketch.minhash import MinHash
+from datasketch.lsh import MinHashLSH
+
+
+# Parameters for Jaccard and Minhash calculations.
+DEFAULT_THRESHOLD = 0.5
+DEFAULT_PERMUTATIONS = 128
+
+def _shingles_from_text(text):
+    """Splits string into words.
+
+    Args:
+        text: String to extract words from.
+
+    Returns:
+        List of words.
+    """
+    # TODO: Remove stopwords using the NLTK python package.
+    # TODO: Remove configured patterns from string.
+    delimiters = self._config.delimiters
+    return filter(None, re.split('|'.join(delimiters), text))
+
+
+def _minhash_from_text(text):
+    """Calculate minhash of text.
+
+    Args:
+        text: String to calculate minhash of.
+
+    Returns:
+        A minhash (instance of datasketch.minhash.MinHash)
+    """
+    minhash = MinHash(self._config.num_perm)
+    for word in _shingles_from_text(text):
+        minhash.update(word.encode('utf8'))
+    return minhash
+
+
+def new_lsh_index(events):
+    """Create a new LSH from a set of Timesketch events.
+
+    Returns:
+        A tuple with an LSH (instance of datasketch.lsh.LSH) and a
+        dictionary with event ID as key and minhash as value.
+    """
+    minhashes = {}
+    lsh = MinHashLSH(self._config.threshold, self._config.num_perm)
+
+    with lsh.insertion_session() as lsh_session:
+        for event in events:
+            # Insert minhash in LSH index
+            key = (event.event_id, event.event_type, event.index_name)
+            minhash = _minhash_from_text(
+                event.source[self._config.field])
+            minhashes[key] = minhash
+            lsh_session.insert(key, minhash)
+
+    return lsh, minhashes
+
+
+def calculate_score(lsh, minhash, total_num_events):
+    """Calculate a score based on Jaccard distance.
+
+    The score is calculated based on how many similar events that there are
+    for the event being scored. This is called neighbours and we simply
+    calculate how many neighbours the event has divided by the total events
+    in the LSH.
+
+    Args:
+        lsh: Instance of datasketch.lsh.MinHashLSH
+        minhash: Instance of datasketch.minhash.MinHash
+        total_num_events: Integer of how many events in the LSH
+
+    Returns:
+        A float between 0 and 1.
+    """
+    neighbours = lsh.query(minhash)
+    return float(len(neighbours)) / float(total_num_events)
