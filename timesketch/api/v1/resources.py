bc111acfccfdd7ec3a666ca8aaff0afd95afe724
Kristinn Gudjonsson <kristinn@log2timeline.net>
2019-01-09 10:30:05 +0000

    Adding the ability to request return fields in the API.

--- timesketch/api/v1/resources.py
+++ timesketch/api/v1/resources.py
@@ -596,6 +596,13 @@ class ExploreResource(ResourceMixin, Resource):
         if form.validate_on_submit():
             query_dsl = form.dsl.data
             query_filter = form.filter.data
+            return_fields = form.fields.data
+
+            print 'FORM DATA IS: {}'.format(return_fields)
+
+            if not return_fields:
+                return_fields = DEFAULT_SOURCE_FIELDS
+
             sketch_indices = {
                 t.searchindex.index_name
                 for t in sketch.timelines
@@ -622,7 +629,7 @@ class ExploreResource(ResourceMixin, Resource):
                 query_dsl,
                 indices,
                 aggregations=None,
-                return_fields=DEFAULT_SOURCE_FIELDS,
+                return_fields=return_fields,
                 enable_scroll=False)
 
             # Get labels for each event that matches the sketch.
