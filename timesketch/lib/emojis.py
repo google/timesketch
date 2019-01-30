04df926c40632b46082165f89385e3006d23ac5a
Ryan Benson <ryan@obsidianforensics.com>
2019-01-30 09:31:17 -0800

    Extract Usernames or Account Email Addresses (#745) (#795)
    * Try to make TS find the analyzer
    
    * Add feature extractor for Gmail addresses
    
    * First pass at adapting the feature extractor analyzer into the account finder analyzer.
    
    * Expanding to more than one account search pattern
    
    * Moving account features back to features.yaml
    
    * Testing getting event attributes
    
    * Make dependent on feature_extraction
    
    * Add emoji (and clean-up)
    
    * Removed unused imports and old .yaml file.
    
    * Implementing feedback from code review.
    
    * Make string splitting prettier

--- timesketch/lib/emojis.py
+++ timesketch/lib/emojis.py
@@ -27,6 +27,7 @@ emoji = collections.namedtuple('emoji', 'code help')
 EMOJI_MAP = {
     'CAMERA': emoji('&#x1F4F7', 'Screenshot activity'),
     'FISHING_POLE': emoji('&#x1F3A3', 'Phishing'),
+    'ID_BUTTON': emoji('&#x1F194', 'Account ID'),
     'LOCK': emoji('&#x1F512', 'Logon activity'),
     'LOCOMOTIVE': emoji('&#x1F682', 'Execution activity'),
     'MAGNIFYING_GLASS': emoji('&#x1F50E', 'Search related activity'),
