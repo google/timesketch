5cbee54bb1d2052ca65982502eb65100d0cb8167
kovakina <33467545+kovakina@users.noreply.github.com>
2019-02-18 23:25:53 -0800

    Update potential_bruteforce.py

--- timesketch/lib/analyzers/potential_bruteforce.py
+++ timesketch/lib/analyzers/potential_bruteforce.py
@@ -1,18 +1,31 @@
 """Sketch analyzer plugin for potential bruteforce."""
 from __future__ import unicode_literals
 
+from timesketch.lib import emojis
 from timesketch.lib.analyzers import interface
 from timesketch.lib.analyzers import manager
 
+import logging
+import re
+import sys
+
 
 class PotentialBruteforceSketchPlugin(interface.BaseSketchAnalyzer):
-    """Sketch analyzer for PotentialBruteforce."""
+    """Sketch analyzer for Potential Bruteforce."""
 
     NAME = 'potential_bruteforce'
+	
+
+	
+	# List of common logins
+	COMMON_NAMES = [
+    "admin",
+	"user",
+	"test",
+	"root"]
 
     def __init__(self, index_name, sketch_id):
         """Initialize The Sketch Analyzer.
-
         Args:
             index_name: Elasticsearch index name
             sketch_id: Sketch ID
@@ -20,10 +33,11 @@ class PotentialBruteforceSketchPlugin(interface.BaseSketchAnalyzer):
         self.index_name = index_name
         super(PotentialBruteforceSketchPlugin, self).__init__(
             index_name, sketch_id)
+	
+
 
     def run(self):
         """Entry point for the analyzer.
-
         Returns:
             String with summary of the analyzer result
         """
@@ -32,7 +46,7 @@ class PotentialBruteforceSketchPlugin(interface.BaseSketchAnalyzer):
                  'AND body:"Invalid user")')
 
         # TODO: Specify what returned fields you need for your analyzer.
-        return_fields = ['message', 'data_type', 'source_short', 'human_readable']
+        return_fields = ['message', 'data_type', 'source_short']
 
         # Generator of events based on your query.
         events = self.event_stream(
@@ -51,18 +65,28 @@ class PotentialBruteforceSketchPlugin(interface.BaseSketchAnalyzer):
         login_count = 0
 
         for event in events:
-        # Fields to analyze.
          data_type = event.source.get('data_type')
          source_short = event.source.get('source_short')
          message = event.source.get('message')
-         human_readable = event.source.get('human_readable')
+		 ip_address = re.findall(r'\b25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\b',message)
+		        if ip_address is None:
+                continue
+		 username = re.match()
+		 		if username is None:
+                continue
+		 for username in self._COMMON_NAMES:
+		   event.add_tags('common_name
+		   ')
+		  event.add_attributes({})
 
-        if login_count:
-         self.sketch.add_view('Potential bruteforce', query_string=query)
+        if login_count > 0:
+         self.sketch.add_view(
+		 view_name='Potential bruteforce', analyzer_name=self.NAME,
+		 query_string=query)
 
         # TODO: Return a summary from the analyzer.
-        return 'Potential bruteforce analyzer completed, {0:d} login attempts from unknown users found'.format(
-    login_count)
+        return 'Potential bruteforce analyzer completed, {0:d} login attempts from {1:d} unknown users and {2:d} IPs found'.format(
+    login_count, user_count, ip_count)
 
 
 
