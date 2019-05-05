81242e5795eb0d1c52d8ab4fc1e27862a483dadc
Joachim Metz <joachim.metz@gmail.com>
2019-05-05 14:59:51 +0200

    Update dependencies (#881)

--- config/travis/runtests.sh
+++ config/travis/runtests.sh
@@ -11,6 +11,7 @@ set -e;
 if test -n "${UBUNTU_VERSION}";
 then
 	CONTAINER_NAME="ubuntu${UBUNTU_VERSION}";
+	CONTAINER_OPTIONS="-e LANG=en_US.UTF-8";
 
 	if test "${TARGET}" = "pylint";
 	then
@@ -18,11 +19,12 @@ then
 
 	elif test ${TRAVIS_PYTHON_VERSION} = "2.7";
 	then
-		TEST_COMMAND="nosetests";
+		TEST_COMMAND="./config/travis/run_python2.sh";
 	else
-		TEST_COMMAND="nosetests3";
+		TEST_COMMAND="./config/travis/run_python3.sh";
 	fi
-	docker exec ${CONTAINER_NAME} sh -c "export LANG=en_US.UTF-8; cd timesketch && ${TEST_COMMAND}";
+	# Note that exec options need to be defined before the container name.
+	docker exec ${CONTAINER_OPTIONS} ${CONTAINER_NAME} sh -c "cd timesketch && ${TEST_COMMAND}";
 
 elif test "${TRAVIS_OS_NAME}" = "linux";
 then
