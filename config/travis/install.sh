7d12f9d94a24b7dbc07a8100aebbfee69c0e14e8
Johan Berggren <jberggren@gmail.com>
2019-06-12 15:26:58 +0200

    Docker

--- config/travis/install.sh
+++ config/travis/install.sh
@@ -61,7 +61,7 @@ then
 	docker exec -e "DEBIAN_FRONTEND=noninteractive" ${CONTAINER_NAME} sh -c "apt-get install -y ${DPKG_PACKAGES}";
 
 	docker cp ../timesketch ${CONTAINER_NAME}:/
-	docker cp ./timesketch/data/timesketch.conf ${CONTAINER_NAME}:/etc/
+	docker cp ./data/timesketch.conf ${CONTAINER_NAME}:/etc/
 
 	docker exec ${CONTAINER_NAME} sh -c "cd timesketch && yarn install";
 
