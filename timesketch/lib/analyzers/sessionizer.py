ad30e1f512f40fa754e47b76b19b5ed57a1ff407
Antonina Uskova <auskova@google.com>
2019-08-06 15:33:33 +0000

    Fixed naming

--- timesketch/lib/analyzers/sessionizer.py
+++ timesketch/lib/analyzers/sessionizer.py
@@ -13,6 +13,7 @@ class SessionizerSketchPlugin(interface.BaseSketchAnalyzer):
     them is less or equal then max_time_diff_micros"""
 
     NAME = 'sessionizer'
+    # TODO max_time_diff_micros should be configurable
     max_time_diff_micros = 300000000
 
     def __init__(self, index_name, sketch_id):
