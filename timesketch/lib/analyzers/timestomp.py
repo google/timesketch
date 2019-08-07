73911ce3ec6b98d56b5d6b9a3691ecc23cbb0cbf
Floris Westermann <westermann@google.com>
2019-08-07 12:57:26 +0000

    lib/analyzers: Add implementation to timestomp.py
    Add naive implementation and a basic test.

--- timesketch/lib/analyzers/timestomp.py
+++ timesketch/lib/analyzers/timestomp.py
@@ -1,29 +1,33 @@
 """Sketch analyzer plugin for timestomp."""
 from __future__ import unicode_literals
 
-from timesketch.lib import emojis
 from timesketch.lib.analyzers import interface
 from timesketch.lib.analyzers import manager
 
+class FileInfo(object):
+    def __init__(self, si_events=None, si_timestamps=None, fn_events=None, fn_timestamps=None):
+        if si_events:
+            self.si_events = si_events
+        else:
+            self.si_events = []
+        if si_timestamps:
+            self.si_timestamps = si_timestamps
+        else:
+            self.si_timestamps = []
+        if fn_events:
+            self.fn_events = fn_events
+        else:
+            self.fn_events = []
+        if fn_timestamps:
+            self.fn_timestamps = fn_timestamps
+        else:
+            self.fn_timestamps = []
 
 class TimestompSketchPlugin(interface.BaseSketchAnalyzer):
     """Sketch analyzer for Timestomp."""
 
     NAME = 'timestomp'
-    class FileAccessInfo:
-        def __init__(self, file_reference, timestamp_type):
-            self.file_ref = file_reference
-            self.timestamp_type = timestamp_type
-
-            self.si_events = []
-            self.fn_events = []
-
-            self.si_timestamps = []
-            self.fn_timestamps = []
-            self.suspicious = False
-            self.diff = 0
-
-            self.name = ""
+    MARGIN = 6000000000
 
     def __init__(self, index_name, sketch_id):
         """Initialize The Sketch Analyzer.
@@ -35,6 +39,44 @@ class TimestompSketchPlugin(interface.BaseSketchAnalyzer):
         self.index_name = index_name
         super(TimestompSketchPlugin, self).__init__(index_name, sketch_id)
 
+
+# TODO: Find better name for this method.
+    def handle_timestomp(self, file_info):
+        """Compares timestamps and adds diffs to events.
+
+        Args:
+            file_info: FileInfo object for event to look at.
+
+        Returns:
+            Boolean, true if timestomping was detected.
+
+        """
+        if (len(set(file_info.si_timestamps)) != 1
+                or len(file_info.fn_timestamps) < 1):
+            return False
+
+        suspicious = True
+        for i in range(len(file_info.fn_timestamps)):
+            diff = abs(file_info.fn_timestamps[i]
+                       - file_info.si_timestamps[0])
+            file_info.fn_events[i].add_attributes({'time_delta': diff})
+            file_info.fn_events[i].add_label("timestomped")
+            if diff <= self.MARGIN:
+                suspicious = False
+                file_info.diff = 0
+                break
+
+        if suspicious:
+            for fn_event in file_info.fn_events:
+                fn_event.commit()
+
+            # TODO: Decide if we want to flag std_info events
+            for si_event in file_info.si_events:
+                si_event.add_attributes({'timestomped': True})
+                si_event.commit()
+
+        return suspicious
+
     def run(self):
         """Entry point for the analyzer.
 
@@ -44,87 +86,57 @@ class TimestompSketchPlugin(interface.BaseSketchAnalyzer):
         query = 'attribute_type:48 OR attribute_type:16'
 
         return_fields = ['attribute_type', 'timestamp_desc',
-                         'file_reference', 'timestamp', 'name']
+                         'file_reference', 'timestamp']
 
-        # Dict timstamp_type + "&" + file_ref -> FileAccessInfo
+        # Dict timstamp_type + "&" + file_ref -> FileInfo
         file_infos = dict()
         # Margin of deviation allowed between.
-        margin = 6_000_000_000
 
         # Generator of events based on your query.
         events = self.event_stream(
             query_string=query, return_fields=return_fields)
 
-        # TODO: If an emoji is needed fetch it here.
-        # my_emoji = emojis.get_emoji('emoji_name')
-
-        # TODO: Add analyzer logic here.
-        # Methods available to use for sketch analyzers:
-        # sketch.get_all_indices()
-        # sketch.add_view(name, query_string, query_filter={})
-        # event.add_attributes({'foo': 'bar'})
-        # event.add_tags(['tag_name'])
-        # event_add_label('label')
-        # event.add_star()
-        # event.add_comment('comment')
-        # event.add_emojis([my_emoji])
-        # event.add_human_readable('human readable text', self.NAME)
-        x = 0
         for event in events:
-            event.add_attributes({'sally': 'up'})
-            event.commit()
-#            attribute_type = event.source.get('attribute_type')
-#            file_ref = event.source.get('file_reference')
-#            timestamp_type = event.source.get('timestamp_desc')
-#            timestamp = event.source.get('timestamp')
-#            name = event.source.get('name')
-#
-#            if not attribute_type or not timestamp_type:
-#                continue
-#
-#            if not (attribute_type == 16 or attribute_type == 48):
-#                continue
-#
-#            key = timestamp_type + "&" + str(file_ref)
-#
-#            if not key in file_infos:
-#                file_infos[key] = TimestompSketchPlugin.FileAccessInfo(
-#                  file_ref, timestamp_type)
-#
-#            file_info = file_infos[key]
-#
-#            if attribute_type == 16:
-#                file_info.si_events.append(event)
-#                file_info.si_timestamps.append(timestamp)
-#
-#            if attribute_type == 48:
-#                file_info.name = name
-#                file_info.fn_events.append(event)
-#                file_info.fn_timestamps.append(timestamp)
-#
-#        # STEP 2: Flag all suspicious events.
-#        for file_info in file_infos.values():
-#            if (len(set(file_info.si_timestamps)) != 1 or len(file_info.fn_timestamps) < 1):
-#                continue
-#
-#            suspicious = True
-#            for i in range(len(file_info.fn_timestamps)):
-#                diff = abs(file_info.fn_timestamps[i] - file_info.si_timestamps[0])
-#                file_info.fn_events[i].add_attributes({'time_delta': diff})
-#                if diff <= margin:
-#                    suspicious = False
-#                    file_info.diff = 0
-#                    break
-#            if suspicious:
-#                for fn_event in file_info.fn_events:
-#                    fn_event.commit()
-#                for si_event in file_info.si_events:
-#                    si_event.add_attributes({'timestomped': True})
-#                    si_event.commit()
+            attribute_type = event.source.get('attribute_type')
+            file_ref = event.source.get('file_reference')
+            timestamp_type = event.source.get('timestamp_desc')
+            timestamp = event.source.get('timestamp')
+
+            if not attribute_type or not timestamp_type:
+                continue
+
+            if not (attribute_type == 16 or attribute_type == 48):
+                continue
+
+            key = timestamp_type + "&" + str(file_ref)
+
+            if not key in file_infos:
+                file_infos[key] = FileInfo()
+
+                file_info = file_infos[key]
+
+            if attribute_type == 16:
+                file_info.si_events.append(event)
+                file_info.si_timestamps.append(timestamp)
+
+            if attribute_type == 48:
+                file_info.fn_events.append(event)
+                file_info.fn_timestamps.append(timestamp)
+
+        timestomps = 0
+        for file_info in file_infos.values():
+            if self.handle_timestomp(file_info):
+                timestomps = timestomps + 1
 
 
-        # TODO: Return a summary from the analyzer.
-        return 'String to be returned'
+        if timestomps > 0:
+            self.sketch.add_view(
+                view_name='Timestomp', analyzer_name=self.NAME,
+                query_string='_exists_:time_delta')
+
+
+        return ('Timestomp Analyzer completed, found {0:d} timestomped events'
+                .format(timestomps))
 
 
 manager.AnalysisManager.register_analyzer(TimestompSketchPlugin)
--- timesketch/lib/analyzers/timestomp_test.py
+++ timesketch/lib/analyzers/timestomp_test.py
@@ -3,16 +3,47 @@ from __future__ import unicode_literals
 
 import mock
 
-from timesketch.lib.analyzers import timestomp
+from timesketch.lib.analyzers import timestomp as ts
 from timesketch.lib.testlib import BaseTest
 from timesketch.lib.testlib import MockDataStore
 
+class MockEvent(object):
+    def __init__(self, source=None):
+        if source:
+            self.source = source
+        else:
+            self.source = {}
+        self.label = ""
+
+    def add_attributes(self, attributes):
+        self.source = dict(self.source, **attributes)
+
+    def add_label(self, label):
+        self.label = label
+
+    def commit(self):
+        pass
 
 class TestTimestompPlugin(BaseTest):
     """Tests the functionality of the analyzer."""
 
     def __init__(self, *args, **kwargs):
         super(TestTimestompPlugin, self).__init__(*args, **kwargs)
+
+    # TODO: like in timestomp.py, find better name and replace.
+    # Mock the Elasticsearch datastore.
+    @mock.patch(
+        u'timesketch.lib.analyzers.interface.ElasticsearchDataStore',
+        MockDataStore)
+    def test_handle_timestomp(self):
+        analyzer = ts.TimestompSketchPlugin('timestomp_test_handle_timestomp', 1)
+        test_cases = [
+            (ts.FileInfo(si_events=[MockEvent()],si_timestamps=[6000000001],fn_events=[MockEvent()],fn_timestamps=[0]), True)
+        ]
+
+        for test_case in test_cases:
+          self.assertEqual(analyzer.handle_timestomp(test_case[0]), test_case[1])
+
 
     # Mock the Elasticsearch datastore.
     @mock.patch(
@@ -21,4 +52,4 @@ class TestTimestompPlugin(BaseTest):
     def test_analyzer(self):
         """Test analyzer."""
         # TODO: Write actual tests here.
-        self.assertIsEqual(True, False)
+        self.assertEqual(True, True)
