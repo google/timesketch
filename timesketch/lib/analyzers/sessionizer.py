aa1b8035c80eb468b77e6e26f009d2adf662d52d
Kate Macleod <katemacleod@google.com>
2019-08-29 15:41:48 +0200

    Pylint fixes

--- timesketch/lib/analyzers/sessionizer.py
+++ timesketch/lib/analyzers/sessionizer.py
@@ -53,7 +53,7 @@ class SessionizerSketchPlugin(interface.BaseSketchAnalyzer):
 
         return ('Sessionizing completed, number of session created:'
                 ' {0:d}'.format(session_num))
-      
+
     def addSessionId(self, event, session_num):
         """Annotate an event with a session ID. Store IDs as dictionary entries
         corresponding to the type of session.
