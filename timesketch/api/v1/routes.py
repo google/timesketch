eb6e84517dec63cdeff0ab5da5f495c23351945c
Kristinn <kristinn@log2timeline.net>
2019-10-23 10:33:17 +0000

    Adding the ability to manually run an analyzer in the API (#998)
    * First steps toward adding the ability to manually run a sketch.
    
    * Fixing the API resources.
    
    * Adding support into API client.
    
    * minor linter issue
    
    * Minor adjustments to client and better error reporting in API.
    
    * Making changes after comments.
    
    * minor client changes
    
    * minor linter
    
    * Adding to a docstring to trigger travis.
    
    * linter update

--- timesketch/api/v1/routes.py
+++ timesketch/api/v1/routes.py
@@ -19,6 +19,7 @@ from .resources import AggregationListResource
 from .resources import AggregationLegacyResource
 from .resources import AggregationExploreResource
 from .resources import AggregationResource
+from .resources import AnalyzerRunResource
 from .resources import ExploreResource
 from .resources import EventResource
 from .resources import EventAnnotationResource
@@ -51,6 +52,7 @@ from .resources import SessionResource
 API_ROUTES = [
     (SketchListResource, '/sketches/'),
     (SketchResource, '/sketches/<int:sketch_id>/'),
+    (AnalyzerRunResource, '/sketches/<int:sketch_id>/analyzer/'),
     (AggregationListResource, '/sketches/<int:sketch_id>/aggregation/'),
     (AggregationLegacyResource, '/sketches/<int:sketch_id>/aggregation/legacy/'),
     (AggregationExploreResource, '/sketches/<int:sketch_id>/aggregation/explore/'),
