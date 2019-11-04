6e7ac91a32fb9166d28faa8176c9c70a5681e0eb
Kristinn Gudjonsson <kristinn@log2timeline.net>
2019-11-04 12:11:13 +0000

    New client version.

--- api_client/python/setup.py
+++ api_client/python/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name='timesketch-api-client',
-    version='20191028',
+    version='20191104',
     description='Timesketch API client',
     license='Apache License, Version 2.0',
     url='http://www.timesketch.org/',
