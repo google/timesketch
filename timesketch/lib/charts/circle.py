a2e4d7d3c022c0e0f74d1267f334d07255309498
Kristinn Gudjonsson <kristinn@log2timeline.net>
2020-03-31 20:40:59 +0000

    Forgot to update one thing

--- timesketch/lib/charts/circle.py
+++ timesketch/lib/charts/circle.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Circles."""
+"""Circle chart class.."""
 
 from __future__ import unicode_literals
 
