e15a33ad909ec06978735343c91b867cf30b2e66
Kristinn Gudjonsson <kristinn@log2timeline.net>
2020-05-25 10:47:05 +0000

    Docstring change.

--- api_client/python/timesketch_api_client/story.py
+++ api_client/python/timesketch_api_client/story.py
@@ -665,12 +665,12 @@ class Story(resource.BaseResource):
         _ = self.blocks
 
     def to_html(self):
-        """Return a HTML formatted string with the content of the story."""
+        """Returns HTML formatted string with the content of the story."""
         story_dict = self.to_export_format('html')
         return story_dict.get('story', '')
 
     def to_markdown(self):
-        """Return a markdown formatted string with the content of the story."""
+        """Returns markdown formatted string with the content of the story."""
         story_dict = self.to_export_format('markdown')
         return story_dict.get('story', '')
 
