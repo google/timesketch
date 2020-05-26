89b1cc37a236dc51a2a2b928c9bbc1603be15d5d
Joachim Metz <joachim.metz@gmail.com>
2020-05-26 07:36:45 +0200

    Changed setup.py to not use pip internal-only API

--- setup.py
+++ setup.py
@@ -25,15 +25,10 @@ import glob
 import os
 import sys
 
+from pkg_resources import parse_requirements
 from setuptools import find_packages
 from setuptools import setup
 
-try:  # for pip >= 10
-    from pip._internal.download import PipSession
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.download import PipSession
-    from pip.req import parse_requirements
 
 version_tuple = (sys.version_info[0], sys.version_info[1])
 if version_tuple < (3, 5):
@@ -79,9 +74,9 @@ setup(
     zip_safe=False,
     entry_points={'console_scripts': ['tsctl=timesketch.tsctl:main']},
     install_requires=[str(req.req) for req in parse_requirements(
-        'requirements.txt', session=PipSession(),
+        'requirements.txt',
     )],
     tests_require=[str(req.req) for req in parse_requirements(
-        'test_requirements.txt', session=PipSession(),
+        'test_requirements.txt',
     )],
 )
