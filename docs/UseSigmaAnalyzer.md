1f721cf55e18410125afecf2563156753b4320bf
Alexander J <741037+jaegeral@users.noreply.github.com>
2020-06-24 10:27:52 +0200

    Add support for Sigma rules (#1231)
    * full sigma commit
    
    * updated git submod
    
    * Apply suggestions from code review
    
    Co-authored-by: Johan Berggren <jberggren@gmail.com>
    
    * change sigma rules from submodule to a git clone that has do be done by
    TS server admin maunally.
    Updating the .gitignore alongside
    
    * remove a leftover print and remove additional fields as this was also
    not needed
    
    * code readability
    
    * remove not needed exception
    
    * s/BROWSER_STORY_HEADER/SIGMA_STORY_HEADER
    
    * widen the try block to catch even more sigma issues
    
    * propper OR coverage for aspects
    
    * improve tag coverage mapping
    
    * introduce exception for timeouts for to many searches
    
    * remove comliance path
    
    * adressing: https://github.com/google/timesketch/issues/1199#issuecomment-639475885 and https://github.com/google/timesketch/issues/1199#issuecomment-639472176
    
    * increase mapping coverage
    
    Co-authored-by: Johan Berggren <jberggren@gmail.com>

---
+++ docs/UseSigmaAnalyzer.md
@@ -0,0 +1,85 @@
+# Sigma Analyser in Timesketch
+
+## What is Sigma
+
+See description at the Sigma Github repository: https://github.com/Neo23x0/sigma#what-is-sigma
+
+## Sigma in Timesketch
+
+Since early 2020 Timesketch has Sigma support implemented. Sigma can be used as an analyser.
+
+### Install rules
+
+Timesketch deliberatly does not provide a set of Sigma rules, as those would add complexity to maintain.
+Instead we recommend to clone https://github.com/Neo23x0/sigma to /data/sigma.
+This directory will not be catched by git. 
+                                                                                                     
+
+```
+cd data
+git clone https://github.com/Neo23x0/sigma
+```
+
+The rules then will be under
+```
+timesketch/data/sigma
+```
+
+### Sigma Rules
+
+The windows rules are stored in 
+```
+timesketch/data/sigma/rules/windows
+```
+
+The linux rules are stored in
+```
+timesketch/data/linux
+timesketch/data/sigma/rules/linux
+```
+
+### Sigma config
+
+In the config file
+```
+sigma_config.yaml
+```
+
+There is a section with mappings, most mappings where copied from HELK configuration.
+If you find a mapping missing, feel free to add and create a PR.
+
+###Field Mapping
+Some adjustments verified:
+
+- s/EventID/event_identifier
+- s/Source/source_name
+
+## Adding a new Operating System to support Sigma TS
+
+To adda  new operating system or new set of rules, adjust the following file:
+```
+timesketch/timesketch/analyzers/sigma_tagger.py
+```
+
+In that file, specify, where your rules are stored and how they should appear in the Timesketch UI.
+
+## Testing new rules
+
+There is a folder in the data directory that has .gitignore content for createing new rules locally.
+ 
+Folder:
+```
+timesketch/data/test_rules 
+```
+
+Which will show up in Timesketch UI as *a_sigma_test* on top of the analyzers.
+
+Note: if you create new rules, you need to restart your celery worker to pick them up.
+
+## Test data
+
+If you want to test that feature, get some evtx files from the following
+ links and parse it via plaso
+
+- https://github.com/sbousseaden/EVTX-ATTACK-SAMPLES
+- https://github.com/sans-blue-team/DeepBlueCLI/evtx
--- timesketch/lib/analyzers/sigma_tagger.py
+++ timesketch/lib/analyzers/sigma_tagger.py
@@ -3,10 +3,15 @@ from __future__ import unicode_literals
 
 import logging
 import os
+import time
+import elasticsearch
+
 
 from sigma.backends import elasticsearch as sigma_elasticsearch
 import sigma.configuration as sigma_configuration
 from sigma.parser import collection as sigma_collection
+from timesketch.lib.analyzers import utils
+
 
 
 from timesketch.lib.analyzers import interface
@@ -23,7 +28,6 @@ class SigmaPlugin(interface.BaseSketchAnalyzer):
     # Path to the directory containing the Sigma Rules to run, relative to
     # this file.
     _RULES_PATH = ''
-
 
     def __init__(self, index_name, sketch_id):
         """Initialize the Index Analyzer.
@@ -55,7 +59,7 @@ class SigmaPlugin(interface.BaseSketchAnalyzer):
         events = self.event_stream(
             query_string=query, return_fields=return_fields)
         for event in events:
-            event.add_tags([tag_name])
+            event.add_tags(['sigma_{0:s}'.format(tag_name)])
             event.commit()
             tagged_events += 1
         return tagged_events
@@ -70,47 +74,128 @@ class SigmaPlugin(interface.BaseSketchAnalyzer):
             self.sigma_config, {})
         tags_applied = {}
 
-        rules_path = os.path.join(os.path.dirname(__file__), self._RULES_PATH)
-        for rule_filename in os.listdir(rules_path):
-            tag_name, _ = rule_filename.rsplit('.')
-            tags_applied[tag_name] = 0
-            rule_file_path = os.path.join(rules_path, rule_filename)
-            rule_file_path = os.path.abspath(rule_file_path)
-            logging.info('[sigma] Reading rules from {0!s}'.format(
-                rule_file_path))
-            with open(rule_file_path, 'r') as rule_file:
-                rule_file_content = rule_file.read()
-                parser = sigma_collection.SigmaCollectionParser(
-                    rule_file_content, self.sigma_config, None)
-                try:
-                    results = parser.generate(sigma_backend)
-                except NotImplementedError as exception:
-                    logging.error(
-                        'Error generating rule in file {0:s}: {1!s}'.format(
-                            rule_file_path, exception))
-                    continue
+        simple_counter = 0
 
-                for result in results:
-                    logging.info(
-                        '[sigma] Generated query {0:s}'.format(result))
-                    number_of_tagged_events = self.run_sigma_rule(
-                        result, tag_name)
-                    tags_applied[tag_name] += number_of_tagged_events
+        rules_path = os.path.join(os.path.dirname(__file__), self._RULES_PATH)
+
+
+        for dirpath, dirnames, files in os.walk(rules_path):
+
+            if 'deprecated' in dirnames:
+                dirnames.remove('deprecated')
+
+            for rule_filename in files:
+                if rule_filename.lower().endswith('yml'):
+
+                    # if a sub dir is found, append it to be scanned for rules
+                    if os.path.isdir(os.path.join(rules_path, rule_filename)):
+                        logging.error(
+                            'this is a directory, not a file, skipping: {0:s}'.format(
+                                rule_filename))
+                        continue
+
+                    tag_name, _ = rule_filename.rsplit('.')
+                    tags_applied[tag_name] = 0
+                    rule_file_path = os.path.join(dirpath, rule_filename)
+                    rule_file_path = os.path.abspath(rule_file_path)
+                    logging.info('[sigma] Reading rules from {0!s}'.format(
+                        rule_file_path))
+                    with open(rule_file_path, 'r') as rule_file:
+                        try:
+                            rule_file_content = rule_file.read()
+                            parser = sigma_collection.SigmaCollectionParser(
+                            rule_file_content, self.sigma_config, None)
+                            parsed_sigma_rules = parser.generate(sigma_backend)
+                        except NotImplementedError as exception:
+                            logging.error(
+                                'Error generating rule in file {0:s}: {1!s}'.format(
+                                    rule_file_path, exception))
+                            continue
+                        except Exception as exception:
+                            logging.error(
+                                'Error generating rule in file {0:s}: {1!s}'.format(
+                                    rule_file_path, exception))
+                            continue
+
+                        for sigma_rule in parsed_sigma_rules:
+                            try:
+                                simple_counter += 1
+                                # TODO fix that in Sigma hack to get rid of nested stuff
+                                # https://github.com/google/timesketch/issues/1199#issuecomment-639475885
+                                sigma_rule = sigma_rule.replace(".keyword:", ":")
+                                logging.info(
+                                    '[sigma] Generated query {0:s}'.format(sigma_rule))
+                                number_of_tagged_events = self.run_sigma_rule(
+                                    sigma_rule, tag_name)
+                                tags_applied[tag_name] += number_of_tagged_events
+                            except elasticsearch.TransportError as es_TransportError:
+                                logging.error(
+                                    'Timeout generating rule in file {0:s}: {1!s} waiting for 10 seconds'.format(
+                                        rule_file_path, es_TransportError))
+                                time.sleep(10) # waiting 5 seconds before continue
 
         total_tagged_events = sum(tags_applied.values())
         output_string = 'Applied {0:d} tags\n'.format(total_tagged_events)
         for tag_name, number_of_tagged_events in tags_applied.items():
-            output_string += '* {0:s}: {1:d}'.format(
+            output_string += '* {0:s}: {1:d}\n'.format(
                 tag_name, number_of_tagged_events)
+
+        if simple_counter > 0:
+            view = self.sketch.add_view(
+                view_name='Sigma Rule matches', analyzer_name=self.NAME,
+                query_string='tag:"sigma*"')
+            agg_params = {
+                'field': 'tag',
+                'limit': 20,
+            }
+            agg_obj = self.sketch.add_aggregation(
+                name='Top 20 Sigma tags', agg_name='field_bucket',
+                agg_params=agg_params, view_id=view.id, chart_type='hbarchart',
+                description='Created by the Sigma analyzer')
+
+
+            story = self.sketch.add_story("Sigma Rule hits")
+            story.add_text(
+                utils.SIGMA_STORY_HEADER, skip_if_exists=True)
+
+            story.add_text(
+                '## Sigma Analyzer.\n\nThe Sigma '
+                'analyzer takes Events and matches them with Sigma rules.'
+                'In this timeline the analyzer discovered {0:d} '
+                'Sigma tags.\n\nThis is a summary of '
+                'it\'s findings.'.format(simple_counter))
+            story.add_text(
+                'The top 20 most commonly discovered tags were:')
+            story.add_aggregation(agg_obj)
+            story.add_text(
+                'And an overview of all the discovered search terms:')
+            story.add_view(view)
+
+
         return output_string
 
 
 class LinuxRulesSigmaPlugin(SigmaPlugin):
     """Sigma plugin to run Linux rules."""
 
-    _RULES_PATH = '../../../data/linux'
+    _RULES_PATH = '../../../data/sigma/rules/linux'
 
     NAME = 'sigma_linux'
 
+class WindowsRulesSigmaPlugin(SigmaPlugin):
+    """Sigma plugin to run Windows rules."""
+
+    _RULES_PATH = '../../../data/sigma/rules/windows'
+
+    NAME = 'sigma_windows'
+
+class TestRulesSigmaPlugin(SigmaPlugin):
+    """Sigma plugin to run Windows rules."""
+
+    _RULES_PATH = '../../../data/test_rules'
+
+    NAME = 'sigma_test'
 
 manager.AnalysisManager.register_analyzer(LinuxRulesSigmaPlugin)
+manager.AnalysisManager.register_analyzer(WindowsRulesSigmaPlugin)
+manager.AnalysisManager.register_analyzer(TestRulesSigmaPlugin)
