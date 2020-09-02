c6dbdd7b88f2ee00059deee7f06ef9ca7ac5d555
Johan Berggren <jberggren@gmail.com>
2020-09-03 00:17:50 +0200

    Sleep before test

--- test_tools/e2e/run_in_container.py
+++ test_tools/e2e/run_in_container.py
@@ -1,6 +1,11 @@
 from end_to_end_tests import manager as test_manager
 
+import time
+
 manager = test_manager.EndToEndTestManager()
+
+print("sleeping..")
+time.sleep(60)
 for test in manager.get_tests():
     t = test[1]()
     t.run_wrapper()
