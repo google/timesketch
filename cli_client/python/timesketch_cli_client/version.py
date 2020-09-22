5bd3e26d6f1a6d008f12b4bfdb7cfb4b7fdbf6f2
Johan Berggren <jberggren@gmail.com>
2020-09-22 08:10:13 +0200

    Cleanup

--- cli_client/python/timesketch_cli_client/version.py
+++ cli_client/python/timesketch_cli_client/version.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Version information for Timesketch CLI client."""
+"""Version information for the Timesketch CLI client."""
 
 __version__ = '20200921'
 
