c6696c0a99bd29be5c1beca6aebe4e9e24cc110f
jaegeral <mail@alexanderjaeger.de>
2020-10-05 13:18:19 +0000

    linter

--- test_tools/sigma_verify_rules.py
+++ test_tools/sigma_verify_rules.py
@@ -223,3 +223,4 @@ if __name__ == '__main__':
         logging.info('### You can import the following rules ###')
         for goodrule in sigma_verified_rules:
             logging.info(goodrule)
+            
