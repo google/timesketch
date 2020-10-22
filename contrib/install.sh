4708fcd601dbb92629dbb3aa4cc11843afd00ef0
Johan Berggren <jberggren@gmail.com>
2020-10-22 11:39:54 +0200

    refactor

---
+++ contrib/install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+set -e
+
+# Exit early if a timesketch directory already exists.
+if [ -d "./timesketch" ]; then
+  echo "ERROR: Timesketch directory already exist."
+  echo 1
+fi
+
+# Exit early if docker is not available.
+if [ "$(systemctl is-active docker)" != "active" ]; then
+  echo "ERROR: Docker is not running. Either start the service or install the package."
+  echo "See: https://docs.docker.com/engine/install/ubuntu/"
+  exit 1
+fi
+
+# Exit early if there are Timesketch containers already running.
+if [ ! -z "$(docker ps | grep timesketch)" ]; then
+  echo "ERROR: Timesketch containers already running, exit."
+  exit 1
+fi
+
+# Tweak for Elasticsearch
+sysctl -w vm.max_map_count=262144
+if [ -z "$(grep vm.max_map_count /etc/sysctl.conf)" ]
+  echo "Setting vm.max_map_count to 262144"
+  echo "vm.max_map_count=262144" >> /etc/sysctl.conf
+fi
+
+# Create dirs
+mkdir -p timesketch/{data/postgresql,data/elasticsearch,logs,etc,etc/timesketch,etc/timesketch/sigma/rules,docker,upload}
+
+POSTGRES_USER="timesketch"
+POSTGRES_PASSWORD="$(< /dev/urandom tr -dc A-Za-z0-9 | head -c 32 ; echo)"
+POSTGRES_ADDRESS="postgres"
+POSTGRES_PORT=5432
+SECRET_KEY="$(< /dev/urandom tr -dc A-Za-z0-9 | head -c 32 ; echo)"
+ELASTIC_ADDRESS="elasticsearch"
+ELASTIC_PORT=9200
+REDIS_ADDRESS="redis"
+REDIS_PORT=6379
+GITHUB_BASE_URL="https://raw.githubusercontent.com/google/timesketch/master"
+
+# Docker compose and configuration
+curl -s $GITHUB_BASE_URL/docker/release/docker-compose.yml > timesketch/docker-compose.yml
+curl -s $GITHUB_BASE_URL/docker/release/config.env > timesketch/config.env
+
+# Fetch default Timesketch config files
+curl -s $GITHUB_BASE_URL/data/timesketch.conf > timesketch/etc/timesketch/timesketch.conf
+curl -s $GITHUB_BASE_URL/data/tags.yaml > timesketch/etc/timesketch/tags.yaml
+curl -s $GITHUB_BASE_URL/data/features.yaml > timesketch/etc/timesketch/features.yaml
+curl -s $GITHUB_BASE_URL/data/sigma_config.yaml > timesketch/etc/timesketch/sigma_config.yaml
+curl -s $GITHUB_BASE_URL/data/sigma/rules/lnx_susp_zenmap.yml > timesketch/etc/timesketch/sigma/rules/lnx_susp_zenmap.yml
+curl -s $GITHUB_BASE_URL/contrib/nginx.conf > timesketch/etc/nginx.conf
+
+# Create a minimal Timesketch config
+# Set SECRET_KEY for signing cookies and XSRF protection
+sed -i 's#SECRET_KEY = \x27\x3CKEY_GOES_HERE\x3E\x27#SECRET_KEY = \x27'$SECRET_KEY'\x27#' timesketch/etc/timesketch/timesketch.conf
+
+# Set up the Elastic connection
+sed -i 's#^ELASTIC_HOST = \x27127.0.0.1\x27#ELASTIC_HOST = \x27'$ELASTIC_ADDRESS'\x27#' timesketch/etc/timesketch/timesketch.conf
+sed -i 's#^ELASTIC_PORT = 9200#ELASTIC_PORT = '$ELASTIC_PORT'#' timesketch/etc/timesketch/timesketch.conf
+
+# Set up the Redis connection
+sed -i 's#^UPLOAD_ENABLED = False#UPLOAD_ENABLED = True#' timesketch/etc/timesketch/timesketch.conf
+sed -i 's#^CELERY_BROKER_URL =.*#CELERY_BROKER_URL = \x27redis://'$REDIS_ADDRESS':'$REDIS_PORT'\x27#' timesketch/etc/timesketch/timesketch.conf
+sed -i 's#^CELERY_RESULT_BACKEND =.*#CELERY_RESULT_BACKEND = \x27redis://'$REDIS_ADDRESS':'$REDIS_PORT'\x27#' timesketch/etc/timesketch/timesketch.conf
+
+# Set up the Postgres connection
+sed -i 's#postgresql://<USERNAME>:<PASSWORD>@localhost#postgresql://'$POSTGRES_USER':'$POSTGRES_PASSWORD'@'$POSTGRES_ADDRESS':'$POSTGRES_PORT'#' timesketch/etc/timesketch/timesketch.conf
+
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
