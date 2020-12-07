18d14ece07d6eb0cafc662fa777941f3605e6947
Kristinn Gudjonsson <kristinn@log2timeline.net>
2020-12-07 08:12:01 +0000

    linter

--- docker/dev/build/10-widgets.py
+++ docker/dev/build/10-widgets.py
@@ -16,31 +16,31 @@
 These are temporary functions until they have been generalized
 and implemented in picatrix.
 """
+# pylint: disable=undefined-variable
 import ipywidgets as widgets
 
 
 # TODO: Generalize and move to picatrix.
 def generate_connect_button(click_function = None):
-  """Creates a button and an int form to connect to timesketch."""
-  button = widgets.Button(description='Connect to sketch')
-  output = widgets.Output()
+    """Creates a button and an int form to connect to timesketch."""
+    button = widgets.Button(description='Connect to sketch')
+    output = widgets.Output()
 
-  sketch_field = widgets.IntText(
-      value=1,
-      description='Sketch ID:',
-      disabled=False
-  )
+    sketch_field = widgets.IntText(
+        value=1,
+        description='Sketch ID:',
+        disabled=False
+    )
 
-  display(sketch_field, button, output)
+    display(sketch_field, button, output)
 
-  def _click_function(b):
-      with output:
-          timesketch_set_active_sketch_func(str(sketch_field.value))
-          sketch = timesketch_get_sketch_func()
-          print(f'Connected to sketch: {sketch.id}:{sketch.name}')
+    def _click_function(_):
+        with output:
+            timesketch_set_active_sketch_func(str(sketch_field.value))
+            sketch = timesketch_get_sketch_func()
+            print(f'Connected to sketch: {sketch.id}:{sketch.name}')
 
-  if click_function:
-    button.on_click(click_function)
-  else:
-    button.on_click(_click_function)
-
+    if click_function:
+        button.on_click(click_function)
+    else:
+        button.on_click(_click_function)
