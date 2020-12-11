c79b7ee04af179108cdcdc1a9466b8937373143f
Kristinn Gudjonsson <kristinn@log2timeline.net>
2020-12-11 14:01:02 +0000

    Updating code

--- api_client/python/timesketch_api_client/graph.py
+++ api_client/python/timesketch_api_client/graph.py
@@ -12,11 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Timesketch API graph object."""
-import datetime
 import json
 import logging
 
+import dateutil.parser
 import pandas
+import networkx as nx
 
 from . import error
 from . import resource
@@ -28,7 +29,6 @@ logger = logging.getLogger('timesketch_api.graph')
     (GraphListResource, '/sketches/<int:sketch_id>/graphs/'),
     (GraphResource, '/sketches/<int:sketch_id>/graphs/<int:graph_id>/'),
     (GraphPluginListResource, '/graphs/'),
-    (GraphCacheResource, '/sketches/<int:sketch_id>/graph/')
 """
 
 class Graph(resource.SketchResource):
@@ -38,25 +38,25 @@ class Graph(resource.SketchResource):
         resource_uri = f'sketches/{sketch.id}/graphs/'
         super().__init__(sketch=sketch, resource_uri=resource_uri)
 
-        self._sketch_id = sketch.id
-
-        self._created_at = ''
+        self._created_at = None
         self._description = ''
-        self._max_entries = self.DEFAULT_SIZE_LIMIT
         self._name = ''
-        self._query_dsl = ''
-        self._query_filter = {}
-        self._query_string = ''
-        self._raw_response = None
-        self._return_fields = ''
-        self._scrolling = None
-        self._searchtemplate = ''
-        self._updated_at = ''
+        self._updated_at = None
 
     @property
     def created_at(self):
         """Property that returns back the creation time of a search."""
+        if self._created_at:
+            return self._created_at.isoformat()
         return self._created_at
+
+    @property
+    def graph(self):
+        """Property that returns back a graph object."""
+        if not self.resource_data:
+            return nx.Graph()
+
+        return nx.cytoscape_graph(self.resource_data)
 
     def delete(self):
         """Deletes the saved graph from the store."""
@@ -100,12 +100,29 @@ class Graph(resource.SketchResource):
             RuntimeError: if the query is missing needed values, or if the
                 sketch is archived.
         """
-        super().from_manual(**kwargs)
         # TODO: Implement
+        super().from_manual(**kwargs)
+        self._resource_id = 0
+        self.resource_data = {}
 
-    def from_cache(self, plugin_name, plugin_config=None, refresh=False):
-        """Initialize the graph from a cached graph."""
-        resource_url = f'{self.api.api_root}/sketches/{self._sketch_id}/graph/'
+
+    def from_plugin(self, plugin_name, plugin_config=None, refresh=False):
+        """Initialize the graph from a cached plugin graph.
+
+        Args:
+            plugin_name (str): the name of the graph plugin to use.
+            plugin_config (dict): optional dictionary to configure the plugin.
+            refresh (bool): optional bool that if set refreshes the graph,
+                otherwise the graph is pulled from the cache, if it exists.
+                Defaults to False, meaning it pulls from the cache if it
+                exists.
+        """
+        plugin_names = [x.get('name', '') for x in self.plugins]
+        if plugin_name.lower() not in plugin_names:
+            raise ValueError(
+                f'Plugin [{plugin_name}] not part of the supported plugins.')
+
+        resource_url = f'{self.api.api_root}/sketches/{self._sketch.id}/graph/'
         data = {
             'plugin': plugin_name,
             'config': plugin_config,
@@ -120,7 +137,20 @@ class Graph(resource.SketchResource):
 
         response_json = error.get_response_json(response, logger)
         cache_dict = response_json.get('objects', [{}])[0]
-        # TODO: Complete this implementation.
+
+        graph_string = cache_dict.get('graph_elements', '')
+
+        try:
+            self.resource_data = json.loads(graph_string)
+        except json.JSONDecodeError as exc:
+            raise ValueError('Unable to read graph data') from exc
+
+        self._created_at = dateutil.parser.parse(
+            cache_dict.get('created_at', ''))
+        self._updated_at = dateutil.parser.parse(
+            cache_dict.get('updated_at', ''))
+        self._description = f'Graph created from the {plugin_name} plugin.'
+
         return cache_dict
 
     def from_saved(self, graph_id):  # pylint: disable=arguments-differ
@@ -131,6 +161,16 @@ class Graph(resource.SketchResource):
                 search (primary key).
         """
         # TODO: Implement.
+
+
+    def from_graph(self, graph_obj):
+        """Initialize from a networkx graph object."""
+        if not graph_obj.size():
+            logger.warning('Unable to load graph from an empty graph object.')
+            return
+        self.resource_data = nx.cytoscape_data(graph_obj)
+        self._name = ''
+        self._description = 'From a graph object.'
 
     @property
     def name(self):
@@ -143,6 +183,17 @@ class Graph(resource.SketchResource):
         self._name = name
         self.commit()
 
+    @property
+    def plugins(self):
+        """Property that returns back the supported plugins."""
+        plugin_dict = self.api.fetch_resource_data('graphs/')
+        return plugin_dict
+
+    @property
+    def plugins_table(self):
+        """Property that returns a dataframe with the available plugins."""
+        return pandas.DataFrame(self.plugins)
+
     def save(self):
         """Save the search in the database.
 
@@ -151,48 +202,42 @@ class Graph(resource.SketchResource):
         """
         if not self.name:
             raise ValueError(
-                'No name for the query saved. Please select a name first.')
+                'No name for the graph. Please select a name first.')
 
         if not self.description:
             logger.warning(
-                'No description selected for search, saving without one')
+                'No description selected for graph, saving without one')
 
         if self._resource_id:
+            # pylint: disable=unused-variable
+            # TODO: Remove this when completed implemention.
             resource_url = (
-                f'{self.api.api_root}/sketches/{self._sketch.id}/views/'
+                f'{self.api.api_root}/sketches/{self._sketch.id}/graphs/'
                 f'{self._resource_id}/')
         else:
             resource_url = (
-                f'{self.api.api_root}/sketches/{self._sketch.id}/views/')
+                f'{self.api.api_root}/sketches/{self._sketch.id}/graphs/')
 
         return False
-        # TODO: Implement this.
-        data = {
-            'name': self.name,
-            'description': self.description,
-            'query': self.query_string,
-            'filter': self.query_filter,
-            'dsl': self.query_dsl,
-            'labels': json.dumps(self.labels),
-        }
-        response = self.api.session.post(resource_url, json=data)
-        status = error.check_return_status(response, logger)
-        if not status:
-            error.error_message(
-                response, 'Unable to save search', error=RuntimeError)
-
-        response_json = error.get_response_json(response, logger)
-        search_dict = response_json.get('objects', [{}])[0]
-        self._resource_id = search_dict.get('id', 0)
-        return f'Saved search to ID: {self._resource_id}'
 
     def to_dict(self):
-        """Returns a dict with the respone of the query."""
-        if not self._raw_response:
-            self._execute_query()
-
-        return self._raw_response
+        """Returns a dict with the graph content."""
+        return self.resource_data
 
     def to_pandas(self):
-        """The interface requires this to be implemented."""
-        return pandas.DataFrame()
+        """Returns a pandas dataframe with the graph content."""
+        if not self.resource_data:
+            return pandas.DataFrame()
+
+        data = self.resource_data
+        elements = data.get('elements', {})
+        nodes = elements.get('nodes', [])
+        edges = elements.get('edges', [])
+
+        node_list = [x.get('data') for x in nodes]
+        edge_list = [x.get('data') for x in edges]
+        node_df = pandas.DataFrame(node_list)
+        node_df['type'] = 'node'
+        edge_df = pandas.DataFrame(edge_list)
+        edge_df['type'] = 'edge'
+        return pandas.concat([node_df, edge_df])
