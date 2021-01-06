d7956ab445deac9c0e844698989c3d4f20ded46e
Johan Berggren <jberggren@gmail.com>
2021-01-06 16:00:29 +0100

    fix

--- api_client/python/timesketch_api_client/sketch.py
+++ api_client/python/timesketch_api_client/sketch.py
@@ -855,11 +855,11 @@ class Sketch(resource.BaseResource):
         if timeline_id is None and timeline_name is None:
             return None
 
-        for _timeline in self.list_timelines():
-            if timeline_id and timeline_id == _timeline.id:
-                return _timeline
-            if timeline_name and timeline_name.lower() == _timeline.name.lower():
-                return _timeline
+        for timeline in self.list_timelines():
+            if timeline_id and timeline_id == timeline.id:
+                return timeline
+            if timeline_name and timeline_name.lower() == timeline.name.lower():
+                return timeline
         return None
 
     def list_stories(self):
