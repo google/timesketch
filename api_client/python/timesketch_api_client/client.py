0de492de754abb0c2ee516e11c3d4c656d38ba41
jaegeral <mail@alexanderjaeger.de>
2021-01-15 10:37:53 +0000

    make the get method more flexible

--- api_client/python/timesketch_api_client/client.py
+++ api_client/python/timesketch_api_client/client.py
@@ -555,13 +555,12 @@ class TimesketchApi:
             # this way it initialices the objects, but does
             #  not pull the data for each
             index_obj = sigma.Sigma(api=self)
-            index_obj.rule_uuid = rule_dict.get('id')
-            index_obj.rule_uuid = rule_dict.get('id')
-            index_obj.title = rule_dict.get('title')
-            index_obj.es_query = rule_dict.get('es_query')
-            index_obj.file_name = rule_dict.get('file_name')
-            index_obj.description = rule_dict.get('description')
-            index_obj.file_relpath = rule_dict.get('file_relpath')
+            index_obj.rule_uuid = rule_dict.get('id', '')
+            index_obj.title = rule_dict.get('title', '')
+            index_obj.es_query = rule_dict.get('es_query', '')
+            index_obj.file_name = rule_dict.get('file_name', '')
+            index_obj.description = rule_dict.get('description', '')
+            index_obj.file_relpath = rule_dict.get('file_relpath', '')
             rules.append(index_obj)
         return rules
 
