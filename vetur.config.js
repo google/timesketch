9ad5dc656eb344a50dc449783a4ac6ac85cb5aa0
Johan Berggren <jberggren@gmail.com>
2021-02-15 23:57:58 +0100

    ESlint

--- vetur.config.js
+++ vetur.config.js
@@ -1,23 +1,13 @@
 // vetur.config.js
 /** @type {import('vls').VeturConfig} */
 module.exports = {
-    // **optional** default: `{}`
-    // override vscode settings
-    // Notice: It only affects the settings used by Vetur.
     settings: {
       "vetur.useWorkspaceDependencies": true,
       "vetur.experimental.templateInterpolationService": false
     },
     projects: [
       {
-        // **required**
-        // Where is your project?
-        // It is relative to `vetur.config.js`.
         root: './timesketch/frontend',
-        // **optional** default: `'package.json'`
-        // Where is `package.json` in the project?
-        // We use it to determine the version of vue.
-        // It is relative to root property.
         package: './package.json',
       }
     ]
