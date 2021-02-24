95f6621369db812bf6b3251639dbf8e3dda47865
Kristinn <kristinn@log2timeline.net>
2021-02-24 13:26:46 +0000

    Update sketch.py

--- api_client/python/timesketch_api_client/sketch.py
+++ api_client/python/timesketch_api_client/sketch.py
@@ -36,6 +36,7 @@ logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO'))
 logger = logging.getLogger('timesketch_api.sketch')
 
 
+#pylint: disable=use-a-generator
 class Sketch(resource.BaseResource):
     """Timesketch sketch object.
 
