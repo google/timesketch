234a0fc04f294f99ef48d756571eeb1432321018
Rayan Hatout <rayan.hatout@gmail.com>
2021-02-26 11:42:58 +0100

    Replaced the use of the csv library in import task to use pandas (for csv/json data ingestion) (#1534)

--- timesketch/lib/utils_test.py
+++ timesketch/lib/utils_test.py
@@ -20,6 +20,12 @@ import re
 from timesketch.lib.testlib import BaseTest
 from timesketch.lib.utils import get_validated_indices
 from timesketch.lib.utils import random_color
+from timesketch.lib.utils import read_and_validate_csv
+
+TEST_CSV = "test_tools/test_events/sigma_events.csv"
+ISO8601_REGEX = r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[' \
+                r'1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][' \
+                r'0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$'
 
 
 class TestUtils(BaseTest):
@@ -37,9 +43,21 @@ class TestUtils(BaseTest):
 
         valid_indices = ['test']
         invalid_indices = ['test', 'fail']
-
         test_indices, _ = get_validated_indices(valid_indices, sketch)
         self.assertListEqual(sketch_indices, test_indices)
 
         test_indices, _ = get_validated_indices(invalid_indices, sketch)
         self.assertFalse('fail' in test_indices)
+
+    def test_header_validation(self):
+        """Test for Timesketch header validation."""
+        mandatory_fields = ['message', 'datetime', 'fortytwo']
+        with self.assertRaises(RuntimeError):
+            # Call next to work around lazy generators.
+            next(read_and_validate_csv(TEST_CSV, ',', mandatory_fields))
+
+    def test_date_normalisation(self):
+        """Test for ISO date compliance."""
+        data_generator = read_and_validate_csv(TEST_CSV)
+        for row in data_generator:
+            self.assertRegex(row['datetime'], ISO8601_REGEX)
