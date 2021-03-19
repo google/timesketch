a25e583ea9bf649e480a6fceeed477a0bb3d217a
Kristinn Gudjonsson <kristinn@log2timeline.net>
2021-03-19 15:28:00 +0000

    lint

--- timesketch/migrations/versions/75af34d75b1e_extending_the_data_source_schema.py
+++ timesketch/migrations/versions/75af34d75b1e_extending_the_data_source_schema.py
@@ -5,6 +5,9 @@ Revises: 41cae2c10cd7
 Create Date: 2021-03-19 13:18:17.575912
 
 """
+# This code is auto generated. Ignore linter errors.
+# pylint: skip-file
+
 
 # revision identifiers, used by Alembic.
 revision = '75af34d75b1e'
