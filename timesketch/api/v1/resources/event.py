5dfbfde5f34fa95922dfcb6035385840f12abea3
Kristinn Gudjonsson <kristinn@log2timeline.net>
2021-04-15 12:13:17 +0000

    update

--- timesketch/api/v1/resources/event.py
+++ timesketch/api/v1/resources/event.py
@@ -744,9 +744,28 @@ class MarkEventsWithTimelineIdentifier(resources.ResourceMixin, Resource):
                 'The sketch ID ({0:d}) does not match with the timeline '
                 'sketch ID ({1:d})'.format(sketch.id, timeline.sketch.id))
 
+        # TODO REMOVE THIS ONCE FIXED
+        """
         query_dsl = {
             'stript': {
-                'inline': f'ctx._source.__ts_timeline_id="{timeline_id}";',
+                'inline': f'ctx._source.__ts_timeline_id={timeline_id};',
+                'lang': 'painless'
+            },
+            'query': {
+                'bool': {
+                    'must_not': {
+                        'exists': {
+                            'field': '__ts_timeline_id',
+                        }
+                    }
+                }
+            }
+        }
+        """
+
+        query_dsl = {
+            'stript': {
+                'source': f'ctx._source.__ts_timeline_id={timeline_id};',
                 'lang': 'painless'
             },
             'query': {
