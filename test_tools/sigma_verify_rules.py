7b2bf8ac039767622002c3f8ce9c69206dfa762d
Alexander Jaeger <alexander@jaeger.org>
2021-05-05 18:09:51 +0200

    feedback

--- test_tools/sigma_verify_rules.py
+++ test_tools/sigma_verify_rules.py
@@ -161,7 +161,7 @@ def move_problematic_rule(filepath, move_to_path, reason=None):
         base_path = os.path.basename(filepath)
         logging.info('Moving the rule: {0:s} to {1:s}'.format(
         filepath, f'{move_to_path}{base_path}'))
-        os.rename(filepath, f'{move_to_path}{base_path}')
+        os.rename(filepath, os.path.join(move_to_path,base_path))
     except OSError:
         logger.error('OS Error - rule not moved', exc_info=True)
 
