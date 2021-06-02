b4e253e7c76e9c36a2d35c19325e1f43da8dd7b1
Kristinn Gudjonsson <kristinn@log2timeline.net>
2021-06-02 20:00:49 +0000

    adding option to re-enter information/confirm choices in Importer client.

--- importer_client/python/tools/timesketch_importer.py
+++ importer_client/python/tools/timesketch_importer.py
@@ -199,6 +199,12 @@ def main(args=None):
             'SSH then it is recommended to set this option. When set an '
             'authentication URL is prompted on the screen, requiring a '
             'copy/paste into a browser to complete the OAUTH dance.'))
+    auth_group.add_argument(
+        '--re-prompt', '--re_prompt', action='store_true', dest='re_prompt',
+        help=(
+            'If for some reasons you type the wrong username, password '
+            'or host information you can use this parameter to re-enter '
+            'those mistyped values.'))
 
     config_group = argument_parser.add_argument_group(
         'Configuration Arguments')
@@ -391,7 +397,8 @@ def main(args=None):
 
     # Gather all questions that are missing.
     config.configure_missing_parameters(
-        config_assistant=assistant, token_password=token_password)
+        config_assistant=assistant, token_password=token_password,
+        confirm_choices=options.re_prompt)
 
     logger.info('Creating a client.')
     ts_client = assistant.get_client(token_password=token_password)
