37a81e75203ff31a3fd31028fa4887806fec4fd1
Kristinn Gudjonsson <kristinn@log2timeline.net>
2021-06-03 15:05:53 +0000

    update e2e test

--- end_to_end_tests/data_finder_test.py
+++ end_to_end_tests/data_finder_test.py
@@ -44,7 +44,7 @@ class DataFinderTest(interface.BaseEndToEndTest):
         result_dict = results[0]
         self.assertions.assertTrue(result_dict.get('test_data_finder')[0])
         self.assertions.assertTrue(
-            result_dict.get('test_data_finder')[1].startswith('Hits Disc'))
+            result_dict.get('test_data_finder')[1].startswith('Data Disc'))
 
 
 manager.EndToEndTestManager.register_test(DataFinderTest)
