d4cb3ee1626a5b9ab9248702fb5a572da711ca04
Kristinn Gudjonsson <kristinn@log2timeline.net>
2021-06-30 12:25:34 +0000

    continue

--- timesketch/api/v1/resources/question.py
+++ timesketch/api/v1/resources/question.py
@@ -15,6 +15,7 @@
 
 import os
 import logging
+import time
 import uuid
 
 import yaml
@@ -24,6 +25,7 @@ from flask import jsonify
 from flask import request
 from flask import abort
 from flask_restful import Resource
+from flask_login import current_user
 from flask_login import login_required
 
 from timesketch.api.v1 import resources
@@ -75,6 +77,13 @@ class QuestionListResource(resources.ResourceMixin, Resource):
 
 class QuestionResource(resources.ResourceMixin, Resource):
     """Resource for asking a question within a Sketch."""
+
+    # Number of seconds to wait between checking whether analyzers have
+    # finished running.
+    ANALYZER_WAIT_PERIOD = 5
+
+    # Maximum number of seconds to wait until all analyzers complete.
+    MAXIMUM_WAIT_SECONDS = 600
 
     @login_required
     def post(self, sketch_id):
@@ -131,9 +140,14 @@ class QuestionResource(resources.ResourceMixin, Resource):
 
         timeline_ids_read = form.get('timeline_ids', [])
         timeline_ids = []
+        indices_struct = {}
         for timeline in sketch.active_timelines:
             if timeline.id in timeline_ids_read:
                 timeline_ids.append(timeline.id)
+                indices_struct.setdefault(
+                    f'{timeline.searchindex.id}|'
+                    f'{timeline.searchindex.index_name}', set())
+                indices_struct[timeline.searchindex.id].add(timeline.id)
 
         parameters = form.get('parameters')
         if parameters and not isinstance(parameters, dict):
@@ -169,36 +183,125 @@ class QuestionResource(resources.ResourceMixin, Resource):
                 }
                 return jsonify(schema)
 
-        # TODO: Use variables.
-        # pylint: disable=unused-variable
         analyzers = question.get('analyzers', [])
-        # TODO: Run analyzers.
+        max_wait_seconds = form.get(
+            'maximum_wait_seconds', self.MAXIMUM_WAIT_SECONDS)
+        if analyzers:
+            _, errors = self._run_analyzers(
+                indices_struct=indices_struct, analyzers=analyzers,
+                sketch_id=sketch.id, max_wait_seconds=max_wait_seconds,
+                get_answer=False)
 
         answer_source = question.get('answer_source', {})
         answer_musts = answer_source.get('must', [])
+
         results = self._run_data_finder_pipeline(
-            data_sources=data_sources, sketch=sketch,
+            data_sources=answer_musts, sketch=sketch,
             start_date=start_date, end_date=end_date,
             timeline_ids=timeline_ids, parameters=parameters)
+        print(results)
 
         answer_must_not = answer_source.get('must_not', [])
-        # TODO: Run all answer data sources.
+        results = self._run_data_finder_pipeline(
+            data_sources=answer_must_not, sketch=sketch,
+            start_date=start_date, end_date=end_date,
+            timeline_ids=timeline_ids, parameters=parameters)
+        print(results)
 
-        answer_analyzer = question.get('graph_plugins', [])
-        # TODO: Run the answer analyzer.
+        answer_analyzer = question.get('answer_analyzer', '')
+        answer = None  # TODO: Fix this, results should be all encompassing.
+        if answer_analyzer:
+            answer, errors = self._run_analyzers(
+                indices_struct=indices_struct,
+                analyzers=[answer_analyzer], sketch_id=sketch_id,
+                max_wait_seconds=max_wait_seconds, get_answer=True)
 
         graph_plugins = question.get('graph_plugins', [])
-        searches = question.get('graph_plugins', [])
+        searches = question.get('searches', [])
         schema = {
             'meta': {
                 'data_available': True,
                 'question_asked': question_name,
                 'graph_plugins': graph_plugins,
                 'searches': searches,
+                'errors': errors,
             },
-            'objects': [results],
+            'objects': [results, answer],
         }
         return jsonify(schema)
+
+    def _run_analyzers(
+            self, indices_struct, analyzers, sketch_id, max_wait_seconds,
+            get_answer=False):
+        """Run analyzers and finish writing this docstring."""
+        # Import here to avoid circular imports.
+        # pylint: disable=import-outside-toplevel
+        from timesketch.lib import tasks
+
+        sessions = []
+        pipelines = []
+        for index_string, timeline_ids in indices_struct.items():
+            index_id_str, _, index_name = index_string.partition('|')
+            index_id = int(index_id_str)
+
+            for timeline_id in timeline_ids:
+                try:
+                    group, session = tasks.build_sketch_analysis_pipeline(
+                        sketch_id=sketch_id, searchindex_id=index_id,
+                        user_id=current_user.id, analyzer_names=analyzers,
+                        timeline_id=timeline_id)
+                except KeyError:
+                    logger.warning(
+                        'Unable to build analyzer pipeline, analyzer does not '
+                        'exists.', exc_info=True)
+                    continue
+
+                if group:
+                    pipeline = (tasks.run_sketch_init.s(
+                        [index_name]) | group)
+                    pipeline.apply_async()
+                    sessions.append(session)
+                    if get_answer:
+                        pipelines.append(pipeline)
+
+        errors = []
+
+        # Wait until analyzers are complete.
+        # Since many of the questions rely on analyzers to run before
+        # attempting to answer them, we need to make sure the analyzers
+        # have successfully completed before moving on to the next step.
+        if sessions:
+            completed = False
+            total_wait = 0
+            while not completed:
+                completed = True
+                for session in sessions:
+                    for analyzer in session.analyses:
+                        if analyzer.get_status.status not in ('DONE', 'ERROR'):
+                            completed = False
+
+                # Sleep for few seconds to allow analyzer to complete
+                # it's run.
+                time.sleep(self.ANALYZER_WAIT_PERIOD)
+                total_wait += self.ANALYZER_WAIT_PERIOD
+
+                if total_wait >= max_wait_seconds:
+                    logger.error(
+                        'Unable to wait for analyzers to complete, answers '
+                        'may be un-reliable.')
+                    errors.append(
+                        'Unable to wait for analyzers to complete, took '
+                        'too long')
+                    break
+
+        if get_answer:
+            results = []
+            for pipeline in pipelines:
+                result = pipeline.delay()
+                results.append(result.join())
+            return results, errors
+
+        return None, errors
 
     def _run_data_finder_pipeline(
             self, data_sources, sketch, start_date, end_date, timeline_ids,
