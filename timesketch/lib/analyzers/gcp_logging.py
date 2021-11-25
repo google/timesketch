f951beb777aaff9a80723082d1c7be28ba958e2f
Jonathan Greig <jonathangreig@google.com>
2021-11-25 14:06:59 +1100

    Init new analyzer outline

---
+++ timesketch/lib/analyzers/gcp_logging.py
@@ -0,0 +1,29 @@
+"""Sketch analyzer plugin for GCP Service Key usage."""
+from __future__ import unicode_literals
+
+from timesketch.lib.analyzers import interface
+from timesketch.lib.analyzers import manager
+
+
+class GcpLoggingSketchPlugin(interface.BaseAnalyzer):
+    """Analyzer for GCP Logging"""
+
+    NAME = 'gcp_logging'
+    DISPLAY_NAME = 'Google Cloud Logging Analyzer'
+    DESCRIPTION = ('Extract accounts and resources from cloud logging and tag'
+        ' security relevant actions.')
+
+    def run(self):
+        """Entry point for the analyzer.
+
+        Returns:
+            String with summary of the analyzer result
+        """
+        # TODO: update dftimewolf stackdriver module to produce more detailed
+        # attributes
+
+        return ('GCP logging analyzer completed with '
+                '{0:d} service key marked').format(0)
+
+
+manager.AnalysisManager.register_analyzer(GcpLoggingSketchPlugin)
