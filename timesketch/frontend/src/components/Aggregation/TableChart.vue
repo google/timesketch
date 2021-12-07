a7c6855cf6004cae3e1336faccfac82f5a042bc7
lprat <lionel.prat9@gmail.com>
2021-12-07 12:46:26 +0100

    Add filter regexp on terms and number of result to display

--- timesketch/frontend/src/components/Aggregation/TableChart.vue
+++ timesketch/frontend/src/components/Aggregation/TableChart.vue
@@ -14,25 +14,75 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 <template>
-  <div v-if="tableData.length">
-    <b-table
-      :data="tableData"
-      :columns="getColumns()"
-      :paginated="true"
-      :pagination-simple="true"
-      :per-page="10"
-      :hoverable="true"
-      icon-pack="fas"
-      pagination-size="is-small"
-    >
-    </b-table>
-  </div>
+  <section>
+    <div v-if="tableData.length">
+      <b-field grouped group-multiline>
+        <filterb-field label="Items per page">
+          <b-input type="number" v-model="perPage"></b-input>
+        </b-field>
+        <b-field label="Filter">
+          <b-input v-model="filter" type="text" placeholder="Search regexp">
+          </b-input>
+        </b-field>
+        <b-field label="Filter Not">
+          <b-input v-model="filternot" type="text" placeholder="Search regexp">
+          </b-input>
+        </b-field>
+      </b-field>
+      <b-table
+        :data="getData()"
+        :columns="getColumns()"
+        :paginated="true"
+        :pagination-simple="true"
+        :per-page="perPage"
+        :filter="filter"
+        :hoverable="true"
+        icon-pack="fas"
+        pagination-size="is-small"
+      >
+      </b-table>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      perPage: 10,
+      filter: null,
+      filternot: null,
+    }
+  },
   props: ['tableData'],
   methods: {
+    getData: function() {
+      let datafilter = []
+      let filter_ok = ''
+      let filter_not = ''
+      if (this.filter != null) {
+        filter_ok = this.filter
+      }
+      if (this.filternot != null) {
+        filter_not = this.filternot
+      }
+      this.tableData.forEach(function(item, array) {
+        Object.keys(item).forEach(function(key) {
+          if (key != 'count' &&
+            key != 'duration' &&
+            key != 'last' &&
+            key != 'first' &&
+            (new RegExp(filter_ok)).test(String(item[key]))) {
+            if (filter_not == '' ||
+              !(new RegExp(filter_not)).test(String(item[key]))) {
+              datafilter.push(item)
+            }
+          }
+        })
+      })
+      //console.log(new Date(), 'tabledata', this.filter, JSON.stringify(datafilter, null, 2));
+      return datafilter
+    },
     getColumns: function() {
       let columns = []
       let firstRow = this.tableData[0]
