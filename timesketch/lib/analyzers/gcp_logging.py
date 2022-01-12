1ade37b6e6992b1cc267dd8f9d082f278c637902
Johan Berggren <jberggren@gmail.com>
2022-01-12 09:22:51 +0100

    Update timesketch/lib/analyzers/gcp_logging.py

--- timesketch/lib/analyzers/gcp_logging.py
+++ timesketch/lib/analyzers/gcp_logging.py
@@ -78,7 +78,6 @@ class GCPLoggingSketchPlugin(interface.BaseAnalyzer):
                 if method_name.endswith('compute.networks.insert'):
                     event.add_tags(['network-created'])
 
-                
                 # pylint: disable-msg=line-too-long
                 if method_name.endswith('compute.projects.setCommonInstanceMetadata'):
                     event.add_tags(['compute-metadata-changed'])
