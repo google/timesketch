b3d51965e0525efe2a397f217b9cc33e375a7aa6
Thomas Chopitea <tomchop@gmail.com>
2022-02-10 13:15:32 +0000

    Reviewer comments

--- timesketch/api/v1/resources/intelligence.py
+++ timesketch/api/v1/resources/intelligence.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Information API for version 1 of the Timesketch API."""
+"""Intelligence API for version 1 of the Timesketch API."""
 
 from flask import jsonify
 from flask_restful import Resource
