13536855340b1f0592a6fa3c130a032094b61c22
Johan Berggren <jberggren@gmail.com>
2022-06-07 13:58:27 +0000

    fix tests

--- timesketch/api/v1/resources_test.py
+++ timesketch/api/v1/resources_test.py
@@ -223,6 +223,9 @@ class ExploreResourceTest(BaseTest):
                     "timestamp_desc": "Content Modification Time",
                     "datetime": "2014-09-13T07:27:03+00:00",
                     "__ts_timeline_id": 1,
+                    "comment": [
+                        "test"
+                    ],
                 },
                 "_score": "null",
                 "selected": False,
@@ -288,6 +291,9 @@ class EventResourceTest(BaseTest):
             "message": "",
             "datetime": "2014-09-16T19:23:40+00:00",
             "__ts_timeline_id": 1,
+            "comment": [
+                "test"
+            ]
         }
     }
 
@@ -300,7 +306,9 @@ class EventResourceTest(BaseTest):
         response = self.client.get(
             self.resource_url + "?searchindex_id=test&event_id=test"
         )
-        self.assertDictContainsSubset(self.expected_response, response.json)
+        response_json = response.json
+        del response_json["meta"]
+        self.assertDictContainsSubset(self.expected_response, response_json)
         self.assert200(response)
 
     @mock.patch(
@@ -475,7 +483,6 @@ class SigmaListResourceTest(BaseTest):
         self.login()
         response = self.client.get(self.resource_url)
         data = json.loads(response.get_data(as_text=True))
-        print(data)
         self.assertDictContainsSubset(self.expected_response, response.json)
         self.assertIsNotNone(response)
 
