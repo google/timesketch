6f2ee255352570f3e2723bb3cfc3deae989ba240
Marco Brotto <marcobrotto@google.com>
2022-08-23 14:15:51 +0000

    add tsdev script

---
+++ contrib/tsdev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CONTAINER_ID="$(docker container list -f name=timesketch-dev -q)"
+frontend=${2:-"frontend"}
+
+if [ $1 == "web" ]; then
+  docker exec -it $CONTAINER_ID gunicorn --reload -b 0.0.0.0:5000 --log-level debug --capture-output --timeout 600 timesketch.wsgi:application
+elif [ $1 == "celery" ]; then
+  docker exec -it $CONTAINER_ID celery -A timesketch.lib.tasks worker --loglevel=info
+elif [ $1 == "vue-install" ]; then
+  docker exec -it $CONTAINER_ID yarn install --cwd=/usr/local/src/timesketch/timesketch/$frontend
+elif [ $1 == "vue-dev" ]; then
+  docker exec -it $CONTAINER_ID yarn run --cwd=/usr/local/src/timesketch/timesketch/$frontend serve
+elif [ $1 == "vue-prod" ]; then
+  docker exec -it $CONTAINER_ID yarn run --cwd=/usr/local/src/timesketch/timesketch/$frontend build
+elif [ $1 == "test" ]; then
+  docker exec -w /usr/local/src/timesketch -it $CONTAINER_ID python3 run_tests.py --coverage
+
+elif [ $1 == "shell" ]; then
+  docker exec -it $CONTAINER_ID /bin/bash
+elif [ $1 == "logs" ]; then
+  docker logs -f $CONTAINER_ID
+fi
