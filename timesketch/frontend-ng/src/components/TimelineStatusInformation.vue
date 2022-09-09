683b1973af4425b2ad137265ea1d53882f4a367f
Marco Brotto <marcobrotto@google.com>
2022-09-09 14:13:25 +0000

    add ===

--- timesketch/frontend-ng/src/components/TimelineStatusInformation.vue
+++ timesketch/frontend-ng/src/components/TimelineStatusInformation.vue
@@ -127,9 +127,9 @@ export default {
       var m = Math.floor((d % 3600) / 60)
       var s = Math.floor((d % 3600) % 60)
 
-      var hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : ''
-      var mDisplay = m > 0 ? m + (m == 1 ? ' minute, ' : ' minutes, ') : ''
-      var sDisplay = s > 0 ? s + (s == 1 ? ' second' : ' seconds') : ''
+      var hDisplay = h > 0 ? h + (h === 1 ? ' hour, ' : ' hours, ') : ''
+      var mDisplay = m > 0 ? m + (m === 1 ? ' minute, ' : ' minutes, ') : ''
+      var sDisplay = s > 0 ? s + (s === 1 ? ' second' : ' seconds') : ''
       return hDisplay + mDisplay + sDisplay
     },
   },
