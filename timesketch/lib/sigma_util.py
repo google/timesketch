6f459ce4234923be3d9d69e20f733571831d45e9
Alexander J <741037+jaegeral@users.noreply.github.com>
2022-09-16 17:18:48 +0200

    Add Sigma Database to the API (#2302)
    * introduce sigma_fields to the API
    
    * SigmaListResource update
    
    * adding licence in again
    
    * foo
    
    * sigma api endpoint
    
    * HTTP_STATUS_CODE_CONFLICT = 409
    
    * lint
    
    * s/Sigma/SigmaRule in the database model
    
    * lint
    
    * s/Sigma/SigmaRule
    
    * fix tests
    
    * user.py update
    
    * feedback from berggren
    
    * remove id from the results
    
    * improvements
    
    * make the old methods re-appear
    
    * s/es_query/query_string
    
    * comments addressed
    
    * lint
    
    * revert gitignore changes
    
    * revert query
    
    * add propper error logging
    
    * fix stuff
    
    * set initial status of a rule
    
    * address some of the comments
    
    * some fixes
    
    * fix docstrings and unit tests and one linter
    
    * lint and make tests not DRY
    
    * lint
    
    * several error messages improved
    
    * if not sigma_rule
    
    * s/get_sigma_rule_by_text/parse_sigma_rule_by_text
    
    * improve the post function
    
    * remove unused form import
    
    * remove put from test
    
    * unused import
    
    * improve error message handling
    
    * feedback addressed
    
    * docstrings
    
    * update tests and introduce checks int he util function to check for missing title and description
    
    * lint
    
    * fix lint
    
    * fix a lot of tests
    
    * lint fix
    
    * put tests
    
    * lint
    
    * fix tests
    
    * format string error
    
    * add description to the fields updated by the API
    
    * docstring
    
    * more documentation
    
    * add HTTP_STATUS_CODE_INTERNAL_SERVER_ERROR to definitions and some reviewer feedback
    
    * docstring
    
    * feedback stuff
    
    * fix unit test
    
    * Update timesketch/api/v1/resources/sigma.py
    
    Co-authored-by: Thomas Chopitea <tomchop@gmail.com>
    
    * Update timesketch/api/v1/resources/sigma.py
    
    Co-authored-by: Thomas Chopitea <tomchop@gmail.com>
    
    * Update timesketch/api/v1/resources/sigma.py
    
    Co-authored-by: Thomas Chopitea <tomchop@gmail.com>
    
    * Update timesketch/api/v1/resources/sigma.py
    
    Co-authored-by: Thomas Chopitea <tomchop@gmail.com>
    
    * Update timesketch/api/v1/resources/sigma.py
    
    Co-authored-by: Thomas Chopitea <tomchop@gmail.com>
    
    * feedback
    
    * fix some minor things
    
    * remove assert
    
    * Minor typos
    
    * move POST to SigmaRuleList
    
    * fix
    
    * Fix minor typos & docstrings
    
    * Fix typos
    
    * Remove superfluous checks
    
    Co-authored-by: Thomas Chopitea <tomchop@gmail.com>

--- timesketch/lib/sigma_util.py
+++ timesketch/lib/sigma_util.py
@@ -500,7 +500,7 @@ def sanitize_incoming_sigma_rule_text(rule_text: string):
 
 
 @lru_cache(maxsize=8)
-def get_sigma_rule_by_text(rule_text, sigma_config=None):
+def parse_sigma_rule_by_text(rule_text, sigma_config=None):
     """Returns a JSON represenation for a rule
 
     Args:
@@ -508,13 +508,20 @@ def get_sigma_rule_by_text(rule_text, sigma_config=None):
         sigma_config: config file object
 
     Returns:
-        Json representation of the parsed rule
+        JSON representation of the parsed rule
     Raises:
         sigma_exceptions.SigmaParseError: Issue with parsing the given rule
         yaml.parser.ParserError: Not a correct YAML text provided
         NotImplementedError: A feature in the provided Sigma rule is not
             implemented in Sigma for Timesketch
+        ValueError: If one of the following fiels are missing in the YAML file:
+            - title
+            - description
+        ValueError: If provided rule_text is not a string
     """
+
+    if not isinstance(rule_text, str):
+        raise ValueError("rule_text needs to be a string.")
 
     try:
         if isinstance(sigma_config, sigma_configuration.SigmaConfiguration):
@@ -565,6 +572,21 @@ def get_sigma_rule_by_text(rule_text, sigma_config=None):
 
     for sigma_rule in parsed_sigma_rules:
         sigma_es_query = _sanitize_query(sigma_rule)
+
+    if not isinstance(rule_return.get("title"), str):
+        error_msg = "Missing value: 'title' from the YAML data."
+        logger.error(error_msg)
+        raise ValueError(error_msg)
+
+    if not isinstance(rule_return.get("description"), str):
+        error_msg = "Missing value: 'description' from the YAML data."
+        logger.error(error_msg)
+        raise ValueError(error_msg)
+
+    if not isinstance(rule_return.get("id"), str):
+        error_msg = "Missing value: 'id' from the YAML data."
+        logger.error(error_msg)
+        raise ValueError(error_msg)
 
     rule_return.update({"es_query": sigma_es_query})
     rule_return.update({"file_name": "N/A"})
