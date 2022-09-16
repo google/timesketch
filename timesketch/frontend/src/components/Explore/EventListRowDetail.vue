bcde31914898adca485537fa45e73ba13ec2ff44
Thomas Chopitea <tomchop@gmail.com>
2022-09-16 11:06:04 +0000

    Add RichAttributes to the EventListRowDetail view + some style changes

--- timesketch/frontend/src/components/Explore/EventListRowDetail.vue
+++ timesketch/frontend/src/components/Explore/EventListRowDetail.vue
@@ -14,72 +14,80 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 <template>
-  <table class="table is-bordered" style="width:100%;table-layout: fixed;" @mouseup="handleSelectionChange">
-    <tbody>
-      <tr v-for="(item, key) in fullEventFiltered" :key="key" @mouseover="c_key = key" @mouseleave="c_key = -1">
-        <td style="width:40px;">
-          <span
-            class="icon is-small"
-            style="cursor:pointer;"
-            title="Apply 'Include' filter"
-            v-on:click="addFilter(key, item, 'must')"
-            ><i class="fas fa-search-plus"></i
-          ></span>
-        </td>
-        <td style="width:40px;">
-          <span
-            class="icon is-small"
-            style="cursor:pointer;"
-            title="Apply 'Exclude' filter"
-            v-on:click="addFilter(key, item, 'must_not')"
-            ><i class="fas fa-search-minus"></i
-          ></span>
-        </td>
+  <div>
+    <div v-for="(item, key) in fullEventFilteredRich" v-bind:key="key" class="rich-attributes">
+      <rich-attribute
+        :item="item"
+        :name="key"
+        @setQueryAndFilter="$emit('setQueryAndFilter', $event)"
+        @addChip="$emit('addChip', $event)"
+      />
+    </div>
 
-        <td style="word-wrap: break-word; width: 150px;">
-          {{ key }}
-          <span
-            v-if="key == c_key"
-            class="icon is-small"
-            style="cursor:pointer;"
-            title="Copy key"
-            v-clipboard:copy="key"
-            v-clipboard:success="handleCopyStatus"
-            ><i class="fas fa-copy"></i
-          ></span>
-        </td>
-        <td>
-          <span
-            v-if="key == c_key"
-            class="icon is-small"
-            style="cursor:pointer; margin-left: 3px; float:right;"
-            title="Copy value"
-            v-clipboard:copy="item"
-            v-clipboard:success="handleCopyStatus"
-            ><i class="fas fa-copy"></i
-          ></span>
-          <text-highlight
-            v-if="getRegexes(key).length > 0"
-            @addChip="$emit('addChip', $event)"
-            :highlightComponent="TsIOCMenu"
-            :queries="getRegexes(key)"
-            :attributeKey="key"
-            >{{ item }}</text-highlight
-          >
-          <span v-else>{{ item }}</span>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+    <table class="table is-bordered" style="width: 100%; table-layout: fixed" @mouseup="handleSelectionChange">
+      <tbody>
+        <tr v-for="(item, key) in fullEventFiltered" :key="key">
+          <td class="search-modifier">
+            <span
+              class="icon is-small"
+              style="cursor: pointer"
+              title="Apply 'Include' filter"
+              v-on:click="addFilter(key, item, 'must')"
+              ><i class="fas fa-search-plus"></i
+            ></span>
+          </td>
+          <td class="search-modifier">
+            <span
+              class="icon is-small"
+              style="cursor: pointer"
+              title="Apply 'Exclude' filter"
+              v-on:click="addFilter(key, item, 'must_not')"
+              ><i class="fas fa-search-minus"></i
+            ></span>
+          </td>
+
+          <td class="attribute-key">
+            {{ key }}
+            <span
+              class="icon is-small copy-action copy-action"
+              title="Copy key"
+              v-clipboard:copy="key"
+              v-clipboard:success="handleCopyStatus"
+              ><i class="fas fa-copy"></i
+            ></span>
+          </td>
+          <td class="attribute-value">
+            <span
+              class="icon is-small copy-action"
+              title="Copy value"
+              v-clipboard:copy="item"
+              v-clipboard:success="handleCopyStatus"
+              ><i class="fas fa-copy"></i
+            ></span>
+            <text-highlight
+              v-if="getRegexes(key).length > 0"
+              @addChip="$emit('addChip', $event)"
+              :highlightComponent="TsIOCMenu"
+              :queries="getRegexes(key)"
+              :attributeKey="key"
+              >{{ item }}</text-highlight
+            >
+            <span v-else>{{ item }} </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 import ApiClient from '../../utils/RestApiClient'
 import TsIOCMenu from '../Common/TsIOCMenu'
 import TextHighlight from 'vue-text-highlight'
+import RichAttribute from './RichAttribute'
 
 export default {
-  components: { TextHighlight },
+  components: { TextHighlight, RichAttribute },
   props: ['event'],
   data() {
     return {
@@ -97,7 +105,6 @@ export default {
         { type: 'hash_sha1', regex: /[0-9a-f]{40}/i, match_field: 'message' },
         { type: 'hash_sha256', regex: /[0-9a-f]{64}/i, match_field: 'message' },
       ],
-      c_key: -1,
       fullEvent: {},
     }
   },
@@ -106,13 +113,24 @@ export default {
       return this.$store.state.sketch
     },
     fullEventFiltered() {
-      Object.getOwnPropertyNames(this.fullEvent).forEach(key => {
-        // Remove internal properties from the UI
-        if (key.startsWith('__ts')) {
-          delete this.fullEvent[key]
-        }
-      })
-      return this.fullEvent
+      return Object.keys(this.fullEvent)
+        .filter((key) => {
+          return !key.startsWith('__')
+        })
+        .reduce((final, key) => {
+          final[key] = this.fullEvent[key]
+          return final
+        }, {})
+    },
+    fullEventFilteredRich() {
+      return Object.keys(this.fullEvent)
+        .filter((key) => {
+          return key.startsWith('__') && !key.startsWith('__ts')
+        })
+        .reduce((final, key) => {
+          final[key] = this.fullEvent[key]
+          return final
+        }, {})
     },
   },
   methods: {
@@ -163,3 +181,31 @@ export default {
   },
 }
 </script>
+
+<style>
+span.copy-action {
+  opacity: 0;
+  cursor: pointer;
+  margin-left: 3px;
+  float: right;
+}
+
+td:hover > span.copy-action {
+  opacity: 0.7;
+}
+
+.attribute-key {
+  word-wrap: break-word;
+  width: 150px;
+}
+
+td.search-modifier {
+  width: 40px;
+}
+
+.rich-attributes {
+  margin-bottom: 15px;
+  float: left;
+  margin-right: 15px;
+}
+</style>
