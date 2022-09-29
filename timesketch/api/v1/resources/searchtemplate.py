9419e71297a7ae04c940353a0a89ff6d8387714a
Johan Berggren <jberggren@gmail.com>
2022-09-29 13:21:38 +0000

    Initial search template UI

--- timesketch/api/v1/resources/searchtemplate.py
+++ timesketch/api/v1/resources/searchtemplate.py
@@ -107,6 +107,7 @@ class SearchTemplateParseResource(resources.ResourceMixin, Resource):
 
         try:
             template = jinja2.Template(searchtemplate.query_string)
+<<<<<<< HEAD
             query_string = template.render(form)
         except jinja2.exceptions.TemplateSyntaxError as e:
             abort(HTTP_STATUS_CODE_BAD_REQUEST, f"Search template syntax error: {e}")
@@ -115,6 +116,16 @@ class SearchTemplateParseResource(resources.ResourceMixin, Resource):
         return jsonify(
             {"objects": [{"query_string": escaped_query_string}], "meta": {}}
         )
+=======
+            parsed_query_string = template.render(form)
+            parsed_query_string = parsed_query_string.replace("/", "\/")
+            parsed_query_string = parsed_query_string.replace(".", "\.")
+        except jinja2.exceptions.TemplateSyntaxError as e:
+            abort(HTTP_STATUS_CODE_BAD_REQUEST, f"Search template syntax error: {e}")
+
+        result_dict = {"query_string": parsed_query_string}
+        return jsonify({"objects": [result_dict], "meta": {}})
+>>>>>>> 937b3dce (Initial search template UI)
 
 
 class SearchTemplateListResource(resources.ResourceMixin, Resource):
