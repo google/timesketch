764041cdc2e2f64c73553bec3b4592db677672b7
Johan Berggren <jberggren@gmail.com>
2022-10-03 16:03:07 +0200

    Remove doc_type from opensearch client code (#2350)
    * Remove doc_type as this has been removed from the opensearch python client
    
    * Remove event_type
    
    * updates, and add black formatter action
    
    * remove formatter action in this PR.
    
    * Linter
    
    * fix tests
    
    Co-authored-by: Alexander J <741037+jaegeral@users.noreply.github.com>

--- timesketch/lib/analyzers/interface.py
+++ timesketch/lib/analyzers/interface.py
@@ -128,7 +128,6 @@ class Event(object):
         datastore: Instance of OpenSearchDataStore.
         sketch: Sketch ID or None if not provided.
         event_id: ID of the Event.
-        event_type: Document type in OpenSearch.
         index_name: The name of the OpenSearch index.
         source: Source document from OpenSearch.
     """
@@ -153,7 +152,6 @@ class Event(object):
 
         try:
             self.event_id = event["_id"]
-            self.event_type = event["_type"]
             self.index_name = event["_index"]
             self.timeline_id = event.get("_source", {}).get("__ts_timeline_id")
             self.source = event.get("_source", None)
@@ -185,7 +183,6 @@ class Event(object):
 
         self.datastore.import_event(
             self.index_name,
-            self.event_type,
             event_id=self.event_id,
             event=event_to_commit,
         )
@@ -215,7 +212,6 @@ class Event(object):
         updated_event = self.datastore.set_label(
             self.index_name,
             self.event_id,
-            self.event_type,
             self.sketch.id,
             user_id,
             label,
