4bd89794bb4a2eedc15d60ffe7491080eb533393
Johan Berggren <jberggren@gmail.com>
2022-10-11 08:35:21 +0000

    rmove redundant import

--- timesketch/frontend-ng/src/plugins/dayjs.js
+++ timesketch/frontend-ng/src/plugins/dayjs.js
@@ -3,10 +3,8 @@ import dayjs from 'dayjs'
 // Add plugins
 import utc from 'dayjs/plugin/utc'
 import relativeTime from 'dayjs/plugin/relativeTime'
-import duration from 'dayjs/plugin/utc'
 
 dayjs.extend(utc)
 dayjs.extend(relativeTime)
-dayjs.extend(duration)
 
 export default dayjs
