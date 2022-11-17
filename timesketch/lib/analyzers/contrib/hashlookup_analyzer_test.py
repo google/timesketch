308801f4dd8bda75f8ba0ca36dbd4939474a5aba
David Cruciani <da.cruciani@laposte.net>
2022-11-17 09:03:46 +0100

    chg: [unittest] misp - hashlookup

--- timesketch/lib/analyzers/contrib/hashlookup_analyzer_test.py
+++ timesketch/lib/analyzers/contrib/hashlookup_analyzer_test.py
@@ -3,9 +3,6 @@ import mock
 from timesketch.lib.testlib import BaseTest
 from timesketch.lib.testlib import MockDataStore
 
-HASHLOOKUP_DB = ["301c9ec7a9aadee4d745e8fd4fa659dafbbcc6b75b9ff491d14cbbdd840814e9", "ac7233de5daa4ab262e2e751028f56a7e9d5b9e724624c1d55e8b070d8c3cd09"]
-
-MATCHING_HASH = {"sha256_hash": "ac7233de5daa4ab262e2e751028f56a7e9d5b9e724624c1d55e8b070d8c3cd09"}
 
 class TestHashlookup(BaseTest):
     """Tests the functionality of the analyzer."""
@@ -13,30 +10,34 @@ class TestHashlookup(BaseTest):
     def setUp(self):
         super().setUp()
 
+        self.HASHLOOKUP_DB = ["301c9ec7a9aadee4d745e8fd4fa659dafbbcc6b75b9ff491d14cbbdd840814e9", "ac7233de5daa4ab262e2e751028f56a7e9d5b9e724624c1d55e8b070d8c3cd09"]
+        self.MATCHING_HASH = {"sha256_hash": "ac7233de5daa4ab262e2e751028f56a7e9d5b9e724624c1d55e8b070d8c3cd09"}
+
+
     def hash_match(self):
         cp = 0
-        if HASHLOOKUP_DB['sha256_hash'] in MATCHING_HASH:
+        if self.MATCHING_HASH['sha256_hash'] in self.HASHLOOKUP_DB:
             cp += 1
         return f"Hash Match: {cp}"
 
-    @mock.patch("timesketch.lib.analyzers.interface.OpenSearchDataStore", MockDataStore)
-    @mock.patch(
-        "timesketch.lib.analyzers.contrib.hashlookup_analyzer." "HashlookupAnalyzer.get_hash_info"
-    )
+    # @mock.patch("timesketch.lib.analyzers.interface.OpenSearchDataStore", MockDataStore)
+    # @mock.patch(
+    #     "timesketch.lib.analyzers.contrib.hashlookup_analyzer." "HashlookupAnalyzer.get_hash_info"
+    # )
     def test_hash_match(self):
-        message = self.attr_match()
+        message = self.hash_match()
         self.assertEqual(
             message,
             ("Hash Match: 1"),
         )
 
-    @mock.patch("timesketch.lib.analyzers.interface.OpenSearchDataStore", MockDataStore)
-    @mock.patch(
-        "timesketch.lib.analyzers.contrib.hashlookup_analyzer." "HashlookupAnalyzer.get_hash_info"
-    )
+    # @mock.patch("timesketch.lib.analyzers.interface.OpenSearchDataStore", MockDataStore)
+    # @mock.patch(
+    #     "timesketch.lib.analyzers.contrib.hashlookup_analyzer." "HashlookupAnalyzer.get_hash_info"
+    # )
     def test_hash_nomatch(self):
-        HASHLOOKUP_DB = {}
-        message = self.attr_match()
+        self.HASHLOOKUP_DB = []
+        message = self.hash_match()
         self.assertEqual(
             message,
             ("Hash Match: 0"),
--- timesketch/lib/analyzers/contrib/misp_analyzer_test.py
+++ timesketch/lib/analyzers/contrib/misp_analyzer_test.py
@@ -3,12 +3,8 @@ import mock
 from timesketch.lib.testlib import BaseTest
 from timesketch.lib.testlib import MockDataStore
 
-MISP_ATTR = {"response": {"Attribute": [{"event_id":"5","category":"Payload delivery","type":"sha256","value":"ac7233de5daa4ab262e2e751028f56a7e9d5b9e724624c1d55e8b070d8c3cd09","Event":{"org_id":"1","id":"5","info":"Hash Test","uuid":"1f456b69-00c6-4fb0-8d92-709a0061b7d4"}},
-    {"event_id":"5","category":"Payload delivery","type":"sha1","value":"a308b3daed1711be6b1e591ba85984572a01134e","Event":{"org_id":"1","id":"5","info":"Hash Test","uuid":"1f456b69-00c6-4fb0-8d92-709a0061b7d4"}},
-    {"event_id":"5","category":"Payload delivery","type":"filename","value":"test.txt","Event":{"org_id":"1","id":"5","info":"Hash Test","uuid":"1f456b69-00c6-4fb0-8d92-709a0061b7d4"}}
-]}}
 
-MATCHING_MISP = {"filename": "test.txt", "sha1_hash": "a308b3daed1711be6b1e591ba85984572a01134e", "sha256_hash": "ac7233de5daa4ab262e2e751028f56a7e9d5b9e724624c1d55e8b070d8c3cd09", "md5_hash": "a308b3daed1711be6b1e591ba85984572a01134e"}
+
 
 class TestMisp(BaseTest):
     """Tests the functionality of the analyzer."""
@@ -16,18 +12,26 @@ class TestMisp(BaseTest):
     def setUp(self):
         super().setUp()
 
+        self.MISP_ATTR = {"response": {"Attribute": [{"event_id":"5","category":"Payload delivery","type":"sha256","value":"ac7233de5daa4ab262e2e751028f56a7e9d5b9e724624c1d55e8b070d8c3cd09","Event":{"org_id":"1","id":"5","info":"Hash Test","uuid":"1f456b69-00c6-4fb0-8d92-709a0061b7d4"}},
+            {"event_id":"5","category":"Payload delivery","type":"sha1","value":"a308b3daed1711be6b1e591ba85984572a01134e","Event":{"org_id":"1","id":"5","info":"Hash Test","uuid":"1f456b69-00c6-4fb0-8d92-709a0061b7d4"}},
+            {"event_id":"5","category":"Payload delivery","type":"filename","value":"test.txt","Event":{"org_id":"1","id":"5","info":"Hash Test","uuid":"1f456b69-00c6-4fb0-8d92-709a0061b7d4"}}
+        ]}}
+        self.MATCHING_MISP = {"filename": "test.txt", "sha1_hash": "a308b3daed1711be6b1e591ba85984572a01134e", "sha256_hash": "ac7233de5daa4ab262e2e751028f56a7e9d5b9e724624c1d55e8b070d8c3cd09", "md5_hash": "e59c93a1fd36c70b37ef654d0603c30f"}
+
     def attr_match(self):
         cp = 0
-        for key in list(MATCHING_MISP.keys()):
-            for attr in MISP_ATTR['response']['Attribute']:
-                if MATCHING_MISP[key] == attr['value']:
+        for key in list(self.MATCHING_MISP.keys()):
+            for attr in self.MISP_ATTR['response']['Attribute']:
+                if self.MATCHING_MISP[key] == attr['value']:
+                    print(f"self.MATCHING_MISP[key]: {self.MATCHING_MISP[key]}")
+                    print(f"attr[value]: {attr['value']}")
                     cp += 1
         return f"MISP Match: {cp}"
 
-    @mock.patch("timesketch.lib.analyzers.interface.OpenSearchDataStore", MockDataStore)
-    @mock.patch(
-        "timesketch.lib.analyzers.contrib.misp_analyzer." "MispAnalyzer.get_attr"
-    )
+    # @mock.patch("timesketch.lib.analyzers.interface.OpenSearchDataStore", MockDataStore)
+    # @mock.patch(
+    #     "timesketch.lib.analyzers.contrib.misp_analyzer." "MispAnalyzer.get_attr"
+    # )
     def test_attr_match(self):
         message = self.attr_match()
         self.assertEqual(
@@ -35,12 +39,12 @@ class TestMisp(BaseTest):
             ("MISP Match: 3"),
         )
 
-    @mock.patch("timesketch.lib.analyzers.interface.OpenSearchDataStore", MockDataStore)
-    @mock.patch(
-        "timesketch.lib.analyzers.contrib.misp_analyzer." "MispAnalyzer.get_attr"
-    )
+    # @mock.patch("timesketch.lib.analyzers.interface.OpenSearchDataStore", MockDataStore)
+    # @mock.patch(
+    #     "timesketch.lib.analyzers.contrib.misp_analyzer." "MispAnalyzer.get_attr"
+    # )
     def test_attr_nomatch(self):
-        MISP_ATTR = {}
+        self.MISP_ATTR = {"response": {"Attribute": []}}
         message = self.attr_match()
         self.assertEqual(
             message,
