8b1f1289bfb3ecf8fe9e26ea1c5e86a2a1f6932b
Johan Berggren <jberggren@gmail.com>
2022-11-21 09:59:26 +0000

    Refactor

--- timesketch/frontend-ng/src/views/Sketch.vue
+++ timesketch/frontend-ng/src/views/Sketch.vue
@@ -15,108 +15,180 @@ limitations under the License.
 -->
 <template>
   <div v-if="sketch">
-    <!-- Left panel -->
-    <v-navigation-drawer app permanent :width="navigationDrawer.width" hide-overlay ref="drawer">
-      <v-toolbar flat>
-        <v-avatar class="mt-2 ml-n4">
-          <router-link to="/">
-            <v-img src="/dist/timesketch-color.png" max-height="25" max-width="25" contain></v-img>
-          </router-link>
-        </v-avatar>
-        <span @click="showSketchMetadata = !showSketchMetadata" style="font-size: 1.1em; cursor: pointer"
-          >{{ sketch.name }}
-        </span>
-        <v-spacer></v-spacer>
-        <v-icon @click="hideDrawer()">mdi-chevron-left</v-icon>
-      </v-toolbar>
-      <v-expand-transition>
-        <v-list v-show="showSketchMetadata" two-line>
-          <v-list-item v-if="sketch.user">
-            <v-list-item-content>
-              <v-list-item-title> <strong>Created:</strong> {{ sketch.created_at | shortDateTime }} </v-list-item-title>
-              <v-list-item-subtitle>
-                <small>{{ sketch.created_at | timeSince }} by {{ sketch.user.username }}</small>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+    <!-- Top horizontal toolbar -->
+    <v-toolbar flat color="transparent">
+      <v-avatar v-if="!showLeftPanel" class="mt-2 ml-n1">
+        <router-link to="/">
+          <v-img src="/dist/timesketch-color.png" max-height="25" max-width="25" contain></v-img>
+        </router-link>
+      </v-avatar>
 
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>
-                <strong>Access: </strong>
-                <span v-if="meta.permissions.public">Public</span>
-                <span v-else>Restricted</span>
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                <small v-if="meta.permissions.public">Visibly to all users on this server</small>
-                <small v-else>Only people with access can open</small>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+      <v-btn icon v-if="!showLeftPanel" @click="toggleLeftPanel" class="ml-n1">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
 
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>
-                <strong>Shared with</strong>
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                <small>People and groups with access</small>
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-expand-transition>
-      <v-divider></v-divider>
-
-      <!-- Reusable dialog for adding a scenario -->
-      <ts-add-scenario-dialog :dialog.sync="dialog" @close-dialog="dialog = false" />
-
-      <div v-if="!scenarios && scenarioTemplates.length">
-        <div class="pa-4" flat :class="$vuetify.theme.dark ? 'dark-hover' : 'light-hover'">
-          <span @click="addScenarioDialog" style="cursor: pointer"
-            ><v-icon left>mdi-plus</v-icon> Add Investigation Scenario</span
-          >
-        </div>
-        <v-divider></v-divider>
+      <div v-if="activeContext.question" class="ml-2">
+        <strong>{{ activeContext.question.display_name }}</strong>
       </div>
 
-      <ts-scenario v-for="scenario in scenarios" :key="scenario.id" :scenario="scenario">
-        <v-menu offset-y :close-on-content-click="true">
-          <template v-slot:activator="{ on, attrs }">
+      <v-spacer></v-spacer>
+      <v-btn small depressed v-on:click="switchUI"> Use the old UI </v-btn>
+      <v-btn small depressed color="primary" class="ml-2">
+        <v-icon small left>mdi-account-multiple-plus</v-icon>
+        Share
+      </v-btn>
+      <v-avatar color="grey lighten-1" size="25" class="ml-3">
+        <span class="white--text">{{ currentUser | capitalize }}</span>
+      </v-avatar>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-avatar>
             <v-btn small icon v-bind="attrs" v-on="on">
               <v-icon>mdi-dots-vertical</v-icon>
             </v-btn>
-          </template>
-          <v-card>
-            <v-list>
-              <v-list-item-group color="primary">
-                <v-list-item @click="addScenarioDialog">
-                  <v-list-item-icon>
-                    <v-icon>mdi-plus</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title>Add another scenario</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
+          </v-avatar>
+        </template>
+        <v-card>
+          <v-list>
+            <v-list-item-group color="primary">
+              <v-list-item v-on:click="toggleTheme">
+                <v-list-item-icon>
+                  <v-icon>mdi-brightness-6</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Toggle theme</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon>mdi-pencil</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title>Rename</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-          </v-card>
-        </v-menu>
-      </ts-scenario>
-      <ts-saved-searches></ts-saved-searches>
-      <ts-data-types></ts-data-types>
-      <ts-tags></ts-tags>
-      <ts-search-templates></ts-search-templates>
-      <ts-sigma-rules></ts-sigma-rules>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-archive</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Archive sketch</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-export</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Export sketch</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </v-toolbar>
+
+    <!-- Left panel -->
+    <v-navigation-drawer app permanent :width="navigationDrawer.width" hide-overlay ref="drawer">
+      <div v-if="showLeftPanel">
+        <v-toolbar flat>
+          <v-avatar class="mt-2 ml-n3">
+            <router-link to="/">
+              <v-img src="/dist/timesketch-color.png" max-height="25" max-width="25" contain></v-img>
+            </router-link>
+          </v-avatar>
+          <span @click="showSketchMetadata = !showSketchMetadata" style="font-size: 1.1em; cursor: pointer"
+            >{{ sketch.name }}
+          </span>
+          <v-spacer></v-spacer>
+          <v-icon @click="toggleLeftPanel">mdi-chevron-left</v-icon>
+        </v-toolbar>
+        <v-expand-transition>
+          <v-list v-show="showSketchMetadata" two-line>
+            <v-list-item v-if="sketch.user">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <strong>Created:</strong> {{ sketch.created_at | shortDateTime }}
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  <small>{{ sketch.created_at | timeSince }} by {{ sketch.user.username }}</small>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <strong>Access: </strong>
+                  <span v-if="meta.permissions">Public</span>
+                  <span v-else>Restricted</span>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  <small v-if="meta.permissions">Visibly to all users on this server</small>
+                  <small v-else>Only people with access can open</small>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <strong>Shared with</strong>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  <small>People and groups with access</small>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-expand-transition>
+        <v-divider></v-divider>
+
+        <!-- Reusable dialog for adding a scenario -->
+        <ts-add-scenario-dialog :dialog.sync="dialog" @close-dialog="dialog = false" />
+
+        <div v-if="!scenarios && scenarioTemplates.length">
+          <div class="pa-4" flat :class="$vuetify.theme.dark ? 'dark-hover' : 'light-hover'">
+            <span @click="addScenarioDialog" style="cursor: pointer"
+              ><v-icon left>mdi-plus</v-icon> Add Investigation Scenario</span
+            >
+          </div>
+          <v-divider></v-divider>
+        </div>
+
+        <ts-scenario v-for="scenario in scenarios" :key="scenario.id" :scenario="scenario">
+          <v-menu offset-y :close-on-content-click="true">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn small icon v-bind="attrs" v-on="on">
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-list>
+                <v-list-item-group color="primary">
+                  <v-list-item @click="addScenarioDialog">
+                    <v-list-item-icon>
+                      <v-icon>mdi-plus</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Add another scenario</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon>mdi-pencil</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Rename</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+          </v-menu>
+        </ts-scenario>
+        <ts-saved-searches v-if="meta.views"></ts-saved-searches>
+        <ts-data-types></ts-data-types>
+        <ts-tags></ts-tags>
+        <ts-search-templates></ts-search-templates>
+        <ts-sigma-rules></ts-sigma-rules>
+      </div>
     </v-navigation-drawer>
 
     <router-view v-if="sketch.status"></router-view>
@@ -133,7 +205,7 @@ import TsSigmaRules from '../components/LeftPanel/SigmaRules'
 import TsAddScenarioDialog from '../components/Scenarios/AddScenarioDialog'
 
 export default {
-  props: ['sketchId', 'showLeftPanel'],
+  props: ['sketchId'],
   components: {
     TsScenario,
     TsSavedSearches,
@@ -151,14 +223,16 @@ export default {
       },
       selectedScenario: null,
       dialog: false,
+      showLeftPanel: true,
     }
   },
-  created: function () {
-    this.$store.dispatch('updateSketch', this.sketchId)
-    this.$store.dispatch('updateSearchHistory', this.sketchId)
-    this.$store.dispatch('updateScenarios', this.sketchId)
-    this.$store.dispatch('updateScenarioTemplates', this.sketchId)
-    this.$store.dispatch('updateSigmaList', this.sketchId)
+  mounted: function () {
+    this.$store.dispatch('updateSketch', this.sketchId).then(() => {
+      this.$store.dispatch('updateSearchHistory', this.sketchId)
+      this.$store.dispatch('updateScenarios', this.sketchId)
+      this.$store.dispatch('updateScenarioTemplates', this.sketchId)
+      this.$store.dispatch('updateSigmaList', this.sketchId)
+    })
   },
   updated() {
     this.$nextTick(function () {
@@ -179,14 +253,23 @@ export default {
     scenarioTemplates() {
       return this.$store.state.scenarioTemplates
     },
+    activeContext() {
+      return this.$store.state.activeContext
+    },
+    currentUser() {
+      return this.$store.state.currentUser
+    },
   },
   methods: {
+    toggleTheme: function () {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      localStorage.setItem('isDarkTheme', this.$vuetify.theme.dark.toString())
+    },
+    switchUI: function () {
+      window.location.href = window.location.href.replace('/v2/', '/')
+    },
     addScenarioDialog() {
       this.dialog = true
-    },
-    hideDrawer() {
-      this.navigationDrawer.width = 0
-      this.$emit('hideLeftPanel')
     },
     setDrawerBorderStyle() {
       let i = this.$refs.drawer.$el.querySelector('.v-navigation-drawer__border')
@@ -223,6 +306,15 @@ export default {
         false
       )
     },
+    toggleLeftPanel() {
+      console.log()
+      this.showLeftPanel = !this.showLeftPanel
+      if (this.showLeftPanel) {
+        this.navigationDrawer.width = 400
+      } else {
+        this.navigationDrawer.width = 0
+      }
+    },
   },
   watch: {
     sketch: function (newVal) {
@@ -230,11 +322,6 @@ export default {
         this.$router.push({ name: 'Overview', params: { sketchId: this.sketch.id } })
       }
       document.title = this.sketch.name
-    },
-    showLeftPanel: function (newVal) {
-      if (newVal === true) {
-        this.navigationDrawer.width = 400
-      }
     },
   },
 }
