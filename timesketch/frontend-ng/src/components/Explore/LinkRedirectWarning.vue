29ee0cd1bf1cc4d45b080b3dd50ce62b094daae9
janosch <jkpr@google.com>
2022-12-05 17:21:29 +0000

    Surfacing the context links in the front-end. + Adjusted the EventDetail.vue component to check and display an action icon when a context link is available. + Added javascript functions used to manage those context links. + Added a new LinkRedirectWarning.vue component to display when an external link is clicked.

---
+++ timesketch/frontend-ng/src/components/Explore/LinkRedirectWarning.vue
@@ -0,0 +1,72 @@
+<!--
+Copyright 2022 Google Inc. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+<template>
+    <v-card>
+      <v-card-title>
+        <v-icon large color="red">mdi-shield-alert-outline</v-icon>
+        <span class="text-h6 ml-2">External redirect warning!</span>
+      </v-card-title>
+      <v-card-text>
+        <div>
+          This action will open the link below in a new tab! Do you really 
+          want to send the following value to this external service?
+        </div>
+        <div>
+          <b>Value:</b><br/>
+          <code class="code">
+            {{ contextValue }}
+          </code>
+        </div>
+        <div>
+          <b>External website:</b><br/>
+          <code class="code">
+            {{ contextUrl }}
+          </code>
+        </div>
+  
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          text
+          @click="clearAndCancel"
+        >
+          cancel
+        </v-btn>
+        <v-btn
+          color="primary"
+          text
+          :href="contextUrl"
+          target="_blank"
+          @click="clearAndCancel"
+        >
+          continue
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+  
+  <script>
+  export default {
+    props: ['contextValue', 'contextUrl' ],
+    methods: {
+      clearAndCancel: function () {
+        this.$emit('cancel')
+      },
+    },
+  }
+  </script>
+  
+  <style scoped lang="scss"></style>
+  
