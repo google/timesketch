7eb9f02d5433b5bb332fc121418090a9415c9a3c
Alexander J <741037+jaegeral@users.noreply.github.com>
2023-01-31 15:26:50 +0100

    Update timesketch/frontend-ng/src/views/Studio.vue
    Co-authored-by: Johan Berggren <jberggren@gmail.com>

--- timesketch/frontend-ng/src/views/Studio.vue
+++ timesketch/frontend-ng/src/views/Studio.vue
@@ -26,53 +26,65 @@ limitations under the License.
       <ts-sigma-rules></ts-sigma-rules>
     </v-navigation-drawer>
 
-    <v-container fluid>
-      <v-row style="height: 64px">
-        <v-col>
-          <!-- Top horizontal toolbar -->
-          <v-toolbar flat color="transparent">
-            <v-spacer></v-spacer>
-            <v-avatar color="grey lighten-1" size="25" class="ml-3">
-              <span class="white--text">{{ currentUser | initialLetter }}</span>
-            </v-avatar>
-            <v-menu offset-y>
-              <template v-slot:activator="{ on, attrs }">
-                <v-avatar>
-                  <v-btn small icon v-bind="attrs" v-on="on">
-                    <v-icon>mdi-dots-vertical</v-icon>
-                  </v-btn>
-                </v-avatar>
-              </template>
-              <v-card>
-                <v-list>
-                  <v-list-item-group color="primary">
-                    <v-list-item v-on:click="toggleTheme">
-                      <v-list-item-icon>
-                        <v-icon>mdi-brightness-6</v-icon>
-                      </v-list-item-icon>
-                      <v-list-item-content>
-                        <v-list-item-title>Toggle theme</v-list-item-title>
-                      </v-list-item-content>
-                    </v-list-item>
+<v-toolbar flat color="transparent">
+      <v-spacer></v-spacer>
+      <v-avatar color="grey lighten-1" size="25" class="ml-3">
+        <span class="white--text">{{ currentUser | initialLetter }}</span>
+      </v-avatar>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-avatar>
+            <v-btn small icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </v-avatar>
+        </template>
+        <v-card>
+          <v-list>
+            <v-list-item-group color="primary">
+              <v-list-item v-on:click="toggleTheme">
+                <v-list-item-icon>
+                  <v-icon>mdi-brightness-6</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Toggle theme</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-                    <a href="/logout/" style="text-decoration: none; color: inherit">
-                      <v-list-item>
-                        <v-list-item-icon>
-                          <v-icon>mdi-logout</v-icon>
-                        </v-list-item-icon>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-archive</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Archive sketch</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
 
-                        <v-list-item-content>
-                          <v-list-item-title>Logout</v-list-item-title>
-                        </v-list-item-content>
-                      </v-list-item>
-                    </a>
-                  </v-list-item-group>
-                </v-list>
-              </v-card>
-            </v-menu>
-          </v-toolbar>
-        </v-col>
-      </v-row>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-icon>mdi-export</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>Export sketch</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <a href="/logout/" style="text-decoration: none; color: inherit">
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon>mdi-logout</v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content>
+                    <v-list-item-title>Logout</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </a>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </v-toolbar>
       <!-- Middle panel -->
       <ts-sigma-rule-modification app @cancel="formatXMLString = false" :rule_uuid="id" v-if="type === 'sigma'">
       </ts-sigma-rule-modification>
