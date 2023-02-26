45d88c5da6b3bcf92ddfbd146e45bb070070598c
janosch <jkpr@google.com>
2023-02-26 08:15:14 +0000

    changed button to text only and primary color. Adjusted margin & padding to match with the rest of the view.

--- timesketch/frontend-ng/src/views/Sketch.vue
+++ timesketch/frontend-ng/src/views/Sketch.vue
@@ -30,9 +30,9 @@ limitations under the License.
         <v-icon>mdi-menu</v-icon>
       </v-btn>
 
-      <v-btn v-show="currentRouteName !== 'Explore'" :to="{ name: 'Explore', params: { sketchId: sketchId } }" small depressed class="left mr-2">
+      <v-btn v-show="currentRouteName !== 'Explore'" :to="{ name: 'Explore', params: { sketchId: sketchId } }" color="primary" small text class="ml-3">
         <v-icon small left>mdi-step-backward</v-icon>
-        explore session
+        explore
       </v-btn>
       <v-spacer></v-spacer>
       <v-btn small depressed v-on:click="switchUI"> Use the old UI </v-btn>
