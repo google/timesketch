746bb7872ef18f937e445ea7f033681fe5167972
jaegeral <mail@alexanderjaeger.de>
2023-02-28 09:29:04 +0000

    lint

--- timesketch/lib/utils_test.py
+++ timesketch/lib/utils_test.py
@@ -17,7 +17,6 @@ from __future__ import unicode_literals
 
 import re
 import pandas as pd
-import datetime as dt
 
 from timesketch.lib.testlib import BaseTest
 from timesketch.lib.utils import get_validated_indices
@@ -126,13 +125,17 @@ class TestUtils(BaseTest):
                 "default_value": None,
             },
         ]
-        self.assertIs(check_mapping_errors(current_headers, valid_mapping_1), None)
+        self.assertIs(
+            check_mapping_errors(current_headers, valid_mapping_1), None
+        )
 
         valid_mapping_2 = [
             {"target": "datetime", "source": ["DT"], "default_value": None},
             {"target": "timestamp_desc", "source": None, "default_value": "a"},
         ]
-        self.assertIs(check_mapping_errors(current_headers, valid_mapping_2), None)
+        self.assertIs(
+            check_mapping_errors(current_headers, valid_mapping_2), None
+        )
 
         current_headers = ["DT", "last_access", "TD", "file_path"]
         valid_mapping_3 = [
@@ -144,7 +147,9 @@ class TestUtils(BaseTest):
                 "default_value": None,
             },
         ]
-        self.assertIs(check_mapping_errors(current_headers, valid_mapping_3), None)
+        self.assertIs(
+            check_mapping_errors(current_headers, valid_mapping_3), None
+        )
 
         current_headers = ["DT", "last_access", "TD", "file_path", "T_desc"]
         valid_mapping_4 = [
@@ -160,7 +165,9 @@ class TestUtils(BaseTest):
                 "default_value": None,
             },
         ]
-        self.assertIs(check_mapping_errors(current_headers, valid_mapping_4), None)
+        self.assertIs(
+            check_mapping_errors(current_headers, valid_mapping_4), None
+        )
 
     def test_invalid_CSV_file(self):
         """Test for CSV with missing mandatory headers without mapping"""
@@ -172,7 +179,9 @@ class TestUtils(BaseTest):
             # Call next to work around lazy generators.
             next(_validate_csv_fields(mandatory_fields, df_01))
 
-        df_02 = pd.DataFrame({"datetime": ["test"], "MSG": ["test"], "TD": ["test"]})
+        df_02 = pd.DataFrame(
+            {"datetime": ["test"], "MSG": ["test"], "TD": ["test"]}
+        )
         with self.assertRaises(RuntimeError):
             # Call next to work around lazy generators.
             next(_validate_csv_fields(mandatory_fields, df_02))
@@ -258,5 +267,7 @@ class TestUtils(BaseTest):
             },
         ]
         self.assertTrue(
-            isinstance(rename_jsonl_headers(linedict, headers_mapping, lineno), dict)
+            isinstance(
+                rename_jsonl_headers(linedict, headers_mapping, lineno), dict
+            )
         )
