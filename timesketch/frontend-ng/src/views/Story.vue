57c558f7f14570b42a5985c8719db7505e239a8f
Johan Berggren <jberggren@gmail.com>
2023-03-03 10:13:46 +0000

    initial story view

---
+++ timesketch/frontend-ng/src/views/Story.vue
@@ -0,0 +1,50 @@
+<!--
+Copyright 2023 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+<template></template>
+
+<script>
+import ApiClient from '../utils/RestApiClient'
+import EventBus from '../main'
+
+const defaultQueryFilter = () => {
+  return {
+    from: 0,
+    terminate_after: 40,
+    size: 40,
+    indices: '_all',
+    order: 'asc',
+    chips: [],
+  }
+}
+
+export default {
+  props: [],
+  components: {},
+  data: function () {
+    return {}
+  },
+  computed: {
+    sketch() {
+      return this.$store.state.sketch
+    },
+    meta() {
+      return this.$store.state.meta
+    },
+  },
+  methods: {},
+  created() {},
+}
+</script>
