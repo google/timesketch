ffb6892cd0b57e7078e23bd2925848861961fa40
Johan Berggren <jberggren@gmail.com>
2023-04-05 11:24:02 +0200

    Empty state for Scenarios tab (#2656)
    * diable button on empty list
    
    * change text

--- timesketch/frontend-ng/src/views/Sketch.vue
+++ timesketch/frontend-ng/src/views/Sketch.vue
@@ -149,8 +149,11 @@ limitations under the License.
         <v-tab-item :transition="false">
           <ts-scenario v-for="scenario in activeScenarios" :key="scenario.id" :scenario="scenario"></ts-scenario>
           <v-row class="mt-0 px-2" flat>
-            <v-col cols="6">
-              <v-btn text color="primary" @click="scenarioDialog = true" style="cursor: pointer"
+            <v-col cols="12">
+              <v-card v-if="!Object.keys(scenarioTemplates).length" flat class="pa-4"
+                >No scenarios available yet. Contact your server admin to add scenarios to this server.</v-card
+              >
+              <v-btn v-else text color="primary" @click="scenarioDialog = true" style="cursor: pointer"
                 ><v-icon left>mdi-plus</v-icon> Add Scenario</v-btn
               >
             </v-col>
