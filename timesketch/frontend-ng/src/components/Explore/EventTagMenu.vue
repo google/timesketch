1a92dcfe4e312f480bff0afc9374af854fcbd8d4
janosch <jkpr@google.com>
2023-04-20 16:08:53 +0000

    * Rework the tagging UI * Adding untag feature

--- timesketch/frontend-ng/src/components/Explore/EventTagMenu.vue
+++ timesketch/frontend-ng/src/components/Explore/EventTagMenu.vue
@@ -19,7 +19,10 @@ limitations under the License.
       <v-icon v-bind="attrs" v-on="on" class="ml-1">mdi-tag-plus-outline</v-icon>
     </template>
 
-    <v-card min-width="500px" class="mx-auto">
+    <v-card min-width="500px" class="mx-auto" max-width="500px" min-height="300px">
+      <v-btn class="float-right mr-1 mt-1" icon @click="showMenu = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
       <v-card-text>
         <strong>Quick tags</strong>
         <v-chip-group>
@@ -30,27 +33,59 @@ limitations under the License.
             :text-color="tag.textColor"
             class="text-center"
             small
+            :close="showClose(tag.tag)"
             @click="addTags(tag.tag)"
+            @click:close="removeTags(tag.tag)"
           >
             <v-icon small left> {{ tag.label }} </v-icon>
-            {{ tag.tag }}</v-chip
+            {{ tag.tag }}
+          </v-chip>
+        </v-chip-group>
+        <strong>Custom tags</strong>
+        <v-chip-group column>
+          <v-chip
+            v-for="tag in customSelectedTags"
+            :key="tag"
+            class="text-center"
+            small
+            close
+            @click:close="removeTags(tag)"
           >
+            {{ tag }}
+          </v-chip>
         </v-chip-group>
         <br />
         <v-combobox
           v-model="selectedTags"
-          :items="tags"
-          label="Add tags.."
-          outlined
-          chips
+          :hide-no-data="!search"
+          :items="customTags"
+          :search-input.sync="search"
+          hide-selected
+          label="Add tags ..."
           small-chips
-          multiple
-        ></v-combobox>
+          outlined
+          @change="addTags(selectedTags)"
+        >
+          <template v-slot:no-data>
+            <v-list-item>
+              <span class="subheading">Create new tag: </span>
+              <v-chip
+                class="ml-1"
+                small
+              >
+                {{ search }}
+              </v-chip>
+            </v-list-item>
+          </template>
+          <template v-slot:item="{ item }">
+            <v-chip
+              small
+            >
+              {{ item }}
+            </v-chip>
+          </template>
+        </v-combobox>
       </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text @click="addTags(selectedTags)">Save</v-btn>
-      </v-card-actions>
     </v-card>
   </v-menu>
 </template>
@@ -64,13 +99,14 @@ export default {
     return {
       showMenu: false,
       listItems: [],
-      selectedTags: [],
+      selectedTags: null,
       // TODO: Refactor this into a configurable option
       quickTags: [
         { tag: 'bad', color: 'red', textColor: 'white', label: 'mdi-alert-circle-outline' },
         { tag: 'suspicious', color: 'orange', textColor: 'white', label: 'mdi-help-circle-outline' },
         { tag: 'good', color: 'green', textColor: 'white', label: 'mdi-check-circle-outline' },
       ],
+      search: null,
     }
   },
   computed: {
@@ -80,30 +116,68 @@ export default {
     tags() {
       return this.$store.state.tags.map((tag) => tag.tag)
     },
+    customSelectedTags() {
+      if (!this.event._source.tag) return []
+      // returns all custom tags that are applied to an event without the quick tags
+      if (this.event._source.tag.length === 0) return []
+      let customSelectedTags = this.tags.filter((tag) => tag !== 'bad' && tag !== 'suspicious' && tag !== 'good')
+      customSelectedTags = customSelectedTags.filter((tag) => this.event._source.tag.includes(tag))
+      customSelectedTags.sort((a, b) => { return a.localeCompare(b)})
+      return customSelectedTags
+    },
+    customTags() {
+      if (!this.event._source.tag) return []
+      // returns all custom tags available for a sketch without the ones that are already applied to an event
+      let customTags = this.tags.filter((tag) => tag !== 'bad' && tag !== 'suspicious' && tag !== 'good')
+      customTags = customTags.filter((tag) => !this.customSelectedTags.includes(tag))
+      customTags.sort((a, b) => { return a.localeCompare(b)})
+      return customTags
+    },
   },
   methods: {
-    addTags: function (tagsToAdd) {
-      if (!Array.isArray(tagsToAdd)) {
-        tagsToAdd = [tagsToAdd]
+    showClose(tag) {
+      if (!this.event._source.tag) return false
+      if (this.event._source.tag.indexOf(tag) !== -1) {
+        return true
+      } else {
+         return false
       }
-
+    },
+    removeTags(tag) {
+      console.log('DEMO: removeTag', this.event, tag)
+      this.event._source.tag.splice(this.event._source.tag.indexOf(tag), 1) // DEMO only
+      this.$store.dispatch('updateTimelineTags', { sketchId: this.sketch.id, tag: tag, num: -1 }) // DEMO only
+      // ApiClient.untagEvents(this.sketch.id, [this.event], [tag])
+      //   .then((response) => {
+      //     this.event._source.tag.splice(this.event._source.tag.indexOf(tag), 1)
+      //     this.$store.dispatch('updateTimelineTags', { sketchId: this.sketch.id, tag: tag, num: -1 })
+      //   })
+      //   .catch((e) => {
+      //     console.error(e)
+      //   })
+    },
+    addTags: function (tagToAdd) {
       if (!this.event._source.hasOwnProperty('tag')) {
         this.$set(this.event._source, 'tag', [])
       }
 
-      tagsToAdd.forEach((tag) => {
-        if (this.event._source.tag.indexOf(tag) === -1) {
-          ApiClient.tagEvents(this.sketch.id, [this.event], [tag])
-            .then((response) => {
-              this.event._source.tag.push(tag)
-              this.$store.dispatch('updateTimelineTags', { sketchId: this.sketch.id, tag: tag, num: 1 })
-            })
-            .catch((e) => {
-              console.error('Cannot tag event')
-            })
+      for (const tag of tagToAdd) {
+        if (this.event._source.tag.indexOf(tag) !== -1) {
+          tagToAdd.splice(tagToAdd.indexOf(tag), 1)
         }
-        this.selectedTags = []
-        this.showMenu = false
+      }
+
+      ApiClient.tagEvents(this.sketch.id, [this.event], [tagToAdd])
+        .then((response) => {
+          this.$set(this.event._source.tag, this.event._source.tag.length, tagToAdd)
+          this.$store.dispatch('updateTimelineTags', { sketchId: this.sketch.id, tag: tagToAdd, num: 1 })
+        })
+        .catch((e) => {
+          console.error('Cannot tag event')
+        })
+      this.$nextTick(() => {
+        this.selectedTags = null
+        this.search = null
       })
     },
   },
