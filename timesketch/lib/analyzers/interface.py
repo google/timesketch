9c26b9485fbb06984410bcefc9f61e1e58579710
roshan maskey <roshanmaskey@gmail.com>
2023-04-25 17:06:23 +1000

    AnalyzerOutput and supporting classes

--- timesketch/lib/analyzers/interface.py
+++ timesketch/lib/analyzers/interface.py
@@ -22,6 +22,9 @@ import os
 import random
 import time
 import traceback
+
+from enum import IntEnum
+
 import yaml
 
 import opensearchpy
@@ -1150,3 +1153,96 @@ class BaseAnalyzer:
     def run(self):
         """Entry point for the analyzer."""
         raise NotImplementedError
+
+
+class Priority(IntEnum):
+    """Reporting priority enum to store common values.
+
+    Priorities can be anything in the range of 0-100, where 0 is the highest
+    priority.
+    """
+
+    LOW = 80
+    MEDIUM = 50
+    HIGH = 20
+    CRITICAL = 10
+
+
+class AnalyzerOutputException(Exception):
+    """Analyzer output exception."""
+
+
+class AnalyzerOutput:
+    """A class to record analyzer output.
+
+    Attributes:
+        platform (str): [Required] Analyzer platfrom.
+        analyzer_identifier (str): [Required] Unique analyzer identifier. This
+            value may be platform specific.
+        analyzer_name (str): [Required] Analyzer name.
+        dfiq_question_id (str): [Optional] Digital Forensics Investigative
+            Question (DFIQ) questions ID.
+        dfiq_question_conclusion (str): [Optional] DFQI questions answer/
+            conclusion.
+        result_priority (str): [Required] Priority of the result based on the
+            analysis findings. The valid values are CRITICAL, HIGH, MEDIUM, LOW.
+        result_summary (str): [Required] A summary statement of the analyzer
+            finding. A result summary must exist even if there is no finding.
+        result_markdown (str): [Optional] A detailed information about the
+            analyzer finding in a markdown format.
+        references (List[str]): A list of references about the analyzer or the
+            issue the analyzer attempts to address.
+        attributes (List[Any]): [Optional] A list of object that holds the
+            finding details.
+    """
+
+    def __init__(self, analyzer_id: str, analyzer_name: str) -> None:
+        """Initialize analyzer output."""
+        self.platform = "timesketch"
+        self.analyzer_identifier = analyzer_id
+        self.analyzer_name = analyzer_name
+        self.result_status = "Success"
+        self.dfiq_question_id = ""
+        self.dfiq_question_conclusion = ""
+        self.result_priority = "LOW"
+        self.result_summary = ""
+        self.result_markdown = ""
+        self.references = []
+        self.attributes = []
+
+    def validate(self) -> None:
+        """Validates the analyzer output and raises exception."""
+        if not self.analyzer_identifier:
+            raise AnalyzerOutputException("Analyzer identifier is empty")
+
+        if not self.analyzer_name:
+            raise AnalyzerOutputException("Analyzer name is empty")
+
+        if self.result_status.lower() not in ["success", "failed"]:
+            raise AnalyzerOutputException(f"Unknown result status {self.result_status}")
+
+        priority_list = ["CRITICAL", "HIGH", "MEDIUM", "LOW"]
+        if self.result_priority.upper() not in priority_list:
+            raise AnalyzerOutputException(
+                f"Unknown result priority value {self.result_priority}"
+            )
+
+        if not self.result_summary:
+            raise AnalyzerOutputException("Result summary is empty")
+
+    def __str__(self) -> str:
+        """Returns string output of AnalyzerOutput."""
+        output = {
+            "platform": self.platform,
+            "analyzer_identifier": self.analyzer_identifier,
+            "analyzer_name": self.analyzer_name,
+            "result_status": self.result_status,
+            "dfiq_question_id": self.dfiq_question_id,
+            "dfiq_question_conclusion": self.dfiq_question_conclusion,
+            "result_priority": self.result_priority,
+            "result_summary": self.result_summary,
+            "result_markdown": self.result_markdown,
+            "references": self.references,
+        }
+
+        return json.dumps(output)
