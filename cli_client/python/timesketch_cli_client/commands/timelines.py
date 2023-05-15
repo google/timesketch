b5b8ef9945a55606a280dd27b9f5f9f4b7075261
jaegeral <mail@alexanderjaeger.de>
2023-05-15 14:23:07 +0000

    give a warning if a output format was chosen that is not supported in the method

--- cli_client/python/timesketch_cli_client/commands/timelines.py
+++ cli_client/python/timesketch_cli_client/commands/timelines.py
@@ -62,6 +62,8 @@ def describe_timeline(ctx, timeline_id, output):
     if output == "json":
         click.echo(f"{timeline.resource_data}")
         return
+    elif output != "text":
+        click.echo(f"Unsupported output format: {output}")
 
     click.echo(f"Name: {timeline.name}")
     click.echo(f"Index: {timeline.index_name}")
