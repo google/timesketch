6f2ba3c635c06edb4b1fbd1f0320c1164f8cbc6e
Zawadi Done <zawadi.done@huntandhackett.com>
2023-09-13 12:42:10 +0200

    Do not show the password in the logging (#13)

--- timesketch/tsctl.py
+++ timesketch/tsctl.py
@@ -82,7 +82,7 @@ def create_user(username, password=None):
     user.set_password(plaintext=password)
     db_session.add(user)
     db_session.commit()
-    print(f"User account for {username} created/updated")
+    print(f"User {username} created/updated")
 
 
 @cli.command(name="enable-user")
