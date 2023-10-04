6b92646fa4a95f5816c53975071a9f8bf9579e9f
jaegeral <mail@alexanderjaeger.de>
2023-10-04 10:01:26 +0000

    introduce a test for csv over flush limit

--- end_to_end_tests/upload_test.py
+++ end_to_end_tests/upload_test.py
@@ -73,5 +73,59 @@ class UploadTest(interface.BaseEndToEndTest):
         events = sketch.explore("data_type:foobarcsv", as_pandas=True)
         self.assertions.assertEqual(len(events), 3251)
 
+    def test_large_upload_csv_over_flush_limit(self):
+        """Test uploading a timeline with an a lot of events > 50 k.
+        The test will create a temporary file with a large number of events
+        and then upload the file to Timesketch.
+        The test will then check that the number of events in the timeline
+        is correct."""
+
+        # create a new sketch
+        import random
+
+        randomnumber = random.randint(0, 10000)
+        sketch = self.api.create_sketch(name=randomnumber)
+        self.sketch = sketch
+
+        file_path = "/tmp/verylarge.csv"
+
+        with open(file_path, "w") as file_object:
+            file_object.write(
+                '"message","timestamp","datetime","timestamp_desc","data_type"\n'
+            )
+
+            for i in range(73251):
+                # write a line with random values for message
+                string = f'"CSV Count: {i} {randomnumber}","123456789","2015-07-24T19:01:01+00:00","Write time","73kcsv"\n'
+                file_object.write(string)
+
+        self.import_timeline(
+            "/tmp/verylarge.csv", index_name=randomnumber, sketch=sketch
+        )
+        os.remove(file_path)
+
+        timeline = sketch.list_timelines()[0]
+        # check that timeline was uploaded correctly
+        self.assertions.assertEqual(timeline.name, file_path)
+        self.assertions.assertEqual(timeline.index.name, str(randomnumber))
+        self.assertions.assertEqual(timeline.index.status, "ready")
+
+        search_obj = search.Search(sketch)
+        search_obj.query_string = "data_type:73kcsv"
+        search_obj.commit()
+
+        # normal max query limit
+        self.assertions.assertEqual(len(search_obj.table), 10000)
+        self.assertions.assertEqual(search_obj.expected_size, 73251)
+
+        # increase max entries returned:
+        search_obj.max_entries = 100000
+        search_obj.commit()
+        self.assertions.assertEqual(len(search_obj.table), 73251)
+
+        # check that the number of events is correct with a different method
+        events = sketch.explore("data_type:73kcsv", as_pandas=True, max_entries=100000)
+        self.assertions.assertEqual(len(events), 73251)
+
 
 manager.EndToEndTestManager.register_test(UploadTest)
