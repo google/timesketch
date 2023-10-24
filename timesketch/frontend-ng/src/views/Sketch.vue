b68f1049ef43b7195a9e8f86886033edbefda166
Johan Berggren <jberggren@gmail.com>
2023-10-24 11:30:48 +0000

    Move progressbar

--- timesketch/frontend-ng/src/views/Sketch.vue
+++ timesketch/frontend-ng/src/views/Sketch.vue
@@ -15,10 +15,10 @@ limitations under the License.
 -->
 <template>
   <div>
+    <!-- Progress indicator when loading sketch data -->
     <v-progress-linear v-if="loadingSketch" indeterminate color="primary"></v-progress-linear>
-    <div v-if="sketch.id" style="height: 30%">
-      <!-- Progress indicator when loading sketch data -->
 
+    <div v-if="sketch.id" style="height: 30%">
       <!-- Empty state -->
       <v-container v-if="!hasTimelines && !loadingSketch" fill-height fluid>
         <v-row align="center" justify="center">
