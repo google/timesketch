c63389ea7ee5fafaa4f6abbe57bca494a966ef6f
janosch <jkpr@google.com>
2023-10-24 10:17:22 +0000

    + Adding zoom icon + Adding reset icon

--- timesketch/frontend-ng/src/components/Explore/UnfurlDialog.vue
+++ timesketch/frontend-ng/src/components/Explore/UnfurlDialog.vue
@@ -20,32 +20,49 @@ limitations under the License.
     </v-card-subtitle>
     <v-card-text>
       <div v-show="!unfurlReady">
-        <v-progress-linear
-          color="primary"
-          indeterminate>
-        </v-progress-linear>
+        <v-progress-linear color="primary" indeterminate> </v-progress-linear>
       </div>
+      <v-divider></v-divider>
       <div v-show="unfurlReady">
         <!-- Cytoscape container -->
-        <div style="border: 1px; border: solid;">
-          <span style="font-weight: bold;">Selected node context: </span>
-          <span id="nodeContext" style="font-style: italic;">Select a node in the graph below to get more information.</span>
+        <div style="font-size: medium" class="py-1 px-1">
+          <span style="font-weight: bold">Selected node context: </span>
+          <br />
+          <span id="nodeContext" style="font-style: italic"
+            >Select a node in the graph below to get more information.</span
+          >
         </div>
-        <div ref="graphContainer">
-          <div ref="cy" width="100%" style="min-height: 400px; border: 1px; border-style: solid;"></div>
+        <div ref="graphContainer" :style="{ height: canvasHeight, width: '100%' }">
+          <v-row no-gutters>
+            <v-col>
+              <div
+                ref="cy"
+                width="100%"
+                class="pa-2"
+                :style="{ 'min-height': canvasHeight, border: '1px', 'border-style': 'solid' }"
+              ></div>
+            </v-col>
+            <v-col cols="auto">
+              <div class="iconWrapper">
+                <v-btn id="resizeGraph" icon v-on:click="resizeCanvas()" title="Fit to canvas">
+                  <v-icon>mdi-fit-to-page-outline</v-icon>
+                </v-btn>
+                <v-btn icon v-on:click="zoomGraph('plus')" title="zoom-in">
+                  <v-icon>mdi-plus</v-icon>
+                </v-btn>
+                <v-btn icon v-on:click="zoomGraph('minus')" title="zoom out">
+                  <v-icon>mdi-minus</v-icon>
+                </v-btn>
+              </div>
+            </v-col>
+          </v-row>
         </div>
       </div>
       <span>Powered by <a href="https://github.com/obsidianforensics/unfurl" target="_blank">dfir-unfurl</a></span>
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn
-        color="primary"
-        text
-        @click="clearAndCancel"
-      >
-        close
-      </v-btn>
+      <v-btn color="primary" text @click="clearAndCancel"> close </v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -59,10 +76,11 @@ cytoscape.use(dagre)
 
 export default {
   props: ['url'],
-  data () {
+  data() {
     return {
       unfurlReady: false,
       unfurlData: {},
+      canvasHeight: '400px',
       config: {
         style: [
           {
@@ -82,15 +100,15 @@ export default {
               'text-halign': 'center',
               'text-valign': 'center',
               color: '#000000',
-            }
+            },
           },
           {
             selector: 'node.highlight',
             // selector: 'node:selected',
             style: {
-                'border-color': ' #000',
-                'border-width': '2px'
-            }
+              'border-color': ' #000',
+              'border-width': '2px',
+            },
           },
           {
             selector: 'edge',
@@ -104,16 +122,16 @@ export default {
               'text-outline-width': 3,
               'text-outline-color': '#FFFFFF',
               label: 'data(label)',
-            }
+            },
           },
           {
             selector: 'edge.highlight',
             // selector: 'edge:selected',
             style: {
-              'lineColor': 'grey',
-              'width': 3
-            }
-          }
+              lineColor: 'grey',
+              width: 3,
+            },
+          },
         ],
 
         layout: {
@@ -121,7 +139,7 @@ export default {
           animated: false,
           prelayout: false,
           spacingFactor: 1,
-        }
+        },
       },
     }
   },
@@ -130,13 +148,48 @@ export default {
       this.$emit('cancel')
     },
     getUnfurlData: function (url) {
-      ApiClient.getUnfurlGraph(url).then((response) =>{
-        this.unfurlData = response.data
-        this.unfurlReady = true
-        this.buildUnfurlGraph(this.unfurlData)
-      }).catch((e) => {
-        console.error('getting unfurl error: ', e)
-      })
+      ApiClient.getUnfurlGraph(url)
+        .then((response) => {
+          this.unfurlData = response.data
+          this.unfurlReady = true
+          this.buildUnfurlGraph(this.unfurlData)
+        })
+        .catch((e) => {
+          console.error('getting unfurl error: ', e)
+        })
+    },
+    resizeCanvasWithDelay: function () {
+      this.resizeTimeout = setTimeout(() => {
+        this.resizeCanvas()
+      }, 250)
+    },
+    resizeCanvas: function () {
+      // Exit early if there is no graph canvas
+      if (!this.$refs.graphContainer) {
+        return
+      }
+      let canvasHeight = this.$refs.graphContainer.style.height
+      let canvasWidth = this.$refs.graphContainer.style.width
+      let canvas = this.$refs.cy
+      canvas.style.minHeight = canvasHeight + 'px'
+      canvas.style.height = canvasHeight + 'px'
+      canvas.style.minWidth = canvasWidth + 'px'
+      canvas.style.width = canvasWidth + 'px'
+      this.cy.resize()
+      this.cy.fit()
+    },
+    zoomGraph(direction) {
+      const currentZoomLevel = this.cy.zoom()
+      if (direction === 'plus') {
+        this.cy.zoom({
+          level: currentZoomLevel + 0.2,
+        })
+      }
+      if (direction === 'minus') {
+        this.cy.zoom({
+          level: currentZoomLevel - 0.2,
+        })
+      }
     },
     buildUnfurlGraph: function (data = {}) {
       this.cy.elements().remove()
@@ -170,19 +223,24 @@ export default {
       this.cy.style(this.config.style)
       const eles = this.cy.add(elements)
       this.cy.layout(this.config.layout).run()
-      // this.resizeGraphCanvas()
-      // this.cy.resize()
-      this.cy.fit(eles)
-      // this.cy.center(eles)
 
-      this.cy.on('click', 'node', function(e){
+      // Why can't I use this.resizeCanvas() here and need a delay?
+      this.resizeCanvasWithDelay()
+
+      this.cy.on('click', 'node', function (e) {
         eles.removeClass('highlight')
         e.target.addClass('highlight').outgoers('edge').addClass('highlight')
-        document.getElementById('nodeContext').innerHTML = e.target.data().context ? e.target.data().context : 'No context available for this node.'
+        document.getElementById('nodeContext').innerHTML = e.target.data().context
+          ? e.target.data().context
+          : 'No context available for this node.'
+        // This is a very hacky way to resize the canvas. But how can I call the
+        // resizeCanvas function from this context? this.resizeCanvas() does not work!
+        document.getElementById('resizeGraph').click()
       })
     },
   },
-  mounted () {
+  mounted() {
+    window.addEventListener('resize', this.resizeCanvasWithDelay)
     this.unfurlReady = false
     this.getUnfurlData(this.url)
     this.cy = cytoscape({
@@ -193,4 +251,10 @@ export default {
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.iconWrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
