75613f2b1dfbca6810052c6510802d4011ca5321
Johan Berggren <jberggren@gmail.com>
2023-11-21 09:45:07 +0000

    Disable linting for unmaintained contrib

--- contrib/gcs_importer.py
+++ contrib/gcs_importer.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Google Cloud Storage importer."""
+# Unmaintained contrib. Skip linting this file.
+# pylint: skip-file
 
 import argparse
 import time
