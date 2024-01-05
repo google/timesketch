03a4f9fcc563a0a31d671c7751e8b0d3e5e8dbe7
Johannes Baum <4518664+Annoraaq@users.noreply.github.com>
2024-01-05 15:03:10 +0000

    add tests

--- timesketch/frontend-ng/src/components/UploadFormButton.test.js
+++ timesketch/frontend-ng/src/components/UploadFormButton.test.js
@@ -17,20 +17,82 @@ limitations under the License.
 import {mount} from "@vue/test-utils"
 import UploadFormButton from './UploadFormButton.vue'
 import Vuetify from 'vuetify'
-import Vue from "vue";
+import Vue from "vue"
+import { expect } from "chai"
+import { vi } from "vitest"
 
-let vuetify;
-let wrapper;
+let vuetify
+let wrapper
+let uploadFormStub
+let uploadFormSlotProps
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 beforeEach(() => {
   vuetify = new Vuetify()
+  uploadFormSlotProps = {
+    on: {click: vi.fn()},
+    attrs: [],
+  }
+
+  uploadFormStub = {
+    name: 'UploadFormStub',
+    template: `
+      <span>
+          <slot :on="on" :attrs="attrs"></slot>
+      </span>
+    `,
+    data() {
+      return uploadFormSlotProps
+    }
+  }
+
   wrapper = mount(UploadFormButton, {
-    vuetify
+    vuetify,
+    stubs: {'ts-upload-timeline-form': uploadFormStub},
   })
+})
+
+afterEach(() => {
+  vi.restoreAllMocks()
 })
 
 it("contains text", async () => {
   expect(wrapper.text()).toContain('Add Timeline')
+});
+
+it("renders a large button by default", async () => {
+  const button = wrapper.findComponent({ name: 'v-btn' })
+  const icon = button.findComponent({ name: 'v-icon' })
+  const buttonProps = button.props()
+  const iconProps = icon.props()
+
+  expect(buttonProps.small).toBe(false)
+  expect(buttonProps.text).toBe(false)
+  expect(buttonProps.rounded).toBe(true)
+  expect(buttonProps.depressed).toBe(true)
+  expect(iconProps.small).toBe(false);
+});
+
+it("renders a small button", async () => {
+  await wrapper.setProps({btnType: 'small'})
+  const button = wrapper.findComponent({ name: 'v-btn' })
+  const icon = button.findComponent({ name: 'v-icon' })
+  const buttonProps = button.props()
+  const iconProps = icon.props()
+
+  expect(buttonProps.small).toBe(true)
+  expect(buttonProps.text).toBe(true)
+  expect(buttonProps.rounded).toBe(true)
+  expect(buttonProps.depressed).toBe(false)
+  expect(iconProps.small).toBe(true);
+});
+
+it("delegates click", async () => {
+  const button = wrapper.findComponent({ name: 'v-btn' })
+  expect(uploadFormSlotProps.on.click).toHaveBeenCalledTimes(0)
+
+  await button.trigger('click')
+
+  expect(uploadFormSlotProps.on.click).toHaveBeenCalledTimes(1)
 });
