05fd94726fcae7d494ca4f3a0042926ddc624757
Johannes Baum <4518664+Annoraaq@users.noreply.github.com>
2024-03-08 11:38:36 +0000

    upgrade vue-cli

--- timesketch/frontend-ng/.eslintrc.js
+++ timesketch/frontend-ng/.eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
   },
     "overrides": [
       {
