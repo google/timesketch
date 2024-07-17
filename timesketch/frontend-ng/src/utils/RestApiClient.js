290c197aeb3bfc6492164144e8b8f11506f495d5
Syd Pleno <syd.pleno@gmail.com>
2024-07-18 08:50:08 +1000

    Fix

--- timesketch/frontend-ng/src/utils/RestApiClient.js
+++ timesketch/frontend-ng/src/utils/RestApiClient.js
@@ -275,10 +275,10 @@ export default {
     return RestApiClient.get('/sketches/' + sketchId + '/aggregation/')
   },
   getAggregationById(sketchId, aggregationId) {
-    return RestApiClient.get('/sketches/' + sketchId + '/aggregation/' + aggregationId)
+    return RestApiClient.get('/sketches/' + sketchId + '/aggregation/' + aggregationId + '/')
   },
   deleteAggregationById(sketchId, aggregationId) {
-    return RestApiClient.delete('/sketches/' + sketchId + '/aggregation/' + aggregationId)
+    return RestApiClient.delete('/sketches/' + sketchId + '/aggregation/' + aggregationId + '/')
   },
   getAggregationGroups(sketchId) {
     return RestApiClient.get('/sketches/' + sketchId + '/aggregation/group/')
