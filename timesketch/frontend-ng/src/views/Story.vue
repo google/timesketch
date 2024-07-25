83e523decfa32f147827f64733f607c4b4767e60
Janosch <99879757+jkppr@users.noreply.github.com>
2024-07-25 17:11:42 +0200

    Update timesketch/frontend-ng/src/views/Story.vue

--- timesketch/frontend-ng/src/views/Story.vue
+++ timesketch/frontend-ng/src/views/Story.vue
@@ -125,7 +125,7 @@ limitations under the License.
                 </v-btn>
               </v-toolbar>
               <v-divider></v-divider>
-              <v-card-text>Legacy group Aggregations are not supported</v-card-text>
+              <v-card-text>Legacy group Aggregations are not supported. Please view this Story in the old UI or update your analyzer.</v-card-text>
             </v-card>
             <v-card v-if="block.componentName === 'TsAggregationCompact'" outlined class="mb-2">
               <v-toolbar dense flat
