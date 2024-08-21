851cb028ca6439e721a5e09a02440db20e34f556
Johan Berggren <jberggren@gmail.com>
2024-08-21 17:39:20 +0200

    Update api_client/python/timesketch_api_client/scenario_test.py

--- api_client/python/timesketch_api_client/scenario_test.py
+++ api_client/python/timesketch_api_client/scenario_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tests for the Timesketch API client"""
-from __future__ import unicode_literals
 
 import unittest
 import mock
