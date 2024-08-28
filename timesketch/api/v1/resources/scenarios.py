412879dafec6f5e46a1aee35e3e7c5544761d64b
janosch <jkpr@google.com>
2024-08-28 11:20:17 +0000

    * Add checks for min supported dfiq version before importing the data. * Catching errors

--- timesketch/api/v1/resources/scenarios.py
+++ timesketch/api/v1/resources/scenarios.py
@@ -49,7 +49,10 @@ def load_dfiq_from_config():
         DFIQ object or None if no DFIQ_PATH is configured.
     """
     dfiq_path = current_app.config.get("DFIQ_PATH")
-    if not dfiq_path:
+    if not current_app.config.get("DFIQ_ENABLED"):
+        logger.info("DFIQ is disabled. Enable in the timesketch.conf!")
+        return None
+    if not dfiq_path or not current_app.config.get("DFIQ_ENABLED"):
         logger.error("No DFIQ_PATH configured")
         return None
     return DFIQ(dfiq_path)
