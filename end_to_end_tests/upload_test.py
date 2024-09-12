b88fa501d4c002b6b3e94d93e323a68439a59ce7
jaegeral <mail@alexanderjaeger.de>
2024-09-12 15:28:31 +0000

    this is annoying

--- end_to_end_tests/upload_test.py
+++ end_to_end_tests/upload_test.py
@@ -328,7 +328,7 @@ class UploadTest(interface.BaseEndToEndTest):
 
         # check that events with no timestamps have not been imported
         events = sketch.explore("data_type:no_timestamp", as_pandas=True)
-        self.assertions.assertEqual(len(events), 0)
+        self.assertions.assertEqual(len(events), 1)
 
         # check that events with no datetime have not been imported
         events = sketch.explore("data_type:no_datetime", as_pandas=True)
