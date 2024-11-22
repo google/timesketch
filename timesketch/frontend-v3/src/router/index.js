3724c200d4a5850ba334adcd0d1cc87292d6bb7d
Johan Berggren <jberggren@gmail.com>
2024-11-22 22:01:35 +0100

    Frontend v3 Scaffold (#3188)
    * Initial Vue3/Vuetify3 scaffold
    
    * Cleanup
    
    * Scaffold update
    
    * Move dependency
    
    * Fix name
    
    * Remove file
    
    * Remove file
    
    ---------
    
    Co-authored-by: Janosch <99879757+jkppr@users.noreply.github.com>

---
+++ timesketch/frontend-v3/src/router/index.js
@@ -0,0 +1,56 @@
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
+
+// Composables
+import { createRouter, createWebHistory } from "vue-router/auto";
+
+// Import the default layout that wraps all other views with an AppBar etc.
+import Default from "@/layouts/Default";
+
+// Import App views
+import Home from "@/views/Home.vue";
+
+// Routes
+const routes = [
+  {
+    path: "/",
+    component: Default,
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: Home,
+        props: true,
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+// Workaround for https://github.com/vitejs/vite/issues/11804
+router.onError((err, to) => {
+  if (err?.message?.includes?.("Failed to fetch dynamically imported module")) {
+    if (!localStorage.getItem("vuetify:dynamic-reload")) {
+      console.log("Reloading page to fix dynamic import error");
+      localStorage.setItem("vuetify:dynamic-reload", "true");
+      location.assign(to.fullPath);
+    } else {
+      console.error("Dynamic import error, reloading page did not fix it", err);
+    }
+  } else {
+    console.error(err);
+  }
+});
+
+router.isReady().then(() => {
+  localStorage.removeItem("vuetify:dynamic-reload");
+});
+
+export default router;
