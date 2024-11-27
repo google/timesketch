faabc6ee3a716e00fc74e2a414343e516dad4548
janosch <jkpr@google.com>
2024-11-27 15:46:15 +0000

    test updated run_test script

--- run_tests.py
+++ run_tests.py
@@ -5,8 +5,7 @@ import subprocess
 
 def run_python_tests():
     subprocess.check_call(
-        "python3 -m pytest timesketch/ api_client/",
-        shell=True,
+        ["pipenv", "run", "pytest", "timesketch/", "api_client/"]
     )
 
 
