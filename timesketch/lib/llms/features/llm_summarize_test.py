e7e82d4c7afb109d81eccd4e10baf6049aac8d66
itsmvd <mvd@google.com>
2025-02-28 17:15:35 +0000

    black formatting

--- timesketch/lib/llms/features/llm_summarize_test.py
+++ timesketch/lib/llms/features/llm_summarize_test.py
@@ -21,7 +21,8 @@ from timesketch.lib.testlib import BaseTest
 from timesketch.lib.testlib import MockDataStore
 from timesketch.lib.llms.features.llm_summarize import LLMSummarizeFeature
 
-#pylint: disable=protected-access
+
+# pylint: disable=protected-access
 class TestLLMSummarizeFeature(BaseTest):
     """Tests for the LLMSummarizeFeature."""
 
