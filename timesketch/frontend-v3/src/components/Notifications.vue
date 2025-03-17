1cb686eb4590b465fba8aa3bd491e35242d34308
David Jones <david.jones@deptagency.com>
2025-03-17 15:04:11 -0400

    options API

--- timesketch/frontend-v3/src/components/Notifications.vue
+++ timesketch/frontend-v3/src/components/Notifications.vue
@@ -13,13 +13,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 -->
-
-<!--
- Canvas is a routing component to keep views alive and to support communication between
- them via Eventbus.
--->
 <template>
-  <v-snackbar v-model="isVisible" :timeout="4000" :color="notification?.type" class="rounded-m">
+  <v-snackbar
+    v-model="isVisible"
+    :timeout="4000"
+    :color="notification?.type"
+    class="rounded-m"
+  >
     <div class="d-flex align-center">
       <v-icon :icon="notification?.icon" class="mr-2" />
       <p>{{ notification?.text }}</p>
@@ -27,15 +27,25 @@ limitations under the License.
   </v-snackbar>
 </template>
 
-<script setup>
-import { useAppStore } from "@/stores/app";
-import { computed } from "vue";
+<script>
+import { useAppStore } from '@/stores/app';
 
-const store = useAppStore();
-const isVisible = ref(false);
-const notification = computed(() => store.notification);
-
-watch(notification, async () => {
-  isVisible.value = true;
-});
+export default {
+  data() {
+    return {
+      isVisible: false,
+      store: useAppStore(),
+    };
+  },
+  computed: {
+    notification() {
+      return this.store.notification;
+    },
+  },
+  watch: {
+    notification() {
+      this.isVisible = true;
+    },
+  },
+};
 </script>
