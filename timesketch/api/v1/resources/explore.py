445c363186c13ab85d12cfcdbfc22182547a9faf
jaegeral <mail@alexanderjaeger.de>
2025-03-21 13:57:29 +0000

    improve docs

--- timesketch/api/v1/resources/explore.py
+++ timesketch/api/v1/resources/explore.py
@@ -154,11 +154,15 @@ class ExploreResource(resources.ResourceMixin, Resource):
         if not query_filter:
             query_filter = {}
 
+        # Searchindices and timelines
+        # Indices here can be either a list of timeline names, IDs or a list
+        # of search indices.
         all_indices = list({t.searchindex.index_name for t in sketch.timelines})
         indices = query_filter.get("indices", all_indices)
         all_timeline_ids = [t.id for t in sketch.timelines]
 
-        # If _all in indices then execute the query on all indices
+        # If _all in indices then execute the query on all timelines in that
+        # sketch
         if "_all" in indices:
             indices = all_timeline_ids
 
