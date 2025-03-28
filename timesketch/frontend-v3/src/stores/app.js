e3e5bee004571132a2a9c90350605461696c3159
David Jones <david.jones@deptagency.com>
2025-03-28 14:23:06 -0400

    formatting

--- timesketch/frontend-v3/src/stores/app.js
+++ timesketch/frontend-v3/src/stores/app.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import ApiClient from "../utils/RestApiClient.js";
+import ApiClient from '../utils/RestApiClient.js'
 import { defineStore } from "pinia";
 
 export const useAppStore = defineStore("app", {
@@ -45,8 +45,8 @@ export const useAppStore = defineStore("app", {
     },
     snackbar: {
       active: false,
-      color: "",
-      message: "",
+      color: '',
+      message: '',
       timeout: -1,
     },
     contextLinkConf: {},
@@ -55,7 +55,7 @@ export const useAppStore = defineStore("app", {
     activeAnalyses: [],
     analyzerResults: [],
     enabledTimelines: [],
-    notification: null,
+    notification: null
   }),
   actions: {
     async setTestAppStore() {
@@ -97,55 +97,58 @@ export const useAppStore = defineStore("app", {
     },
 
     setNotification(notification) {
-      this.notification = notification;
+      this.notification = notification
     },
 
     async updateSketch(sketchId) {
       try {
         const response = await ApiClient.getSketch(sketchId);
-        this.sketch = response.data.objects[0];
-        this.meta = response.data.meta;
+          this.sketch = response.data.objects[0];
+          this.meta = response.data.meta;
         const userResp = await ApiClient.getLoggedInUser();
         let currentUser = userResp.data.objects[0].username;
         this.currentUser = currentUser;
         await this.updateTimelineTags(sketchId);
         await this.updateDataTypes(sketchId);
-      } catch (e) {}
+      } catch (e) {
+      }
     },
 
     async updateTimelineTags(sketchId) {
       if (!this.sketch.active_timelines.length) {
-        return;
+        return
       }
       let formData = {
-        aggregator_name: "field_bucket",
+        aggregator_name: 'field_bucket',
         aggregator_parameters: {
-          field: "tag",
-          limit: "1000",
+          field: 'tag',
+          limit: '1000',
         },
-      };
+      }
       try {
         const response = await ApiClient.runAggregator(sketchId, formData);
-        this.tags = response.data.objects[0]["field_bucket"]["buckets"];
-      } catch (e) {}
+        this.tags = response.data.objects[0]['field_bucket']['buckets'];
+      } catch (e) {
+      }
     },
 
     async updateDataTypes(context, sketchId) {
       if (!this.sketch.active_timelines.length) {
-        return;
+        return
       }
       let formData = {
-        aggregator_name: "field_bucket",
+        aggregator_name: 'field_bucket',
         aggregator_parameters: {
-          field: "data_type",
-          limit: "1000",
+          field: 'data_type',
+          limit: '1000',
         },
-      };
+      }
 
       try {
         const response = await ApiClient.runAggregator(sketchId, formData);
-        this.dataTypes = response.data.objects[0]["field_bucket"]["buckets"];
-      } catch (e) {}
+        this.dataTypes = response.data.objects[0]['field_bucket']['buckets']
+      } catch (e) {
+      }
     },
 
     async updateSearchHistory(sketchId) {
@@ -155,14 +158,16 @@ export const useAppStore = defineStore("app", {
       try {
         const response = await ApiClient.getSearchHistory(sketchId);
         this.getSearchHistory = response.data.objects;
-      } catch (e) {}
+      } catch (e) {
+      }
     },
 
     async updateScenarioTemplates(sketchId) {
       try {
         const response = await ApiClient.getScenarioTemplates(sketchId);
-        this.scenarioTemplates = response.data.objects;
-      } catch (e) {}
+        this.scenarioTemplates = response.data.objects
+      } catch (e) {
+      }
     },
 
     async updateSavedGraphs(sketchId) {
@@ -173,7 +178,7 @@ export const useAppStore = defineStore("app", {
         const response = await ApiClient.getSavedGraphList(sketchId);
         this.savedGraphs = response.data.objects[0] || [];
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
     },
 
@@ -181,31 +186,33 @@ export const useAppStore = defineStore("app", {
       try {
         const response = await ApiClient.getGraphPluginList();
         this.graphPlugins = response.data;
-      } catch (e) {}
+      } catch (e) {
+      }
     },
 
     async updateContextLinks() {
       try {
         const response = await ApiClient.getContextLinkConfig();
         this.contextLinkConf = response.data;
-      } catch (e) {}
+      } catch (e) {
+      }
     },
 
     async updateAnalyzerList(sketchId) {
       if (!sketchId) {
-        sketchId = this.sketch.id;
+        sketchId = this.sketch.id
       }
       try {
         const response = await ApiClient.getAnalyzers(sketchId);
-        let analyzerList = {};
+        let analyzerList = {}
         if (response.data !== undefined) {
           response.data.forEach((analyzer) => {
-            analyzerList[analyzer.name] = analyzer;
-          });
+            analyzerList[analyzer.name] = analyzer
+          })
         }
         this.sketchAnalyzerList = analyzerList;
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
     },
 
@@ -214,7 +221,7 @@ export const useAppStore = defineStore("app", {
         const response = await ApiClient.getSystemSettings();
         this.systemSettings = response.data || {};
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
     },
 
@@ -223,7 +230,7 @@ export const useAppStore = defineStore("app", {
         const response = await ApiClient.getUserSettings();
         this.settings = response.data.objects[0] || {};
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
     },
 
