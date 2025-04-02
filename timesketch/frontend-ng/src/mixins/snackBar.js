3451d56e661244f129f518b05da530c6309f55ff
itsmvd <mvd@google.com>
2025-04-02 18:09:16 +0000

    update snackbar changes from other pr

--- timesketch/frontend-ng/src/mixins/snackBar.js
+++ timesketch/frontend-ng/src/mixins/snackBar.js
@@ -1,13 +1,9 @@
-
 /*
 Copyright 2022 Google Inc. All rights reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,44 +11,42 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 import Vue from 'vue'
-
 const defaultTimeout = 5000
-const defaultSnackBar = {
-    "message": "",
-    "color": "info",
-    "timeout": defaultTimeout
-}
 
 // These methods will be available to all components without any further imports.
 Vue.mixin({
     methods: {
-        successSnackBar(message, timeout) {
-            let snackbar = defaultSnackBar
-            snackbar.message = message
-            snackbar.color = "success"
-            snackbar.timeout = timeout || defaultTimeout
+        successSnackBar(message, timeout = defaultTimeout) {
+            const snackbar = {
+                message: message,
+                color: "success",
+                timeout: timeout
+            }
             this.$store.dispatch('setSnackBar', snackbar)
         },
-        errorSnackBar(message, timeout) {
-            let snackbar = defaultSnackBar
-            snackbar.message = message
-            snackbar.color = "error"
-            snackbar.timeout = timeout || defaultTimeout
+        errorSnackBar(message, timeout = defaultTimeout) {
+            const snackbar = {
+                message: message,
+                color: "error",
+                timeout: timeout
+            }
             this.$store.dispatch('setSnackBar', snackbar)
         },
-        warningSnackBar(message, timeout) {
-          let snackbar = defaultSnackBar
-          snackbar.message = message
-          snackbar.color = "warning"
-          snackbar.timeout = timeout || defaultTimeout
-          this.$store.dispatch('setSnackBar', snackbar)
+        warningSnackBar(message, timeout = defaultTimeout) {
+            const snackbar = {
+                message: message,
+                color: "warning",
+                timeout: timeout
+            }
+            this.$store.dispatch('setSnackBar', snackbar)
         },
-        infoSnackBar(message, timeout) {
-          let snackbar = defaultSnackBar
-          snackbar.message = message
-          snackbar.color = "info"
-          snackbar.timeout = timeout || defaultTimeout
-          this.$store.dispatch('setSnackBar', snackbar)
-      },
+        infoSnackBar(message, timeout = 2000) {
+            const snackbar = {
+                message: message,
+                color: "info",
+                timeout: timeout
+            }
+            this.$store.dispatch('setSnackBar', snackbar)
+        },
     }
 })
