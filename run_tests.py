b57467e41c14429b5fad3f462f9a9d1402b48f43
jaegeral <mail@alexanderjaeger.de>
2025-05-08 08:25:00 +0000

    add cli_client to run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,9 @@ import subprocess
 
 
 def run_python_tests():
-    subprocess.check_call(["python3", "-m", "pytest", "timesketch/", "api_client/"])
+    subprocess.check_call(
+        ["python3", "-m", "pytest", "timesketch/", "api_client/", "cli_client/"]
+    )
 
 
 def main():
