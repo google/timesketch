ff76fa2d49986fce59fc95c7e80905a97bc22cde
jaegeral <mail@alexanderjaeger.de>
2025-05-19 13:35:28 +0000

    pull new image

--- utils/tsdev.sh
+++ utils/tsdev.sh
@@ -174,7 +174,7 @@ case "$1" in
 	rebuild-dev)
 		echo "Rebuilding the development environment..."
 		echo "This will stop and remove existing dev containers, then build and start new ones."
-		(cd docker/dev && $s docker-compose down && $s docker-compose up -d)
+		(cd docker/dev && $s docker-compose down && $s docker-compose up --pull always --build -d)
 		echo "Rebuild complete. You might need to re-fetch the CONTAINER_ID if it changed."
 		;;
 	restart-dev)
