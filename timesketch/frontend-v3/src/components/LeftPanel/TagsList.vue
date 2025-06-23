9a27a50907da3776e1c902571aa7c3714dec86b2
Janosch <99879757+jkppr@users.noreply.github.com>
2025-06-23 17:38:15 +0200

    Adding SearchGuideCard to the v3 frontend (#3455)
    * Adding search guidance cards to frontend-v3.
    * Adjust data iterator to v3 code
    * Make the card lists scroll-able instead of pagination.

--- timesketch/frontend-v3/src/components/LeftPanel/TagsList.vue
+++ timesketch/frontend-v3/src/components/LeftPanel/TagsList.vue
@@ -15,36 +15,36 @@ limitations under the License.
 -->
 <template>
   <div>
-    <div>
-      <v-data-iterator
-          :items="allTagsAndLabels"
-          :items-per-page.sync="itemsPerPage"
-          :search="search"
-          :hide-default-footer="allTagsAndLabels.length <= itemsPerPage"
-        >
-        <template v-slot:header v-if="allTagsAndLabels.length > itemsPerPage">
-          <v-toolbar flat>
-            <v-text-field
-              v-model="search"
-              clearable
-              hide-details
-              outlined
-              dense
-              prepend-inner-icon="mdi-magnify"
-              label="Search for tags ..."
-            ></v-text-field>
-          </v-toolbar>
-        </template>
-        <template v-slot:default="props">
-            <div
+    <v-data-iterator
+      :items="allTagsAndLabels"
+      v-model:items-per-page="itemsPerPage"
+      v-model:page="page"
+      :search="search"
+    >
+      <template v-slot:header>
+        <div v-if="showSearch" class="pa-2">
+          <v-text-field
+            v-model="search"
+            clearable
+            hide-details
+            variant="outlined"
+            density="compact"
+            prepend-inner-icon="mdi-magnify"
+            label="Search for tags ..."
+          ></v-text-field>
+        </div>
+      </template>
+      <template v-slot:default="props">
+        <div :style="containerStyles">
+          <div
             v-for="item in props.items"
             :key="item.raw.tag || item.raw.label"
             @click="applyFilterChip(item.raw.tag || item.raw.label, item.raw.tag ? 'tag' : '', item.raw.tag ? 'term' : 'label')"
             style="cursor: pointer; font-size: 0.9em"
           >
             <v-row no-gutters class="pa-2 pl-5" :class="$vuetify.theme.dark ? 'dark-hover' : 'light-hover'">
-              <v-icon v-if="item.raw.label === '__ts_star'" left size="small" class="mr-2" color="amber">mdi-star</v-icon>
-              <v-icon v-if="item.raw.label === '__ts_comment'" left size="small" class="mr-2">mdi-comment-multiple-outline</v-icon>
+              <v-icon v-if="item.raw.label === '__ts_star'" size="small" class="mr-2" color="amber">mdi-star</v-icon>
+              <v-icon v-if="item.raw.label === '__ts_comment'" size="small" class="mr-2">mdi-comment-multiple-outline</v-icon>
               <v-icon v-if="getQuickTag(item.raw.tag)" size="small" class="mr-2" :color="getQuickTag(item.raw.tag).color">{{ getQuickTag(item.raw.tag).label }}</v-icon>
               <v-icon v-else-if="item.raw.tag" size="small" class="mr-2">mdi-tag</v-icon>
 
@@ -53,9 +53,14 @@ limitations under the License.
               </span>
             </v-row>
           </div>
-        </template>
-      </v-data-iterator>
-    </div>
+        </div>
+      </template>
+      <template v-slot:footer>
+        <div v-if="!isScrollMode && numberOfPages > 1" class="d-flex justify-center pa-2">
+          <v-pagination v-model="page" :length="numberOfPages" :total-visible="5" density="compact"></v-pagination>
+        </div>
+      </template>
+    </v-data-iterator>
   </div>
 </template>
 
@@ -64,17 +69,36 @@ import EventBus from "@/event-bus.js";
 import { useAppStore } from "@/stores/app";
 
 export default {
+  props: {
+    mode: {
+      type: String,
+      default: 'paginate',
+      validator: (val) => ['paginate', 'scroll'].includes(val),
+    },
+    searchThreshold: {
+      type: Number,
+      default: 10,
+    },
+    scrollHeight: {
+      type: String,
+      default: '500px',
+    },
+    pageSize: {
+      type: Number,
+      default: 10,
+    },
+  },
   data: function () {
     return {
       appStore: useAppStore(),
-      // TODO: Refactor this into a configurable option
       quickTags: [
         { tag: 'bad', color: 'red', textColor: 'white', label: 'mdi-alert-circle-outline' },
         { tag: 'suspicious', color: 'orange', textColor: 'white', label: 'mdi-help-circle-outline' },
         { tag: 'good', color: 'green', textColor: 'white', label: 'mdi-check-circle-outline' },
       ],
-      itemsPerPage: 10,
-      search: ''
+      itemsPerPage: this.pageSize,
+      search: '',
+      page: 1,
     }
   },
   computed: {
@@ -96,28 +120,52 @@ export default {
     allTagsAndLabels() {
       const labelOrder = ['__ts_star', '__ts_comment', 'bad', 'suspicious', 'good']
       return [...this.labels, ...this.assignedQuickTags, ...this.customTags]
-        .filter(item => item.tag || item.label) // Filter out items without tag or label
+        .filter(item => item.tag || item.label)
         .sort((a, b) => {
           const aLabel = a.tag || a.label
           const bLabel = b.tag || b.label
-
           const aIsLabel = !!a.label
           const bIsLabel = !!b.label
-
-          // Sort labels before tags
           if (aIsLabel && !bIsLabel) return -1
           if (!aIsLabel && bIsLabel) return 1
-
-          // Within labels and tags, sort by predefined order first, then alphabetically
           const aOrder = labelOrder.indexOf(aLabel)
           const bOrder = labelOrder.indexOf(bLabel)
-
-          if (aOrder > -1 && bOrder > -1) return aOrder - bOrder // Sort by predefined order
-          if (aOrder > -1) return -1 // Predefined labels come first
-          if (bOrder > -1) return 1 // Predefined labels come first
-
+          if (aOrder > -1 && bOrder > -1) return aOrder - bOrder
+          if (aOrder > -1) return -1
+          if (bOrder > -1) return 1
           return aLabel.localeCompare(bLabel)
         })
+    },
+    isScrollMode() {
+      return this.mode === 'scroll'
+    },
+    showSearch() {
+      return this.allTagsAndLabels.length > this.searchThreshold
+    },
+    containerStyles() {
+      if (this.isScrollMode) {
+        return {
+          maxHeight: this.scrollHeight,
+          overflowY: 'auto',
+        }
+      }
+      return {}
+    },
+    numberOfPages() {
+      if (this.itemsPerPage <= 0) return 1
+      return Math.ceil(this.allTagsAndLabels.length / this.itemsPerPage)
+    },
+  },
+  watch: {
+    mode: {
+      immediate: true,
+      handler(newVal) {
+        if (newVal === 'scroll') {
+          this.itemsPerPage = -1
+        } else {
+          this.itemsPerPage = this.pageSize
+        }
+      },
     },
   },
   methods: {
