6a7f943e0b31f875dd9efa5d6869753796609448
jaegeral <mail@alexanderjaeger.de>
2025-09-29 14:24:01 +0000

    tests

--- end_to_end_tests/upload_test.py
+++ end_to_end_tests/upload_test.py
@@ -386,5 +386,26 @@ class UploadTest(interface.BaseEndToEndTest):
         events = sketch.explore("data_type:no_datetime", as_pandas=True)
         self.assertions.assertEqual(len(events), 1)
 
+    def test_plaso_import(self):
+        """Test the upload of a plaso file with a few events."""
+        # create a new sketch
+        rand = random.randint(0, 10000)
+        sketch = self.api.create_sketch(name=f"test_plaso_import_{rand}")
+        self.sketch = sketch
+
+        file_path = (
+            "/usr/local/src/timesketch/end_to_end_tests/test_data/evtx_20250918.plaso"
+        )
+        self.import_timeline(file_path, sketch=sketch)
+        timeline = sketch.list_timelines()[0]
+        # check that timeline was uploaded correctly
+        self.assertions.assertEqual(timeline.name, file_path)
+        self.assertions.assertEqual(timeline.index.status, "ready")
+
+        events = sketch.explore("*", as_pandas=True)
+        self.assertions.assertEqual(len(events), 1)
+
+
+manager.EndToEndTestManager.register_test(ImportPlasoTest)
 
 manager.EndToEndTestManager.register_test(UploadTest)
