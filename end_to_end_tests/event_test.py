ecb76923aa03c7a0eaf487d60ab2a4a4eaba79f5
jaegeral <mail@alexanderjaeger.de>
2025-11-21 14:59:18 +0000

    test to fix

--- end_to_end_tests/event_test.py
+++ end_to_end_tests/event_test.py
@@ -151,8 +151,12 @@ class EventTest(interface.BaseEndToEndTest):
         # 3. Add a comment and a label.
         comment_text = "This is a test comment."
         label_text = "test_label"
-        sketch.add_comment_to_event(event_id, index_id, comment_text)
-        sketch.add_label_to_event(event_id, index_id, label_text)
+        sketch.comment_event(event_id, index_id, comment_text)
+
+        events_to_label = [
+            {"_id": event_id, "_index": index_id, "_type": "generic_event"}
+        ]
+        sketch.label_events(events_to_label, label_text)
 
         # 4. Retrieve the event again to verify the annotations.
         # A short delay might be needed for the changes to be indexed.
@@ -194,7 +198,10 @@ class EventTest(interface.BaseEndToEndTest):
         label_to_toggle = "__ts_star"
 
         # 3. Add the label.
-        sketch.add_label_to_event(event_id, index_id, label_to_toggle)
+        events_to_label = [
+            {"_id": event_id, "_index": index_id, "_type": "generic_event"}
+        ]
+        sketch.label_events(events_to_label, label_to_toggle)
 
         # 4. Verify the label was added.
         time.sleep(2)  # Allow for indexing.
@@ -205,7 +212,7 @@ class EventTest(interface.BaseEndToEndTest):
         )
 
         # 5. Remove the label.
-        sketch.add_label_to_event(event_id, index_id, label_to_toggle, remove=True)
+        sketch.label_events(events_to_label, label_to_toggle, remove=True)
 
         # 6. Verify the label was removed.
         time.sleep(2)  # Allow for indexing.
