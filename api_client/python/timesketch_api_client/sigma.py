dc8b5cc76a590d00e949596a164746a50d7bb90f
janosch <jkpr@google.com>
2026-01-19 10:22:13 +0000

    apply black formatter updates to the api client code

--- api_client/python/timesketch_api_client/sigma.py
+++ api_client/python/timesketch_api_client/sigma.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Timesketch API sigma library."""
+
 from __future__ import unicode_literals
 
 import logging
