dcd1a9105a9796e8c514e0498584f16bc7a0e893
janosch <jkpr@google.com>
2026-01-19 10:47:06 +0000

    last files

--- end_to_end_tests/export_sketch_test.py
+++ end_to_end_tests/export_sketch_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """End to end tests for sketch export."""
+
 import os
 import uuid
 import zipfile
