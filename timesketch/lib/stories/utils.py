cb419d9c1937abe8976a1daf0ddd2ff2e020da2f
itsmvd <mvd@google.com>
2026-02-10 19:25:09 +0000

    linter

--- timesketch/lib/stories/utils.py
+++ timesketch/lib/stories/utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Actions for LLM features in Timesketch."""
+
 import json
 import logging
 import time
