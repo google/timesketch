7e4e9037eb4a33c14a42691b42b0a2420857bff5
janosch <jkpr@google.com>
2026-02-13 14:40:08 +0000

    * Refactor dev docker-entrypoint.sh to support a local persistent key per dev env preventing frequent re-authentication after each restart. * Adding the local key to the gitignore file to prevent leakages in PRs * Fix some format missmatches with sed commands on timesketch.conf * Moving configs that don't need change to links instead of copies

--- docker/dev/build/docker-entrypoint.sh
+++ docker/dev/build/docker-entrypoint.sh
@@ -5,16 +5,7 @@ setup_config() {
   # Copy config files
   mkdir -p /etc/timesketch
   cp /usr/local/src/timesketch/data/timesketch.conf /etc/timesketch/
-  cp /usr/local/src/timesketch/data/regex_features.yaml /etc/timesketch/
-  cp /usr/local/src/timesketch/data/winevt_features.yaml /etc/timesketch/
-  cp /usr/local/src/timesketch/data/tags.yaml /etc/timesketch/
-  cp /usr/local/src/timesketch/data/intelligence_tag_metadata.yaml /etc/timesketch/
-  cp /usr/local/src/timesketch/data/plaso.mappings /etc/timesketch/
-  cp /usr/local/src/timesketch/data/generic.mappings /etc/timesketch/
-  cp /usr/local/src/timesketch/data/ontology.yaml /etc/timesketch/
-  cp /usr/local/src/timesketch/data/data_finder.yaml /etc/timesketch/
-  cp /usr/local/src/timesketch/data/bigquery_matcher.yaml /etc/timesketch/
-  
+
   # Use -f to ignore error if link already exists
   ln -sf /usr/local/src/timesketch/data/sigma_config.yaml /etc/timesketch/sigma_config.yaml
   ln -sf /usr/local/src/timesketch/data/sigma /etc/timesketch/
@@ -23,42 +14,58 @@ setup_config() {
   ln -sf /usr/local/src/timesketch/data/plaso_formatters.yaml /etc/timesketch/plaso_formatters.yaml
   ln -sf /usr/local/src/timesketch/data/nl2q /etc/timesketch/
   ln -sf /usr/local/src/timesketch/data/llm_summarize /etc/timesketch/
+  ln -sf /usr/local/src/timesketch/data/regex_features.yaml /etc/timesketch/regex_features.yaml
+  ln -sf /usr/local/src/timesketch/data/winevt_features.yaml /etc/timesketch/winevt_features.yaml
+  ln -sf /usr/local/src/timesketch/data/tags.yaml /etc/timesketch/tags.yaml
+  ln -sf /usr/local/src/timesketch/data/intelligence_tag_metadata.yaml /etc/timesketch/intelligence_tag_metadata.yaml
+  ln -sf /usr/local/src/timesketch/data/plaso.mappings /etc/timesketch/plaso.mappings
+  ln -sf /usr/local/src/timesketch/data/generic.mappings /etc/timesketch/generic.mappings
+  ln -sf /usr/local/src/timesketch/data/ontology.yaml /etc/timesketch/ontology.yaml
+  ln -sf /usr/local/src/timesketch/data/data_finder.yaml /etc/timesketch/data_finder.yaml
+  ln -sf /usr/local/src/timesketch/data/bigquery_matcher.yaml /etc/timesketch/bigquery_matcher.yaml
 
   # Set SECRET_KEY in /etc/timesketch/timesketch.conf if it isn't already set
-  if grep -q "SECRET_KEY = '<KEY_GOES_HERE>'" /etc/timesketch/timesketch.conf; then
-    OPENSSL_RAND=$( openssl rand -base64 32 )
-    # Using the pound sign as a delimiter to avoid problems with / being output from openssl
-    sed -i 's#SECRET_KEY = \x27\x3CKEY_GOES_HERE\x3E\x27#SECRET_KEY = \x27'$OPENSSL_RAND'\x27#' /etc/timesketch/timesketch.conf
+  KEY_FILE="/usr/local/src/timesketch/.dev_secret_key"
+
+  if [ ! -f "$KEY_FILE" ]; then
+    echo "Generating new persistent development secret key..."
+    openssl rand -base64 32 > "$KEY_FILE"
+  fi
+
+  SECRET_KEY=$(cat "$KEY_FILE")
+  if grep -q 'SECRET_KEY = "<KEY_GOES_HERE>"' /etc/timesketch/timesketch.conf; then
+    # Use double quotes for the variable expansion to be safe
+    sed -i 's#SECRET_KEY = "<KEY_GOES_HERE>"#SECRET_KEY = "'"$SECRET_KEY"'"#' /etc/timesketch/timesketch.conf
   fi
 
   # Set up the Postgres connection
-  if [ $POSTGRES_USER ] && [ $POSTGRES_PASSWORD ] && [ $POSTGRES_ADDRESS ] && [ $POSTGRES_PORT ]; then
+  if [ "$POSTGRES_USER" ] && [ "$POSTGRES_PASSWORD" ] && [ "$POSTGRES_ADDRESS" ] && [ "$POSTGRES_PORT" ]; then
     sed -i 's#postgresql://<USERNAME>:<PASSWORD>@localhost#postgresql://'$POSTGRES_USER':'$POSTGRES_PASSWORD'@'$POSTGRES_ADDRESS':'$POSTGRES_PORT'#' /etc/timesketch/timesketch.conf
   fi
 
   # Set up the OpenSearch connection
-  if [ $OPENSEARCH_HOST ] && [ $OPENSEARCH_PORT ]; then
-    sed -i 's#OPENSEARCH_HOST = \x27127.0.0.1\x27#OPENSEARCH_HOST = \x27'$OPENSEARCH_HOST'\x27#' /etc/timesketch/timesketch.conf
-    sed -i 's#OPENSEARCH_PORT = 9200#OPENSEARCH_PORT = '$OPENSEARCH_PORT'#' /etc/timesketch/timesketch.conf
+  # FIX: Changed OPENSEARCH_ADDRESS to OPENSEARCH_HOST to match the if-check and docker-compose
+  if [ "$OPENSEARCH_HOST" ] && [ "$OPENSEARCH_PORT" ]; then
+    sed -i 's#OPENSEARCH_HOSTS = \[{"host": "opensearch", "port": 9200}\]#OPENSEARCH_HOSTS = [{"host": "'$OPENSEARCH_HOST'", "port": '$OPENSEARCH_PORT'}]#' /etc/timesketch/timesketch.conf
   fi
 
   # Set up the Redis connection
-  if [ $REDIS_ADDRESS ] && [ $REDIS_PORT ]; then
-    sed -i 's#UPLOAD_ENABLED = False#UPLOAD_ENABLED = True#' /etc/timesketch/timesketch.conf
-    sed -i 's#^CELERY_BROKER_URL =.*#CELERY_BROKER_URL = \x27redis://'$REDIS_ADDRESS':'$REDIS_PORT'\x27#' /etc/timesketch/timesketch.conf
-    sed -i 's#^CELERY_RESULT_BACKEND =.*#CELERY_RESULT_BACKEND = \x27redis://'$REDIS_ADDRESS':'$REDIS_PORT'\x27#' /etc/timesketch/timesketch.conf
+  if [ "$REDIS_ADDRESS" ] && [ "$REDIS_PORT" ]; then
+    sed -i 's#^UPLOAD_ENABLED = False#UPLOAD_ENABLED = True#' /etc/timesketch/timesketch.conf
+    sed -i 's#^CELERY_BROKER_URL =.*#CELERY_BROKER_URL = "redis://'$REDIS_ADDRESS':'$REDIS_PORT'"#' /etc/timesketch/timesketch.conf
+    sed -i 's#^CELERY_RESULT_BACKEND =.*#CELERY_RESULT_BACKEND = "redis://'$REDIS_ADDRESS':'$REDIS_PORT'"#' /etc/timesketch/timesketch.conf
   fi
 
   # Enable debug for the development server
-  sed -i s/"DEBUG = False"/"DEBUG = True"/ /etc/timesketch/timesketch.conf
+  sed -i 's/DEBUG = False/DEBUG = True/' /etc/timesketch/timesketch.conf
 
   # Enable index and sketch analyzers
-  sed -i s/"ENABLE_INDEX_ANALYZERS = False"/"ENABLE_INDEX_ANALYZERS = True"/ /etc/timesketch/timesketch.conf
-  sed -i s/"ENABLE_SKETCH_ANALYZERS = False"/"ENABLE_SKETCH_ANALYZERS = True"/ /etc/timesketch/timesketch.conf
-  sed -i s/"ENABLE_EXPERIMENTAL_UI = False"/"ENABLE_EXPERIMENTAL_UI = True"/ /etc/timesketch/timesketch.conf
+  sed -i 's/ENABLE_INDEX_ANALYZERS = False/ENABLE_INDEX_ANALYZERS = True/' /etc/timesketch/timesketch.conf
+  sed -i 's/ENABLE_SKETCH_ANALYZERS = False/ENABLE_SKETCH_ANALYZERS = True/' /etc/timesketch/timesketch.conf
+  sed -i 's/ENABLE_EXPERIMENTAL_UI = False/ENABLE_EXPERIMENTAL_UI = True/' /etc/timesketch/timesketch.conf
 
   # Disable CSRF checks for the development server
-  echo "WTF_CSRF_ENABLED = False" >> /etc/timesketch/timesketch.conf
+  sed -i 's/^# WTF_CSRF_ENABLED = False.*/WTF_CSRF_ENABLED = False/' /etc/timesketch/timesketch.conf
 }
 
 # Run the container the default way
@@ -77,7 +84,7 @@ if [ "$1" = 'timesketch' ]; then
   setup_config
 
   # Add web user
-  tsctl create-user --password "${TIMESKETCH_USER}" "${TIMESKETCH_USER}"
+  tsctl create-user --password "${TIMESKETCH_USER}" "${TIMESKETCH_USER}" || true
 
   # Wrap up things
   echo "Timesketch development server is ready!"
